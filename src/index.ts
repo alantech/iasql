c25a339864ce4dd44d5336a19d66b3105b2c0561
David Ellis <david@alantechnologies.com>
2021-09-17 06:17:44 -0700

    Create and Delete DBs with a template (#7)
    * Connect to the postgres DB and create and delete new databases based on a template file. Also expose the postgres DB for debugging with local clients
    
    * Address comment by @depombo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,60 @@
 import * as http from 'http'
+import * as fs from 'fs'
+import knex from 'knex'
 
-const server = http.createServer((req, res) => {
-  res.end('Hello, World!')
+const template = fs.readFileSync(`${__dirname}/template.sql`, 'utf8');
+
+const server = http.createServer(async (req, res) => {
+  if (/^\/create\/.*/.test(req.url ?? '')) {
+    // TODO: Clean/validate this input
+    const dbname = (/^\/create\/(.*)/.exec(req.url ?? '') ?? [])[1];
+    try {
+      const conn1 = knex({
+        client: 'pg',
+        connection: {
+          user: 'postgres',
+          password: 'test',
+          host: 'postgresql',
+        },
+      });
+      const resp1 = await conn1.raw(`
+        CREATE DATABASE ${dbname};
+      `);
+      const conn2 = knex({
+        client: 'pg',
+        connection: {
+          user: 'postgres',
+          password: 'test',
+          host: 'postgresql',
+          database: dbname,
+        },
+      });
+      const resp2 = await conn2.raw(template)
+      res.end(`create ${dbname}: ${JSON.stringify(resp1)} ${JSON.stringify(resp2)}`);
+    } catch (e: any) {
+      res.end(`failure to create DB: ${e?.message ?? ''}`);
+    }
+  } else if (/^\/delete\/.*/.test(req.url ?? '')) {
+    // TODO: Clean/validate this input
+    const dbname = (/^\/delete\/(.*)/.exec(req.url ?? '') ?? [])[1];
+    try {
+      const conn = knex({
+        client: 'pg',
+        connection: {
+          user: 'postgres',
+          password: 'test',
+          host: 'postgresql',
+        },
+      });
+      const resp1 = await conn.raw(`
+        DROP DATABASE ${dbname};
+      `);
+      res.end(`delete ${dbname}`);
+    } catch (e: any) {
+      res.end(`failure to drop DB: ${e?.message ?? ''}`);
+    }
+  } else {
+    res.end('Hello, World!');
+  }
 });
 server.listen(8088);
