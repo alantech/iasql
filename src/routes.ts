b1a0aa21fcff5a8ec3ddbb940e9cf122ecb7484b
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-20 18:23:24 +0200

    Express (#10)

---
+++ src/routes.ts
@@ -0,0 +1,60 @@
+import * as express from 'express'
+import * as fs from 'fs'
+import knex from 'knex'
+
+const v1 = express.Router();
+
+v1.get('/create/:db', async (req, res) => {
+  // TODO: Clean/validate this input
+  const dbname = req.params['db'];
+  const template = fs.readFileSync(`${__dirname}/template.sql`, 'utf8');
+  try {
+    const conn1 = knex({
+      client: 'pg',
+      connection: {
+        user: 'postgres',
+        password: 'test',
+        host: 'postgresql',
+      },
+    });
+    const resp1 = await conn1.raw(`
+      CREATE DATABASE ${dbname};
+    `);
+    const conn2 = knex({
+      client: 'pg',
+      connection: {
+        user: 'postgres',
+        password: 'test',
+        host: 'postgresql',
+        database: dbname,
+      },
+    });
+    const resp2 = await conn2.raw(template)
+    res.end(`create ${dbname}: ${JSON.stringify(resp1)} ${JSON.stringify(resp2)}`);
+  } catch (e: any) {
+    res.end(`failure to create DB: ${e?.message ?? ''}`);
+  }
+});
+
+v1.get('/delete/:db', async (req, res) => {
+  // TODO: Clean/validate this input
+  const dbname = req.params['db'];
+  try {
+    const conn = knex({
+      client: 'pg',
+      connection: {
+        user: 'postgres',
+        password: 'test',
+        host: 'postgresql',
+      },
+    });
+    const resp1 = await conn.raw(`
+      DROP DATABASE ${dbname};
+    `);
+    res.end(`delete ${dbname}`);
+  } catch (e: any) {
+    res.end(`failure to drop DB: ${e?.message ?? ''}`);
+  }
+});
+
+export { v1 };
