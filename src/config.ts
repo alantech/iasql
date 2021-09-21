7bcda965e2cdb4045e74605f622fd9f36b1c6d4a
depombo <lfdepombo@gmail.com>
2021-09-21 09:50:04 -0700

    WIP for e2e instance working (#19)

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,7 @@
 // env variables set locally on .env via `yarn run watch` or thorugh docker compose
 export default {
   port: process.env.PORT,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
 };
---
+++ src/sql/inserts.sql
@@ -0,0 +1,8 @@
+insert into region (region_name)
+values ('eu-west-1');
+
+insert into instance_type (instance_type)
+values ('t2.micro');
+
+insert into instance (instance_type_id)
+values (1);
