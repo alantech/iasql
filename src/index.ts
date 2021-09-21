429056de00f05f1bc75e74e45e9e6b337068cacf
David Ellis <isv.damocles@gmail.com>
2021-09-21 14:45:13 -0500

    Initial refactoring to use typeorm instead of knex

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express from 'express';
 import { inspect } from 'util';
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
 import * as express from 'express'
 import * as fs from 'fs'
-import knex from 'knex'
+import { createConnection, } from 'typeorm'
 
 import { AWS } from './services/gateways/aws'
 import config from './config'
-import { inspect } from 'util'
 
 const v1 = express.Router();
 
@@ -13,29 +12,25 @@ v1.get('/create/:db', async (req, res) => {
   const dbname = req.params['db'];
   const template = fs.readFileSync(`${__dirname}/template.sql`, 'utf8');
   try {
-    const conn1 = knex({
-      client: 'pg',
-      connection: {
-        user: 'postgres',
-        password: 'test',
-        host: 'postgresql',
-      },
+    const conn1 = await createConnection({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'test',
+      host: 'postgresql',
     });
-    const resp1 = await conn1.raw(`
+    const resp1 = await conn1.query(`
       CREATE DATABASE ${dbname};
     `);
-    const conn2 = knex({
-      client: 'pg',
-      connection: {
-        user: 'postgres',
-        password: 'test',
-        host: 'postgresql',
-        database: dbname,
-      },
+    const conn2 = await createConnection({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'test',
+      host: 'postgresql',
+      database: dbname,
     });
-    const resp2 = await conn2.raw(template);
-    conn1.destroy();
-    conn2.destroy();
+    const resp2 = await conn2.query(template);
+    conn1.close();
+    conn2.close();
     res.end(`create ${dbname}: ${JSON.stringify(resp1)} ${JSON.stringify(resp2)}`);
   } catch (e: any) {
     res.end(`failure to create DB: ${e?.message ?? ''}`);
@@ -46,18 +41,16 @@ v1.get('/delete/:db', async (req, res) => {
   // TODO: Clean/validate this input
   const dbname = req.params['db'];
   try {
-    const conn = knex({
-      client: 'pg',
-      connection: {
-        user: 'postgres',
-        password: 'test',
-        host: 'postgresql',
-      },
+    const conn = await createConnection({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'test',
+      host: 'postgresql',
     });
-    const resp1 = await conn.raw(`
+    await conn.query(`
       DROP DATABASE ${dbname};
     `);
-    conn.destroy();
+    conn.close();
     res.end(`delete ${dbname}`);
   } catch (e: any) {
     res.end(`failure to drop DB: ${e?.message ?? ''}`);
@@ -68,16 +61,13 @@ v1.get('/check/:db', async (req, res) => {
   // TODO: Clean/validate this input
   const dbname = req.params['db'];
   try {
-    const conn = knex({
-      client: 'pg',
-      connection: {
-        user: 'postgres',
-        password: 'test',
-        host: 'postgresql',
-        database: dbname,
-      },
+    const conn = await createConnection({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'test',
+      host: 'postgresql',
+      database: dbname,
     });
-    //const resp1 = await conn.select(``);
     // TODO actually use database records
     const awsClient = new AWS({ region: config.region ?? 'eu-west-1', credentials: { accessKeyId: config.accessKeyId ?? '', secretAccessKey: config.secretAccessKey ?? '' } })
     const awsInstances = await awsClient.getInstances()
@@ -86,16 +76,19 @@ v1.get('/check/:db', async (req, res) => {
       const rInstancesIds: string[] = r.Instances?.filter(i => i.State?.Name === 'running').map((i) => i.InstanceId ?? '') ?? []
       awsInstanceIds = awsInstanceIds.concat(rInstancesIds)
     });
-    const instancesToCreate = await conn.select('instance.id', 'instance_type.instance_type').table('instance')
-      .leftJoin('instance_type', 'instance.instance_type_id', 'instance_type.instance_type_id')
-      .whereNotIn('instance.instance_id', awsInstanceIds)
-      .orWhereNull('instance.instance_id')
-    await Promise.all(instancesToCreate.map(async (i) => {
+    const instancesToCreate = await conn.query(`
+      select instance.id, instance_type.instance_type
+      from instance
+      left join instance_type on instance.instance_type_id = instance_type.instance_type_id
+      where instance.instance_id not in ${awsInstanceIds}
+      or instance.instance_id is null
+    `);
+    await Promise.all(instancesToCreate.map(async (i: any) => {
       const newInstanceId = await awsClient.newInstance(i['instance_type']);
-      await conn.table('instance').where({ id: i.id }).update({ 'instance_id': newInstanceId })
-    }))
-    console.log(await conn.select().table('instance'))
-    conn.destroy();
+      await conn.query(`update instance set instance_id = ${newInstanceId} where id = ${i.id}`);
+    }));
+    console.log(await conn.query(`select * from instance`));
+    conn.close();
     res.end(`check ${dbname}`);
   } catch (e: any) {
     res.end(`failure to check DB: ${e?.message ?? ''}`);
