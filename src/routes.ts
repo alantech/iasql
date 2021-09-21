201eade359c668cd6f2d0b045ef8e74972e93715
David Ellis <isv.damocles@gmail.com>
2021-09-21 15:47:14 -0500

    Actually get it working the way it did with knex before

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import * as express from 'express'
 import * as fs from 'fs'
-import { createConnection, } from 'typeorm'
+import { createConnection, Connection, } from 'typeorm'
 
 import { AWS } from './services/gateways/aws'
 import config from './config'
@@ -11,8 +11,10 @@ v1.get('/create/:db', async (req, res) => {
   // TODO: Clean/validate this input
   const dbname = req.params['db'];
   const template = fs.readFileSync(`${__dirname}/template.sql`, 'utf8');
+  let conn1, conn2;;
   try {
-    const conn1 = await createConnection({
+    conn1 = await createConnection({
+      name: 'base', // If you use multiple connections they must have unique names or typeorm bails
       type: 'postgres',
       username: 'postgres',
       password: 'test',
@@ -21,7 +23,8 @@ v1.get('/create/:db', async (req, res) => {
     const resp1 = await conn1.query(`
       CREATE DATABASE ${dbname};
     `);
-    const conn2 = await createConnection({
+    conn2 = await createConnection({
+      name: dbname,
       type: 'postgres',
       username: 'postgres',
       password: 'test',
@@ -29,19 +32,22 @@ v1.get('/create/:db', async (req, res) => {
       database: dbname,
     });
     const resp2 = await conn2.query(template);
-    conn1.close();
-    conn2.close();
     res.end(`create ${dbname}: ${JSON.stringify(resp1)} ${JSON.stringify(resp2)}`);
   } catch (e: any) {
     res.end(`failure to create DB: ${e?.message ?? ''}`);
+  } finally {
+    conn1?.close();
+    conn2?.close();
   }
 });
 
 v1.get('/delete/:db', async (req, res) => {
   // TODO: Clean/validate this input
   const dbname = req.params['db'];
+  let conn;
   try {
-    const conn = await createConnection({
+    conn = await createConnection({
+      name: 'base',
       type: 'postgres',
       username: 'postgres',
       password: 'test',
@@ -50,18 +56,21 @@ v1.get('/delete/:db', async (req, res) => {
     await conn.query(`
       DROP DATABASE ${dbname};
     `);
-    conn.close();
     res.end(`delete ${dbname}`);
   } catch (e: any) {
     res.end(`failure to drop DB: ${e?.message ?? ''}`);
+  } finally {
+    conn?.close();
   }
 });
 
 v1.get('/check/:db', async (req, res) => {
   // TODO: Clean/validate this input
   const dbname = req.params['db'];
+  let conn: Connection | null = null;
   try {
-    const conn = await createConnection({
+    conn = await createConnection({
+      name: dbname,
       type: 'postgres',
       username: 'postgres',
       password: 'test',
@@ -85,13 +94,14 @@ v1.get('/check/:db', async (req, res) => {
     `);
     await Promise.all(instancesToCreate.map(async (i: any) => {
       const newInstanceId = await awsClient.newInstance(i['instance_type']);
-      await conn.query(`update instance set instance_id = ${newInstanceId} where id = ${i.id}`);
+      await conn?.query(`update instance set instance_id = ${newInstanceId} where id = ${i.id}`);
     }));
     console.log(await conn.query(`select * from instance`));
-    conn.close();
     res.end(`check ${dbname}`);
   } catch (e: any) {
     res.end(`failure to check DB: ${e?.message ?? ''}`);
+  } finally {
+    conn?.close();
   }
 });
 
