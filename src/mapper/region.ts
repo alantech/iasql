f18ec18a0f6489bc04b92c59f49e87f92de444f0
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-22 20:16:01 +0200

    Add todo

--- src/mapper/region.ts
+++ src/mapper/region.ts
@@ -8,6 +8,7 @@ export class RegionMapper {
   static async fromAWS(regionAWS: RegionAWS[]) {
     const regions = regionAWS.map(r => {
       const region = new Region();
+      // TODO in case the aws property does not exist do not default to '' but use actual nulls once we figure out how to handle the models
       region.name = r.RegionName ?? ''
       region.endpoint = r.Endpoint ?? ''
       region.optInStatus = r.OptInStatus ?? ''
