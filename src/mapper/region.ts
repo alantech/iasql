fdd0871c8a32f44adb5edf01d20b79ab621bd3be
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-22 19:32:14 +0200

    Refactor to save on mappers

--- src/mapper/region.ts
+++ src/mapper/region.ts
@@ -1,12 +1,20 @@
 import { Region as RegionAWS } from '@aws-sdk/client-ec2'
+
+import { Typeorm } from '../services/typeorm'
 import { Region } from '../entity/region'
 
 export class RegionMapper {
-  static fromAWS(regionAWS: RegionAWS) {
-    const region = new Region();
-    region.name = regionAWS.RegionName ?? ''
-    region.endpoint = regionAWS.Endpoint ?? ''
-    region.optInStatus = regionAWS.OptInStatus ?? ''
-    return region
+  
+  static async fromAWS(regionAWS: RegionAWS[]) {
+    const regions = regionAWS.map(r => {
+      const region = new Region();
+      region.name = r.RegionName ?? ''
+      region.endpoint = r.Endpoint ?? ''
+      region.optInStatus = r.OptInStatus ?? ''
+    });
+    // TODO make database name dynamic
+    const db = 'typeorm'
+    const orm = await Typeorm.createConn(db);
+    await orm.save(Region, regions);
   }
 }
--- src/router/aws.ts
+++ src/router/aws.ts
@@ -1,7 +1,6 @@
 import * as express from 'express'
 
 import config from '../config'
-import { RegionController } from '../controller/region'
 import { RegionMapper } from '../mapper/region'
 import { AWS } from '../services/gateways/aws'
 
@@ -11,8 +10,7 @@ aws.get('/regions', async (req, res) => {
   try {
     const awsClient = new AWS({ region: config.region ?? 'eu-west-1', credentials: { accessKeyId: config.accessKeyId ?? '', secretAccessKey: config.secretAccessKey ?? '' } })
     const awsRegions = await awsClient.getRegions()
-    const regions = awsRegions?.Regions?.map(r => RegionMapper.fromAWS(r)) ?? []
-    await RegionController.save(regions)
+    await RegionMapper.fromAWS(awsRegions?.Regions ?? [])
     res.end('ok');
   } catch (e: any) {
     console.log(e)
