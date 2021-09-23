f6f5c109ede1d73fbc2e19eb5eb7ae237db7c595
David Ellis <david@alantechnologies.com>
2021-09-22 20:18:32 -0700

    Drop the template.sql file and use the migration files directly (#26)

--- src/sql/create-template.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cat ./src/sql/types.sql > dist/template.sql
-cat ./src/sql/tables.sql >> dist/template.sql
-cat ./src/sql/indices.sql >> dist/template.sql
--- src/sql/inserts.sql
+++
@@ -1,8 +0,0 @@
-insert into region (region_name)
-values ('eu-west-1');
-
-insert into instance_type (instance_type)
-values ('t2.micro');
-
-insert into instance (instance_type_id)
-values (1);
