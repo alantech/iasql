b7f54ff4b18cc07377477cf7395c0cbc3dc0535c
David Ellis <david@alantechnologies.com>
2021-09-22 20:18:32 -0700

    Drop the template.sql file and use the migration files directly (#26)

--- src/sql/indices.sql
+++
@@ -1,7 +0,0 @@
-CREATE UNIQUE INDEX region_name_idx ON region (region_name);
-
-CREATE UNIQUE INDEX availability_zone_id_idx ON availability_zone (zone_id);
-
-CREATE UNIQUE INDEX instance_type_idx ON instance_type (instance_type);
-
-CREATE UNIQUE INDEX image_id_idx ON ami (image_id);
