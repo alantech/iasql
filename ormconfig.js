ef099e091f81cc091547e3cc092f5f9a631ca97b
David Ellis <david@alantechnologies.com>
2021-09-22 17:04:25 -0700

    Get TypeORM working again. We need to get CI set up (#24)

---
+++ ormconfig.js
@@ -0,0 +1,28 @@
+const { SnakeNamingStrategy } = require('typeorm-naming-strategies');
+
+module.exports = {
+ "type": "postgres",
+ "host": "localhost",
+ "port": 5432,
+ "username": "postgres",
+ "password": "test",
+ "database": "__example__",
+ "synchronize": true,
+ "logging": false,
+ "entities": [
+  "src/entity/**/*.ts"
+ ],
+ "migrationsTableName": "__migrations__",
+ "migrations": [
+  "src/migration/**/*.ts"
+ ],
+ "subscribers": [
+  "src/subscriber/**/*.ts"
+ ],
+ "cli": {
+  "entitiesDir": "src/entity",
+  "migrationsDir": "src/migration",
+  "subscribersDir": "src/subscriber"
+ },
+ namingStrategy: new SnakeNamingStrategy(),
+};
