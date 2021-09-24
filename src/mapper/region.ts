4b827ce99121536d188a96915a646bdc44e5451d
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-24 21:30:46 +0200

    Refactor mappers logic using a common structure (#30)

--- src/mapper/region.ts
+++ src/mapper/region.ts
@@ -1,17 +1,11 @@
 import { Region as RegionAWS } from '@aws-sdk/client-ec2'
 
-import { Region } from '../entity/region'
+import { IndexedAWS } from '../services/indexed-aws'
+import { EntityMapper } from './entity';
+import { Region } from '../entity/region';
 
-export class RegionMapper {
-  
-  static async fromAWS(regionAWS: RegionAWS[]): Promise<any> {
-    const regions = regionAWS.map(r => {
-      const region = new Region();
-      region.name = r?.RegionName
-      region.endpoint = r?.Endpoint
-      region.optInStatus = r?.OptInStatus
-      return region;
-    });
-    return regions;
-  }
-}
+export const RegionMapper = new EntityMapper(Region, {
+  name: async (region: RegionAWS, indexes: IndexedAWS) => region?.RegionName,
+  endpoint: async (region: RegionAWS, indexes: IndexedAWS) => region?.Endpoint,
+  optInStatus: async (region: RegionAWS, indexes: IndexedAWS) => region?.OptInStatus,
+})
