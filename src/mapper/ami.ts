86d1e321e9beb044b148a3e24e062967638c6c06
Alejandro Guillen <aguillenv93@gmail.com>
2021-09-27 13:27:07 +0200

    Add basic mapper

---
+++ src/mapper/ami.ts
@@ -0,0 +1,38 @@
+import { Image, } from '@aws-sdk/client-ec2'
+
+import { IndexedAWS, } from '../services/indexed-aws'
+import { EntityMapper, } from './entity';
+import { AMI, } from '../entity/ami';
+import { CPUArchitectureMapper } from '.';
+
+export const AMIMapper = new EntityMapper(AMI, {
+  cpuArchitecture: async (ami: Image, _indexes: IndexedAWS) => CPUArchitectureMapper.fromAWS(
+    ami?.Architecture, _indexes
+  ),
+  creationDate: async (ami: Image, _indexes: IndexedAWS) => ami?.CreationDate,
+  imageId: async (ami: Image, _indexes: IndexedAWS) => ami?.ImageId,
+  imageLocation: async (ami: Image, _indexes: IndexedAWS) => ami?.ImageLocation,
+  imageType: async (ami: Image, _indexes: IndexedAWS) => ami?.ImageType,
+  public: async (ami: Image, _indexes: IndexedAWS) => ami?.Public,
+  kernelId: async (ami: Image, _indexes: IndexedAWS) => ami?.KernelId,
+  ownerId: async (ami: Image, _indexes: IndexedAWS) => ami?.OwnerId,
+  platform: async (ami: Image, _indexes: IndexedAWS) => ami?.Platform,
+  platformDetails: async (ami: Image, _indexes: IndexedAWS) => ami?.PlatformDetails,
+  usageOperation: async (ami: Image, _indexes: IndexedAWS) => ami?.UsageOperation,
+  productCodes: async (ami: Image, _indexes: IndexedAWS) => ami?.ProductCodes, // TODO: add mapper
+  ramdiskId: async (ami: Image, _indexes: IndexedAWS) => ami?.RamdiskId,
+  state: async (ami: Image, _indexes: IndexedAWS) => ami?.State,
+  blockDeviceMappings: async (ami: Image, _indexes: IndexedAWS) => ami?.BlockDeviceMappings, // TODO: add mapper
+  description: async (ami: Image, _indexes: IndexedAWS) => ami?.Description,
+  enaSupport: async (ami: Image, _indexes: IndexedAWS) => ami?.EnaSupport,
+  hypervisor: async (ami: Image, _indexes: IndexedAWS) => ami?.Hypervisor,
+  imageOwnerAlias: async (ami: Image, _indexes: IndexedAWS) => ami?.ImageOwnerAlias,
+  name: async (ami: Image, _indexes: IndexedAWS) => ami?.Name,
+  rootDeviceName: async (ami: Image, _indexes: IndexedAWS) => ami?.RootDeviceName,
+  rootDeviceType: async (ami: Image, _indexes: IndexedAWS) => ami?.RootDeviceType,
+  sirovNetSupport: async (ami: Image, _indexes: IndexedAWS) => ami?.SriovNetSupport,
+  stateReason: async (ami: Image, _indexes: IndexedAWS) => ami?.StateReason, // TODO: add mapper
+  bootMode: async (ami: Image, _indexes: IndexedAWS) => ami?.BootMode, // TODO: add mapper
+  deprecationTime: async (ami: Image, _indexes: IndexedAWS) => ami?.DeprecationTime,
+  tags: async (ami: Image, _indexes: IndexedAWS) => ami?.Tags, // TODO: add mapper
+})
