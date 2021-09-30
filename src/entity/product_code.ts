15d29ef291c2a078c6faf60bbf7c9305838480b0
David Ellis <isv.damocles@gmail.com>
2021-09-30 09:45:02 -0500

    Remove some decorators from entities that cannot be independently queried from AWS

--- src/entity/product_code.ts
+++ src/entity/product_code.ts
@@ -1,17 +1,10 @@
 import { Entity, PrimaryGeneratedColumn, Column, } from 'typeorm';
 
-import { source, Source, } from '../services/source-of-truth'
-import { awsPrimaryKey, } from '../services/aws-primary-key'
-import { noDiff, } from '../services/diff'
-
-@source(Source.AWS)
 @Entity()
 export class ProductCode {
-  @noDiff
   @PrimaryGeneratedColumn()
   id: number;
 
-  @awsPrimaryKey // TODO: What if it really is nullable?
   @Column({
     nullable: true
   })
