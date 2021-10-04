838844a320bc21c0565ce304a741d6ebac87e58e
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-04 17:46:49 +0200

    Add missing decorator and security group

--- src/entity/instance.ts
+++ src/entity/instance.ts
@@ -4,17 +4,26 @@ import {
   Column,
   JoinColumn,
   ManyToOne,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
+import { awsPrimaryKey } from '../services/aws-primary-key';
+import { noDiff } from '../services/diff';
+import { Source, source } from '../services/source-of-truth';
 import { AMI } from './ami'
 import { InstanceType } from './instance_type'
 import { Region } from './region'
+import { SecurityGroup } from './security_group';
 
 // TODO complete instance schema
+@source(Source.DB)
 @Entity()
 export class Instance {
+  @noDiff
   @PrimaryGeneratedColumn()
   id: number;
 
+  @awsPrimaryKey
   @Column({
     nullable: true
   })
@@ -37,4 +46,8 @@ export class Instance {
     name: 'instance_type_id',
   })
   instanceType: InstanceType;
+
+  @ManyToMany(() => SecurityGroup)
+  @JoinTable()
+  securityGroups: SecurityGroup[]
 }
