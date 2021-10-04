31919a81e058903b1f1cc6aa8005761d126a46bf
David Ellis <david@alantechnologies.com>
2021-10-04 10:59:59 -0700

    /check endpoint now writes (for a single entity) (#43)
    * /check endpoint now writes (for a single entity)
    
    * Minor change to createAWS api
    
    * Get compiling again
    
    * Fix lint
    
    * Add check for availability zone mapper to only run when appropriate (until better solution is implemented)
    
    * Update src/router/db.ts
    
    Co-authored-by: Alejandro Guillen <aguillenv93@gmail.com>
    
    Co-authored-by: Alejandro Guillen <aguillenv93@gmail.com>

--- src/mapper/valid_threads_per_core.ts
+++ src/mapper/valid_threads_per_core.ts
@@ -7,7 +7,7 @@ export const ValidThreadsPerCoreMapper = new EntityMapper(ValidThreadsPerCore, {
   count: (count: number, _indexes: IndexedAWS) => count ?? null,
 }, {
   readAWS: async (_awsClient: AWS, _indexes: IndexedAWS) => { return },
-  createAWS: async (_obj: any, _indexes: IndexedAWS) => { throw new Error('tbd') },
-  updateAWS: async (_obj: any, _indexes: IndexedAWS) => { throw new Error('tbd') },
-  deleteAWS: async (_obj: any, _indexes: IndexedAWS) => { throw new Error('tbd') },
+  createAWS: async (_obj: any, _awsClient: AWS, _indexes: IndexedAWS) => { throw new Error('tbd') },
+  updateAWS: async (_obj: any, _awsClient: AWS, _indexes: IndexedAWS) => { throw new Error('tbd') },
+  deleteAWS: async (_obj: any, _awsClient: AWS, _indexes: IndexedAWS) => { throw new Error('tbd') },
 })
