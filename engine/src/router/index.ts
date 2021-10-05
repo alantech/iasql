147a2ad6d17f8d63ceb2321bdc12f25012890c94
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-05 21:55:52 +0200

    Update CI/CD (#52)
    * Remove legacy test endpoints
    
    * Add find endpoint
    
    * Add endpoints to ci
    
    * Test branch
    
    * Comment while adding secrets
    
    * Send error code
    
    * Add -f flag to curl
    
    * Add migrate
    
    * Complete env
    
    * Restore branch
    
    * Test create
    
    * debug
    
    * Debug config
    
    * Handle env vars
    
    * Remove debuggers
    
    * Add us-east region
    
    * Remove migrate endpoint
    
    * Remove -f option
    
    * Debug
    
    * Test timeouts
    
    * Add node options
    
    * Remove debuggers and restore config
    
    * Update engine/package.json
    
    Co-authored-by: David Ellis <david@alantechnologies.com>
    
    Co-authored-by: David Ellis <david@alantechnologies.com>

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,30 +1,9 @@
-import { inspect, } from 'util'
-
 import * as express from 'express'
 
-import { AWS, } from '../services/gateways/aws'
-import config from '../config'
-import { aws, } from './aws'
 import { db, } from './db'
-import { RegionMapper, } from '../mapper/region'
-import { IndexedAWS, } from '../services/indexed-aws'
 
 const v1 = express.Router();
-v1.get('/map', async(_req, res) => {
-  const awsClient = new AWS({
-    region: config.region ?? 'eu-west-1',
-    credentials: {
-      accessKeyId: config.accessKeyId ?? '',
-      secretAccessKey: config.secretAccessKey ?? '',
-    },
-  })
-  const regionsAWS = await awsClient.getRegions();
-  const indexes = new IndexedAWS();
-  const entity = await RegionMapper.fromAWS(regionsAWS.Regions?.pop(), indexes);
-  res.end(`mapped: ${inspect(entity)}`);
-})
 
 v1.use('/db', db)
-v1.use('/aws', aws)
 
 export { v1 };
