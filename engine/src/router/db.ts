290430f59b171118b30c8f6c785c6c5dc27541c9
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-05 21:14:39 +0200

    Debug

--- engine/src/router/db.ts
+++ engine/src/router/db.ts
@@ -93,6 +93,7 @@ db.get('/create/:db', async (req, res) => {
       database: dbname,
     });
     await migrate(conn2);
+    if (!config.accessKeyId) throw new Error('noo credentials');
     const awsClient = new AWS({
       region: config.region ?? 'eu-west-1',
       credentials: {
