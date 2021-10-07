60852878906f87728543d65ec57fef6f9c3dd515
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-07 19:10:20 +0200

    Complete rds schema

--- engine/src/entity/rds.ts
+++ engine/src/entity/rds.ts
@@ -26,7 +26,6 @@ export enum StorageType {
 
 export type MonitoringIntervals = 0 | 1 | 5 | 10 | 15 | 30 | 60;
 
-// TODO complete instance schema
 @source(Source.DB)
 @Entity()
 export class RDS {
@@ -43,7 +42,7 @@ export class RDS {
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-rds/interfaces/createdbinstancecommandinput.html#dbinstanceidentifier
   @awsPrimaryKey
   @Column({
-    unique: true, 
+    unique: true,
   })
   dbInstanceIdentifier: string;
 
@@ -59,13 +58,12 @@ export class RDS {
   })
   autoMinorVersionUpgrade?: boolean;
 
-  // Only defined if MultiAZ is off
   @ManyToOne(() => AvailabilityZone, { eager: true, })
   @JoinColumn({
     name: 'availability_zone_id',
   })
   availabilityZone: AvailabilityZone;
-  
+
   // TODO: Add constraints
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-rds/interfaces/createdbinstancecommandinput.html#backupretentionperiod
   @Column({
@@ -138,7 +136,7 @@ export class RDS {
   })
   enableCustomerOwnedIp?: boolean;
 
-  
+
   @Column({
     nullable: true,
     default: false,
@@ -166,7 +164,7 @@ export class RDS {
     nullable: true,
   })
   iops?: number;
-  
+
   @Column({
     nullable: true,
   })
@@ -219,13 +217,12 @@ export class RDS {
     nullable: true,
   })
   ncharCharacterSetName?: string;
-  
+
   // TODO: Update relationship and create groups entity
   @Column({
     nullable: true,
   })
   optionGroupName?: string;
-
 
   @Column({
     nullable: true,
@@ -242,7 +239,7 @@ export class RDS {
   @Column({
     nullable: true,
   })
-  port?: number; 
+  port?: number;
 
   // TODO: add constraint?
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-rds/interfaces/createdbinstancecommandinput.html#preferredbackupwindow
@@ -300,13 +297,148 @@ export class RDS {
     nullable: true,
   })
   tdeCredentialPassword?: string;
-  
+
   @Column({
     nullable: true,
   })
   timezone?: string;
 
+  // TODO: DBSecurityGroupMembership instead of actual security group structure. probably create an intermediate table
   @ManyToMany(() => SecurityGroup, { eager: true, })
   @JoinTable()
   vpcSecurityGroups: SecurityGroup;
+
+  // TODO: enum all statuses?
+  // https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/accessing-monitoring.html#Overview.DBInstance.Status
+  @Column({
+    nullable: true,
+  })
+  dbInstanceStatus?: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  automaticRestartTime?: Date;
+
+  // TODO: update to endpoint structure and add FK
+  @Column({
+    nullable: true,
+  })
+  endpoint?: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  instanceCreateTime?: Date;
+
+  // TODO: update using subnet group entity
+  @Column({
+    nullable: true,
+  })
+  dbSubnetGroup?: string;
+
+  @Column({
+    type: 'timestamp with time zone',
+    nullable: true,
+  })
+  latestRestorableTime?: Date;
+
+  // Only defined for MultiAZ 
+  @ManyToOne(() => AvailabilityZone, { eager: true, })
+  @JoinColumn({
+    name: 'availability_zone_id',
+  })
+  secondaryAvailabilityZone: AvailabilityZone;
+
+  @Column({
+    nullable: true,
+  })
+  caCertificateIdentifier?: string;
+
+  @Column({
+    nullable: true,
+  })
+  dbInstanceArn?: string;
+
+  // TODO: update using DBInstanceRole entity
+  @Column({
+    nullable: true,
+  })
+  associatedRoles?: string;
+
+  // TODO: update once Endpoint entity is implemented
+  // SQL server only
+  @Column({
+    nullable: true,
+  })
+  listenerEndpoint?: string;
+
+  @Column({
+    nullable: true,
+  })
+  awsBackupRecoveryPointArn?: string;
+
+  // TODO: add enum
+  @Column({
+    nullable: true,
+  })
+  activityStreamStatus?: string;
+
+  // TODO: add enum
+  @Column({
+    nullable: true,
+  })
+  activityStreamMode?: string;
+
+  @Column({
+    nullable: true,
+  })
+  activityStreamKmsKeyId?: string;
+
+  @Column({
+    nullable: true,
+  })
+  activityStreamKinesisStreamName?: string;
+
+  @Column({
+    nullable: true,
+  })
+  activityStreamEngineNativeAuditFieldsIncluded?: boolean;
+
+  @Column({
+    nullable: true,
+  })
+  readReplicaSourceDBInstanceIdentifier?: string;
+
+  // TODO: create sub entity with many relationship
+  @Column({
+    nullable: true,
+  })
+  readReplicaDBInstanceIdentifiers?: string;
+
+  // TODO: create sub entity with many relationship
+  @Column({
+    nullable: true,
+  })
+  readReplicaDBClusterIdentifiers?: string;
+
+  // TODO: add enum
+  @Column({
+    nullable: true,
+  })
+  replicaMode?: string;
+
+  // TODO: create sub entity with many relationship
+  @Column({
+    nullable: true,
+  })
+  statusInfos?: string;
+
+  // TODO: create sub entity with many relationship
+  @Column({
+    nullable: true,
+  })
+  dbInstanceAutomatedBackupsReplications?: string;
 }
