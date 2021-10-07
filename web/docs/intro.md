7509fa5eb2728d7cf9e3613b009171067467295a
luifer <lfdepombo@gmail.com>
2021-10-07 12:58:50 -0400

    Basic website under beta.iasql.com (#55)

---
+++ web/docs/intro.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# Tutorial Intro
+
+Let's discover **IaSQL in less than 5 minutes**.
+
+## Getting Started
+
+Get started by providing AWS credentials
---
+++ web/docusaurus.config.js
@@ -0,0 +1,108 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'IaSQL',
+  tagline: 'AWS Infrastructure as SQL',
+  url: 'https://beta.iasql.com',
+  baseUrl: '/',
+  trailingSlash: true,
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'alantech', // Usually your GitHub org/user name.
+  projectName: 'iasql', // Usually your repo name.
+
+  customFields: {
+    auth: {
+      domain: "auth.iasql.com",
+      clientId: "OLQAngfr1LnenTt6wmQOYKmzx1c1dSxg",
+      redirectUri: 'https://beta.iasql.com'
+    },
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'IaSQL',
+        logo: {
+          alt: 'logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'right',
+            label: 'Docs',
+          },
+          {to: '/blog', label: 'Blog', position: 'right'},
+          {to: '/schema', label: 'Schema', position: 'right'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Reddit',
+                href: 'https://reddit.com/r/iasql',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/sqlcloudinfra',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'Schema',
+                to: '/schema',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
