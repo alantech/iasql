cdd4dc98924402565f0d337504f68908d811f5f4
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-14 12:01:40 +0200

    Remove tmp route

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,24 +1,9 @@
 import * as express from 'express'
-import config from '../config';
-import { AWS } from '../services/gateways/aws';
 
 import { db, } from './db'
 
 const v1 = express.Router();
 
 v1.use('/db', db)
-
-v1.get('/aws', async (req, res) => {
-  const awsClient = new AWS({
-    region: config.region ?? 'eu-west-1',
-    credentials: {
-      accessKeyId: config.accessKeyId ?? '',
-      secretAccessKey: config.secretAccessKey ?? '',
-    },
-  });
-  // const ord = await awsClient.getOrdInsOpt();
-  const ord = await awsClient.getEngineVersions();
-  res.end(JSON.stringify(ord))
-})
 
 export { v1 };
