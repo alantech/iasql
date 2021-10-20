7b8281397a09b8eef2f42860c5ddf24f0813b50f
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-20 19:37:51 +0200

    Add noDiff decorator for unnecessary props

--- engine/src/entity/engine_version.ts
+++ engine/src/entity/engine_version.ts
@@ -1,6 +1,7 @@
 import { Entity, PrimaryGeneratedColumn, Column, JoinColumn, JoinTable, ManyToMany, ManyToOne, } from 'typeorm';
 import { UpgradeTarget } from '.';
 import { awsPrimaryKey } from '../services/aws-primary-key';
+import { noDiff } from '../services/diff';
 import { CharacterSet } from './character_set';
 import { ExportableLogType } from './exportable_log_type';
 import { FeatureName } from './feature_name';
@@ -49,32 +50,39 @@ export class EngineVersion {
   @JoinTable()
   validUpgradeTargets: UpgradeTarget[];
 
+  @noDiff
   @ManyToMany(() => ExportableLogType, { cascade: true, eager: true, })
   @JoinTable()
   exportableLogTypes: ExportableLogType[];
 
+  @noDiff
   @ManyToMany(() => SupportedEngineMode, { cascade: true, eager: true, })
   @JoinTable()
   supportedEngineModes: SupportedEngineMode[];
 
+  @noDiff
   @ManyToOne(() => CharacterSet, { cascade: true, eager: true, })
   @JoinColumn({
     name: 'character_set_id'
   })
   defaultCharacterSet: CharacterSet;
 
+  @noDiff
   @ManyToMany(() => CharacterSet, { cascade: true, eager: true, })
   @JoinTable()
   supportedCharacterSets: CharacterSet[];
 
+  @noDiff
   @ManyToMany(() => CharacterSet, { cascade: true, eager: true, })
   @JoinTable()
   supportedNcharCharacterSets: CharacterSet[];
 
+  @noDiff
   @ManyToMany(() => FeatureName, { cascade: true, eager: true, })
   @JoinTable()
   supportedFeatureNames: FeatureName[];
 
+  @noDiff
   @ManyToMany(() => Timezone, { cascade: true, eager: true, })
   @JoinTable()
   supportedTimezones: Timezone[];
