e12218c3b44a5d505e0f45436895c6ccc90fd638
David Ellis <isv.damocles@gmail.com>
2021-10-25 15:31:40 -0500

    Implement remove as a mostly-inverse of install

--- engine/src/router/module.ts
+++ engine/src/router/module.ts
@@ -169,13 +169,82 @@ ${Object.keys(tableCollisions)
 });
 
 // Needed at the beginning
-mod.post('/remove', (req, res) => {
-  if (Array.isArray(req.body.list)) {
-    // TODO
-    res.end(JSON.stringify("TODO", undefined, '  '));
-  } else {
-    res.end(JSON.stringify("ERROR", undefined, '  '));
+mod.post('/remove', async (req, res) => {
+  // TODO: Add security to all of these endpoints
+  // Don't do anything if we don't know what database to impact
+  if (!req.body.dbname) return res.json("Missing 'dbname' to install into");
+  // Also don't do anything if we don't have any list of modules to install
+  if (!Array.isArray(req.body.list)) return res.json("ERROR: No packages provided in 'list' property");
+  // Check to make sure that all specified modules actually exist
+  const modules = req.body.list.map((n: string) => Object.values(Modules).find(m => m.name === n)) as Modules.ModuleInterface[];
+  if (modules.some((m: any) => m === undefined)) {
+    return res.json(`ERROR. The following modules do not exist: ${
+      req.body.list.filter((n: string) => !Object.values(Modules).find(m => m.name === n)).join(' , ')
+    }`);
   }
+  // Grab all of the entities from the module plus the IaSQL Module entity itself and create the
+  // TypeORM connection with it.
+  const entities = modules.map((m: any) => m.mappers.map((ma: any) => ma.entity)).flat();
+  entities.push(IasqlModule);
+  const orm = await TypeormWrapper.createConn(req.body.dbname, {
+    name: req.body.dbname,
+    type: 'postgres',
+    username: 'postgres', // TODO: Should we use the user's account for this?
+    password: 'test',
+    host: 'postgresql',
+    entities,
+    namingStrategy: new SnakeNamingStrategy(), // TODO: Do we allow modules to change this?
+  });
+  const queryRunner = orm.createQueryRunner();
+  await queryRunner.connect();
+  // See what modules are already removed and prune them from the list
+  const existingModules = (await orm.find(IasqlModule)).map((m: IasqlModule) => m.name);
+  for (let i = 0; i < modules.length; i++) {
+    if (!existingModules.includes(modules[i].name)) {
+      modules.splice(i, 1);
+      i--;
+    }
+  }
+  // See if we need to abort because now there's nothing to do
+  if (modules.length === 0) {
+    return res.json("All modules already removed. Aborting");
+  }
+  // Sort the modules based on their dependencies, with both root-to-leaf order and vice-versa
+  const rootToLeafOrder = [...modules].sort((a, b) => {
+    // Assuming no dependency loops
+    if (a.dependencies.includes(b.name)) return 1;
+    if (b.dependencies.includes(a.name)) return -1;
+    return 0;
+  });
+  const leafToRootOrder = [...rootToLeafOrder].reverse();
+  // Actually run the removal. First running all of the preremove scripts from leaf-to-root, then
+  // all of the postremove scripts from root-to-leaf. Wrapped in a transaction so any failure at
+  // this point when we're actually mutating the database doesn't leave things in a busted state.
+  await queryRunner.startTransaction();
+  try {
+    for (let mod of leafToRootOrder) {
+      if (mod.migrations?.preinstall) {
+        await mod.migrations.preinstall(queryRunner);
+      }
+    }
+    for (let mod of rootToLeafOrder) {
+      if (mod.migrations?.preremove) {
+        await mod.migrations.preremove(queryRunner);
+      }
+      if (mod.migrations?.postremove) {
+        await mod.migrations.postremove(queryRunner);
+      }
+      const e = await orm.findOne(IasqlModule, { name: mod.name, });
+      await orm.remove(IasqlModule, e);
+    }
+    await queryRunner.commitTransaction();
+  } catch (e) {
+    await queryRunner.rollbackTransaction();
+    return res.json(`Error: ${(e as any).message}`);
+  } finally {
+    await queryRunner.release();
+  }
+  res.json("Done!");
 });
 
 // Needed before first beta
