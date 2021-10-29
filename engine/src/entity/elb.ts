e7ce7085f1bc651cbaa91023dbf99a070846f057
Alejandro Guillen <aguillenv93@gmail.com>
2021-10-29 17:27:27 +0200

    Add elb no diff and az to force it existence

--- engine/src/entity/elb.ts
+++ engine/src/entity/elb.ts
@@ -44,42 +44,50 @@ export class ELB {
   id: number;
 
   @awsPrimaryKey
+  @noDiff
   @Column({
     nullable: true,
   })
   loadBalancerArn?: string;
 
+  @noDiff
   @Column({
     nullable: true,
   })
   dnsName?: string;
 
+  @noDiff
   @Column({
     nullable: true,
   })
   canonicalHostedZoneId?: string;
 
+  @noDiff
   @Column({
     nullable: true,
     type: 'timestamp with time zone'
   })
   createdTime?: Date;
 
+  @noDiff
   @Column({
     unique: true,
   })
   loadBalancerName: string;
 
+  @noDiff
   @Column({
     type: 'enum',
     enum: LoadBalancerSchemeEnum,
   })
   scheme: LoadBalancerSchemeEnum;
 
+  @noDiff
   @ManyToOne(() => Vpc, { eager: true, })
   @JoinColumn({ name: 'vpc_id' })
   vpc: Vpc;
 
+  @noDiff
   @Column({
     nullable: true,
     type: 'enum',
@@ -87,30 +95,44 @@ export class ELB {
   })
   state?: LoadBalancerStateEnum;
 
+  @noDiff
   @Column({
     type: 'enum',
     enum: LoadBalancerTypeEnum,
   })
   elbType: LoadBalancerTypeEnum;
 
+  // Not in the mapper since is just needes as input for the creation and teh retrieve endpoint
+  // do not return any information related to the subnets
+  @noDiff
   @ManyToMany(() => Subnet, { eager: true, })
   @JoinTable()
   subnets?: Subnet[];
 
+  @noDiff
   @ManyToMany(() => AvailabilityZone, { eager: true, })
   @JoinTable()
   availabilityZones?: AvailabilityZone[];
 
+  // TODO: work around for now to force lazy laoder fail and create the join table relations :'(
+  @noDiff
+  @ManyToOne(() => AvailabilityZone, { eager: true, })
+  @JoinColumn({ name: 'availability_zone_id', })
+  availabilityZone?: AvailabilityZone;
+
+  @noDiff
   @ManyToMany(() => SecurityGroup, { eager: true, })
   @JoinTable()
   securityGroups?: SecurityGroup[];
 
+  @noDiff
   @Column({
     type: 'enum',
     enum: IpAddressType,
   })
   ipAddressType: IpAddressType;
 
+  @noDiff
   @Column({
     nullable: true,
   })
