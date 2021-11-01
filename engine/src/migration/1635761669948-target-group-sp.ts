1cbdd969ce30b72da5dd3a14a4742e607fb9dac3
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-01 19:13:34 +0100

    Add create target group function (#99)

---
+++ engine/src/migration/1635761669948-target-group-sp.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class targetGroupSp1635761669948 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Example of use: select * from create_target_group('test-sp', 'ip', 8888, 'vpc-41895538', 'TCP');
+    await queryRunner.query(`
+      create or replace function create_target_group(
+        _name text,
+        _target_type target_group_target_type_enum,
+        _port integer,
+        _vpc_id text,
+        _protocol target_group_protocol_enum
+      ) returns integer as $$ 
+        declare 
+          tg_vpc_id integer;
+          target_group_id integer;
+        begin
+        select id into tg_vpc_id
+        from vpc
+        where
+          vpc_id = _vpc_id
+        order by id desc
+        limit 1;
+      
+        insert into target_group
+          (
+            target_group_name,
+            target_type,
+            protocol,
+            port,
+            vpc_id
+          )
+        values
+          (
+            _name,
+            _target_type,
+            _protocol,
+            _port,
+            tg_vpc_id
+          );
+      
+        select id into target_group_id
+        from target_group
+        order by id desc
+        limit 1;
+      
+        return target_group_id;
+        end; $$ language plpgsql;
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP function create_target_group;`);
+  }
+
+}
