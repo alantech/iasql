35414a6e07414fb42b02823bd8f24cd13d23175b
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-01 17:32:55 +0100

    Add route to setup isolated IaSQL pieces

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,24 +1,11 @@
 import * as express from 'express'
-import { AWS } from '../services/gateways/aws';
 
 import { db, } from './db'
+import { iasql, } from './iasql'
 
 const v1 = express.Router();
 
 v1.use('/db', db)
-
-db.post('/aws', async (req, res) => {
-  const t1 = Date.now();
-  const {dbAlias, awsRegion, awsAccessKeyId, awsSecretAccessKey} = req.body;
-  const awsClient = new AWS({
-    region: awsRegion,
-    credentials: {
-      accessKeyId: awsAccessKeyId,
-      secretAccessKey: awsSecretAccessKey,
-    },
-  });
-  const subnets = await awsClient.getSubnets();
-  res.end(`ok = ${JSON.stringify(subnets)}`)
-})
+v1.use('/iasql', iasql)
 
 export { v1 };
