7a53453bfc533d75ae6340780123d23e4fb8e87f
David Ellis <isv.damocles@gmail.com>
2021-11-04 15:27:38 -0500

    Centralized memoization working as desired :D

--- engine/src/modules/interfaces.ts
+++ engine/src/modules/interfaces.ts
@@ -15,7 +15,7 @@ export class Crud<E> {
   updateFn: (e: E | E[], ctx: Context) => Promise<void | E | E[]>;
   deleteFn: (e: E | E[], ctx: Context) => Promise<void | E | E[]>;
   dest?: 'db' | 'cloud';
-  entityName?: string;
+  entity?: { new (): E };
   entityId?: (e: E) => string;
   
   constructor(def: CrudInterface<E>) {
@@ -29,30 +29,72 @@ export class Crud<E> {
     if (!e) return;
     const es = Array.isArray(e) ? e : [e];
     const dest = this.dest ?? 'What?';
-    const entityName = this.entityName ?? 'What?';
+    const entityName = this.entity?.name ?? 'What?';
+    const entityId = this.entityId ?? function (_e: E) { return 'What?'; };
+    es.forEach((e, i) => {
+      ctx.memo[dest] = ctx.memo[dest] ?? {};
+      ctx.memo[dest][entityName] = ctx.memo[dest][entityName] ?? {};
+      if (!ctx.memo[dest][entityName][entityId(e)]) {
+        ctx.memo[dest][entityName][entityId(e)] = e;
+      } else {
+        // Transfer the properties from the entity to the one already memoized so other references
+        // to the same entity also get updated, then update the output array
+        const realE = ctx.memo[dest][entityName][entityId(e)];
+        console.log({
+          realE,
+          e,
+        });
+        Object.keys(e).forEach(k => realE[k] = (e as any)[k]);
+        es[i] = realE;
+      }
+    });
+    if (Array.isArray(e)) {
+      return e;
+    } else {
+      // To return the possibly-changed entity instead of the original input one
+      return es[0];
+    }
+  }
+
+  unmemo(e: void | E | E[], ctx: Context) {
+    if (!e) return;
+    const es = Array.isArray(e) ? e : [e];
+    const dest = this.dest ?? 'What?';
+    const entityName = this.entity?.name ?? 'What?';
     const entityId = this.entityId ?? function (_e: E) { return 'What?'; };
     es.forEach(e => {
       ctx.memo[dest] = ctx.memo[dest] ?? {};
       ctx.memo[dest][entityName] = ctx.memo[dest][entityName] ?? {};
-      ctx.memo[dest][entityName][entityId(e)] = e;
+      delete ctx.memo[dest][entityName][entityId(e)];
     });
-    return e;
   }
 
   // TODO: Add memoization of everything
   async create(e: E | E[], ctx: Context) {
+    // Memoize before and after the actual logic to make sure the unique ID is reserved
+    this.memo(e, ctx);
     return this.memo(await this.createFn(e, ctx), ctx);
   }
 
   async read(ctx: Context, id?: string | string[]) {
+    console.dir({
+      ctx,
+      id,
+    }, { depth: 4, });
     if (id) {
       const dest = this.dest ?? 'What?';
-      const entityName = this.entityName ?? 'What?';
+      const entityName = this.entity?.name ?? 'What?';
       if (Array.isArray(id)) {
         const missing: string[] = [];
         const vals = id.map(i => {
-          const val = ctx.memo?.[dest]?.[entityName]?.[i];
+          const val = ctx.memo[dest]?.[entityName]?.[i];
           if (!val) {
+            // We create a placeholder value to put here so recursive calls will resolve an object
+            // and we will rely on later code to stitch things back together to make sure circular
+            // references are fine
+            ctx.memo[dest] = ctx.memo[dest] ?? {};
+            ctx.memo[dest][entityName] = ctx.memo[dest][entityName] ?? {};
+            ctx.memo[dest][entityName][i] = new (this.entity as { new (): E, })();
             missing.push(i);
             return i;
           } else {
@@ -64,23 +106,37 @@ export class Crud<E> {
         // The order is the same in both lists, so we can cheat and do a single pass
         for (let i = 0, j = 0; i < vals.length; i++) {
           if (vals[i] === missing[j]) {
-            vals[i] = missingVals[j];
+            const realE = ctx.memo[dest][entityName][vals[i]];
+            Object.keys(missingVals[j]).forEach(k => realE[k] = (missingVals[j] as any)[k]);
+            vals[i] = realE;
             j++;
           }
         }
         return vals;
       } else {
-        return ctx.memo?.[dest]?.[entityName]?.[id] ?? this.memo(await this.readFn(ctx, id), ctx);
+        // Possibly store an empty entity in the memoization before the call is done to make sure
+        // circular loops don't really happen (a later call for the same ID will eject earlier)
+        ctx.memo[dest] = ctx.memo[dest] ?? {};
+        ctx.memo[dest][entityName] = ctx.memo[dest][entityName] ?? {};
+        if (!ctx.memo[dest][entityName][id]) {
+          ctx.memo[dest][entityName][id] = new (this.entity as { new (): E})();
+        } else {
+          return ctx.memo[dest][entityName][id];
+        }
+        return this.memo(await this.readFn(ctx, id), ctx);
       }
     }
     return this.memo(await this.readFn(ctx, id), ctx);
   }
 
   async update(e: E | E[], ctx: Context) {
+    // Memoize before and after the actual logic to make sure the unique ID is reserved
+    this.memo(e, ctx);
     return this.memo(await this.updateFn(e, ctx), ctx);
   }
 
   async delete(e: E | E[], ctx: Context) {
+    this.unmemo(e, ctx); // Remove deleted record(s) from the memo
     return await this.deleteFn(e, ctx);
   }
 }
@@ -108,11 +164,11 @@ export class Mapper<E> {
     this.equals = def.equals;
     this.source = def.source;
     this.db = def.db;
-    this.db.entityName = def.entity.name;
+    this.db.entity = def.entity;
     this.db.entityId = def.entityId;
     this.db.dest = 'db';
     this.cloud = def.cloud;
-    this.cloud.entityName = def.entity.name;
+    this.cloud.entity = def.entity;
     this.cloud.entityId = def.entityId;
     this.cloud.dest = 'cloud';
   }
