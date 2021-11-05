d804c918d2d7e2980b2de906ca4e0a5bc785138f
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-05 18:00:51 +0100

    Fix indent

--- engine/src/mapper/action.ts
+++ engine/src/mapper/action.ts
@@ -17,19 +17,17 @@ export const ActionMapper = new EntityMapper(Action, {
       return null;
     }
   },
-},
-  {
-    readAWS: async (_awsClient: AWS, _indexes: IndexedAWS) => {
-      return;
-    },
-    createAWS: async (_obj: TargetGroup, _awsClient: AWS, _indexes: IndexedAWS) => {
-      throw new Error('tbd');
-    },
-    updateAWS: async (obj: TargetGroup, awsClient: AWS, indexes: IndexedAWS) => {
-      throw new Error('tbd');
-    },
-    deleteAWS: async (_obj: TargetGroup, _awsClient: AWS, _indexes: IndexedAWS) => {
-      throw new Error('tbd');
-    },
-  }
-)
+}, {
+  readAWS: async (_awsClient: AWS, _indexes: IndexedAWS) => {
+    return;
+  },
+  createAWS: async (_obj: TargetGroup, _awsClient: AWS, _indexes: IndexedAWS) => {
+    throw new Error('tbd');
+  },
+  updateAWS: async (obj: TargetGroup, awsClient: AWS, indexes: IndexedAWS) => {
+    throw new Error('tbd');
+  },
+  deleteAWS: async (_obj: TargetGroup, _awsClient: AWS, _indexes: IndexedAWS) => {
+    throw new Error('tbd');
+  },
+})
--- engine/src/mapper/listener.ts
+++ engine/src/mapper/listener.ts
@@ -23,44 +23,42 @@ export const ListenerMapper = new EntityMapper(Listener, {
       return [];
     }
   },
-},
-  {
-    readAWS: async (awsClient: AWS, indexes: IndexedAWS) => {
-      const t1 = Date.now();
-      const loadBalancers = indexes.get(ELB);
-      if (!loadBalancers) throw new DepError('ELBs must be loaded first');
-      const loadBalancerArns = Object.keys(loadBalancers);
-      const listeners = (await awsClient.getListeners(loadBalancerArns))?.Listeners ?? [];
-      indexes.setAll(Listener, listeners, 'ListenerArn');
-      const t2 = Date.now();
-      console.log(`Listeners set in ${t2 - t1}ms`);
-    },
-    createAWS: async (l: Listener, awsClient: AWS, indexes: IndexedAWS) => {
-      const result = await awsClient.createListener({
-        Port: l.port,
-        Protocol: l.protocol,
-        LoadBalancerArn: l.elb?.loadBalancerArn,
-        DefaultActions: l.defaultActions?.map(a => ({ Type: a.actionType, TargetGroupArn: a.targetGroup.targetGroupArn })),
-      });
-      if (!result?.hasOwnProperty('ListenerArn')) { // Failure
-        throw new Error('what should we do here?');
-      }
-      const newInstance = await indexes.getOr(Listener, result.ListenerArn ?? '', awsClient.getListener.bind(awsClient));
-      const newEntity: Listener = await ListenerMapper.fromAWS(newInstance, awsClient, indexes);
-      newEntity.id = l.id;
-      for (const key of Object.keys(newEntity)) {
-        EntityMapper.keepId((l as any)[key], (newEntity as any)[key]);
-        (l as any)[key] = (newEntity as any)[key];
-      }
-      return newEntity;
-    },
-    updateAWS: async (_obj: Listener, _awsClient: AWS, _indexes: IndexedAWS) => {
-      throw new Error('tbd');
-    },
-    deleteAWS: async (l: Listener, awsClient: AWS, indexes: IndexedAWS) => {
-      await awsClient.deleteListener(l.listenerArn!);
-      indexes.del(Listener, l.listenerArn!);
-      return l;
-    },
-  }
-)
+}, {
+  readAWS: async (awsClient: AWS, indexes: IndexedAWS) => {
+    const t1 = Date.now();
+    const loadBalancers = indexes.get(ELB);
+    if (!loadBalancers) throw new DepError('ELBs must be loaded first');
+    const loadBalancerArns = Object.keys(loadBalancers);
+    const listeners = (await awsClient.getListeners(loadBalancerArns))?.Listeners ?? [];
+    indexes.setAll(Listener, listeners, 'ListenerArn');
+    const t2 = Date.now();
+    console.log(`Listeners set in ${t2 - t1}ms`);
+  },
+  createAWS: async (l: Listener, awsClient: AWS, indexes: IndexedAWS) => {
+    const result = await awsClient.createListener({
+      Port: l.port,
+      Protocol: l.protocol,
+      LoadBalancerArn: l.elb?.loadBalancerArn,
+      DefaultActions: l.defaultActions?.map(a => ({ Type: a.actionType, TargetGroupArn: a.targetGroup.targetGroupArn })),
+    });
+    if (!result?.hasOwnProperty('ListenerArn')) { // Failure
+      throw new Error('what should we do here?');
+    }
+    const newInstance = await indexes.getOr(Listener, result.ListenerArn ?? '', awsClient.getListener.bind(awsClient));
+    const newEntity: Listener = await ListenerMapper.fromAWS(newInstance, awsClient, indexes);
+    newEntity.id = l.id;
+    for (const key of Object.keys(newEntity)) {
+      EntityMapper.keepId((l as any)[key], (newEntity as any)[key]);
+      (l as any)[key] = (newEntity as any)[key];
+    }
+    return newEntity;
+  },
+  updateAWS: async (_obj: Listener, _awsClient: AWS, _indexes: IndexedAWS) => {
+    throw new Error('tbd');
+  },
+  deleteAWS: async (l: Listener, awsClient: AWS, indexes: IndexedAWS) => {
+    await awsClient.deleteListener(l.listenerArn!);
+    indexes.del(Listener, l.listenerArn!);
+    return l;
+  },
+})
