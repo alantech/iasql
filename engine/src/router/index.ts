07f877c5123db3650fcadff56825d141e71c3e2c
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-08 11:14:30 +0100

    Restore previous router

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,11 +1,24 @@
 import * as express from 'express'
+ import { AWS } from '../services/gateways/aws';
 
-import { db, } from './db'
-import { iasql, } from './iasql'
+ import { db, } from './db'
 
-const v1 = express.Router();
+ const v1 = express.Router();
 
-v1.use('/db', db)
-v1.use('/iasql', iasql)
+ v1.use('/db', db)
 
-export { v1 };
+ db.post('/aws', async (req, res) => {
+   const t1 = Date.now();
+   const {dbAlias, awsRegion, awsAccessKeyId, awsSecretAccessKey} = req.body;
+   const awsClient = new AWS({
+     region: awsRegion,
+     credentials: {
+       accessKeyId: awsAccessKeyId,
+       secretAccessKey: awsSecretAccessKey,
+     },
+   });
+   const subnets = await awsClient.getSubnets();
+   res.end(`ok = ${JSON.stringify(subnets)}`)
+ })
+
+ export { v1 };
