b375b1f1ab250a0148648ea78113fc5055241a67
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-08 11:14:56 +0100

    Fix indent

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,24 +1,24 @@
 import * as express from 'express'
- import { AWS } from '../services/gateways/aws';
+import { AWS } from '../services/gateways/aws';
 
- import { db, } from './db'
+import { db, } from './db'
 
- const v1 = express.Router();
+const v1 = express.Router();
 
- v1.use('/db', db)
+v1.use('/db', db)
 
- db.post('/aws', async (req, res) => {
-   const t1 = Date.now();
-   const {dbAlias, awsRegion, awsAccessKeyId, awsSecretAccessKey} = req.body;
-   const awsClient = new AWS({
-     region: awsRegion,
-     credentials: {
-       accessKeyId: awsAccessKeyId,
-       secretAccessKey: awsSecretAccessKey,
-     },
-   });
-   const subnets = await awsClient.getSubnets();
-   res.end(`ok = ${JSON.stringify(subnets)}`)
- })
+db.post('/aws', async (req, res) => {
+  const t1 = Date.now();
+  const { dbAlias, awsRegion, awsAccessKeyId, awsSecretAccessKey } = req.body;
+  const awsClient = new AWS({
+    region: awsRegion,
+    credentials: {
+      accessKeyId: awsAccessKeyId,
+      secretAccessKey: awsSecretAccessKey,
+    },
+  });
+  const subnets = await awsClient.getSubnets();
+  res.end(`ok = ${JSON.stringify(subnets)}`)
+})
 
- export { v1 };
+export { v1 };
