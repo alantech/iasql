6fd57927241a27db353b0efbd5ee9fe70c052dfa
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-12 17:24:53 +0000

    Remove iasql endpoints

--- engine/src/router/index.ts
+++ engine/src/router/index.ts
@@ -1,11 +1,9 @@
 import * as express from 'express'
 
 import { db, } from './db'
-import { iasql, } from './iasql'
 
 const v1 = express.Router();
 
 v1.use('/db', db)
-v1.use('/iasql', iasql)
 
 export { v1 };
