4ce1ef4065a61a5080b7eb8e2e7d2174ec9954e3
Alejandro Guillen <aguillenv93@gmail.com>
2021-11-12 09:45:34 +0000

    Update create_target_group to store procedure

--- engine/src/migration/1636655461356-target-group-sp.ts
+++ engine/src/migration/1636655461356-target-group-sp.ts
@@ -3,57 +3,62 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class targetGroupSp1636655461356 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    // Example of use: select * from create_target_group('test-sp', 'ip', 8888, 'vpc-41895538', 'TCP');
+    await queryRunner.query(`drop function create_target_group;`);
+    // Example of use: call create_target_group('test-sp', 'ip', 8888, 'vpc-41895538', 'TCP');
     await queryRunner.query(`
-      create or replace function create_target_group(
+      create or replace procedure create_target_group(
         _name text,
         _target_type target_group_target_type_enum,
         _port integer,
         _vpc_id text,
         _protocol target_group_protocol_enum
-      ) returns integer as $$ 
+      )
+      language plpgsql
+      as $$
         declare 
           tg_vpc_id integer;
           target_group_id integer;
         begin
-        select id into tg_vpc_id
-        from vpc
-        where
-          vpc_id = _vpc_id
-        order by id desc
-        limit 1;
+          select id into tg_vpc_id
+          from vpc
+          where
+            vpc_id = _vpc_id
+          order by id desc
+          limit 1;
       
-        insert into target_group
-          (
-            target_group_name,
-            target_type,
-            protocol,
-            port,
-            vpc_id
-          )
-        values
-          (
-            _name,
-            _target_type,
-            _protocol,
-            _port,
-            tg_vpc_id
-          )
-        on conflict (target_group_name)
-        do nothing;
+          insert into target_group
+            (
+              target_group_name,
+              target_type,
+              protocol,
+              port,
+              vpc_id
+            )
+          values
+            (
+              _name,
+              _target_type,
+              _protocol,
+              _port,
+              tg_vpc_id
+            )
+          on conflict (target_group_name)
+          do nothing;
       
-        select id into target_group_id
-        from target_group
-        where target_group_name = _name
-        order by id desc
-        limit 1;
+          select id into target_group_id
+          from target_group
+          where target_group_name = _name
+          order by id desc
+          limit 1;
       
-        return target_group_id;
-        end; $$ language plpgsql;
+          raise info 'target_group_id = %', target_group_id;
+        end;
+      $$;
     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`drop procedure create_target_group;`);
     // Example of use: select * from create_target_group('test-sp', 'ip', 8888, 'vpc-41895538', 'TCP');
     await queryRunner.query(`
       create or replace function create_target_group(
