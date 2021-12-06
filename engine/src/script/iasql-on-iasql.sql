d166b00fab9fb35e42b3ffeee0cc3acfd46a565a
Alejandro Guillen <aguillenv93@gmail.com>
2021-12-06 16:18:30 +0100

    Update iasql script

--- engine/src/script/iasql-on-iasql.sql
+++ engine/src/script/iasql-on-iasql.sql
@@ -35,6 +35,9 @@ do $$
     iasql_postgres_rds_db_username text := 'iasql';
     iasql_postgres_rds_db_password text := '4l4nU$3r';
     iasql_postgres_rds_db_az text := 'us-east-2a';
+    iasql_db_host text := 'db.iasql.com';
+    iasql_db_user text := 'iasql';
+    iasql_db_password text;  -- Do not commit db password value
   begin
     select vpc_id, id into default_vpc, default_vpc_id
     from aws_vpc
@@ -71,7 +74,7 @@ do $$
 
     call create_container_definition(
       iasql_engine_container, true, iasql_engine_container_memory_reservation, iasql_engine_port, iasql_engine_port, 'tcp',
-      ('{"PORT": ' || iasql_engine_port || '}')::json, iasql_engine_image_tag,
+      ('{"PORT": ' || iasql_engine_port || ', "DB_HOST": ' || iasql_db_host || ', "DB_USER": ' || iasql_db_user || '}')::json, iasql_engine_image_tag,
       _ecr_repository_name := iasql_engine_repository
     );
 
