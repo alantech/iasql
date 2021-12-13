0b52e9733f6c2743e453a1ed532e57d530808fec
Alejandro Guillen <aguillenv93@gmail.com>
2021-12-13 15:54:10 +0000

    Fix script and add deploy env (#251)

--- engine/deploy.sh
+++ engine/deploy.sh
@@ -1,38 +1,42 @@
 ## Prerequisites:
 ## - Engine running locally
 ## - Added a db called iasql using organization credentials
-## - PGPASSWORD environment variable defined
-## - DB_PASSWORD environment variable defined
-## - IRONPLANS_TOKEN environment variable defined
+## - PGPASSWORD environment variable defined in .deploy-env
+## - DB_PASSWORD environment variable defined in .deploy-env
+## - IRONPLANS_TOKEN environment variable defined in .deploy-env
 #!/bin/bash
 
 # Fail on error
 set -e
 
 # Login. Review your profile
-echo "Docker login..."
+echo "\nDocker login..."
 aws ecr get-login-password --region us-east-2 --profile iasql | docker login --username AWS --password-stdin 547931376551.dkr.ecr.us-east-2.amazonaws.com
 
 # Build
-echo "Building image..."
+echo "\nBuilding image..."
 docker build -t iasql-engine-repository .
 
 # Tag
-echo "Tagging image..."
+echo "\nTagging image..."
 docker tag iasql-engine-repository:latest 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository:latest
 
 # Push
-echo "Pushing image..."
+echo "\nPushing image..."
 docker push 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository:latest
 
 # Prepare iasql-on-iasql.sql script
-echo "Preparing iasql script..."
-sed "s/<DB_PASSWORD>/${DB_PASSWORD}/g;s/<IRONPLANS_TOKEN>/${IRONPLANS_TOKEN}/g" iasql-on-iasql.sql > iasql-on-iasql.out.sql
+echo "\nPreparing iasql script..."
+export $(cat .deploy-env | xargs) && sed "s/<DB_PASSWORD>/${DB_PASSWORD}/g;s/<IRONPLANS_TOKEN>/${IRONPLANS_TOKEN}/g" ./src/script/iasql-on-iasql.sql > ./src/script/iasql-on-iasql.out.sql
 
 # Update service. Set PGPASSWORD environment variable to avoid interaction
-echo "Updating iasql db..."
+echo "\nUpdating iasql db..."
 psql -h localhost -p 5432 -U postgres -d iasql -f ./src/script/iasql-on-iasql.out.sql
 
-# IaSQL db apply
-echo "Applying changes from iasql db..."
+# IaSQL db apply. Using local debug version
+echo "\nApplying changes from iasql db..."
 cargo run --manifest-path=../cli/Cargo.toml -- db apply iasql
+
+# Clean and leave just the last image
+echo "\nCleaning docker images..."
+docker rmi $(docker images -q 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository | tail -n +2)
