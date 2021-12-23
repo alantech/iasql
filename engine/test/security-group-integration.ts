db1bcb8ccae60d5beb4bf6f92f2551e3a66a8caa
David Ellis <isv.damocles@gmail.com>
2021-12-23 11:55:37 -0600

    Increase security group integration testing coverage

--- engine/test/security-group-integration.ts
+++ engine/test/security-group-integration.ts
@@ -14,68 +14,90 @@ afterAll(() => {
   execSync('cd test && docker-compose down');
 });
 
+function finish(done: (e?: any) => {}) {
+  return [() => done(), (e: any) => { done(e); }];
+}
+
+function runApply(done: (e?: any) => {}) {
+  iasql.apply('sgtest', false, 'not-needed').then(...finish(done));
+}
+
+function query(queryString: string) {
+  return function (done: (e?: any) => {}) {
+    createConnection({
+      name: 'sgtest',
+      type: 'postgres',
+      username: 'postgres',
+      password: 'test',
+      host: 'localhost',
+      port: 5432,
+      database: 'sgtest',
+      extra: { ssl: false, },
+    }).then((conn) => {
+      conn.query(queryString).then(() => {
+        conn.close().then(...finish(done));
+      }, (e) => {
+        conn.close().then(() => done(e), (e2) => done(e2));
+      });
+    }, done);
+  }
+}
+
 describe('Security Group Integration Testing', () => {
   it('creates a new test db', (done) => void iasql.add(
     'sgtest',
     'us-west-2',
     process.env.AWS_ACCESS_KEY_ID ?? 'barf',
     process.env.AWS_SECRET_ACCESS_KEY ?? 'barf',
-    'not-needed').then(() => done(), (e) => { done(e); }));
+    'not-needed').then(...finish(done)));
 
   it('installs the security group module', (done) => void iasql.install(
     ['aws_security_group'],
     'sgtest',
-    'not-needed').then(() => done(), (e) => { done(e); }));
-  
-  it('adds a new security group', (done) => {
-    createConnection({
-      name: 'sgtest',
-      type: 'postgres',
-      username: 'postgres',
-      password: 'test',
-      host: 'localhost',
-      port: 5432,
-      database: 'sgtest',
-      extra: { ssl: false, },
-    }).then((conn) => {
-      conn.query(`
-        INSERT INTO aws_security_group (description, group_name)
-        VALUES ('Security Group Test', 'sgtest');
-      `).then(() => {
-        conn.close().then(() => done(), (e) => { done(e); });
-      }, (e) => { done(e); });
-    }, (e) => { done(e); });
-  });
+    'not-needed').then(...finish(done)));
 
-  it('applies the security group change', (done) => void iasql
-    .apply('sgtest', false, 'not-needed')
-    .then(() => done(), (e) => { done(e); }));
+  it('adds a new security group', query(`  
+    INSERT INTO aws_security_group (description, group_name)
+    VALUES ('Security Group Test', 'sgtest');
+  `));
 
-  it('deletes the security group', (done) => {
-    createConnection({
-      name: 'sgtest',
-      type: 'postgres',
-      username: 'postgres',
-      password: 'test',
-      host: 'localhost',
-      port: 5432,
-      database: 'sgtest',
-      extra: { ssl: false, },
-    }).then((conn) => {
-      conn.query(`
-        DELETE FROM aws_security_group
-        WHERE group_name = 'sgtest';
-      `).then(() => {
-        conn.close().then(() => done(), (e) => { done(e); });
-      }, (e) => { done(e); });
-    }, (e) => { done(e); });
-  });
+  it('applies the security group change', runApply);
 
-  it('applies the security group change (again)', (done) => void iasql
-    .apply('sgtest', false, 'not-needed')
-    .then(() => done(), (e) => { done(e); }));
+  it('adds a security group rule', query(`
+    INSERT INTO aws_security_group_rule (is_egress, ip_protocol, from_port, to_port, cidr_ipv4, description, security_group_id)
+    SELECT true, 'tcp', 443, 443, '0.0.0.0/8', 'testrule', id
+    FROM aws_security_group
+    WHERE group_name = 'sgtest';
+  `));
+
+  it('applies the security group rule change', runApply);
+
+  it('updates the security group rule', query(`
+    UPDATE aws_security_group_rule SET to_port = 8443 WHERE description = 'testrule';
+  `));
+
+  it('applies the security group rule change (again)', runApply);
+
+  it('updates the security group', query(`
+    UPDATE aws_security_group SET group_name = 'sgtest2' WHERE group_name = 'sgtest';
+  `));
+
+  it('applies the security group change (again)', runApply);
+
+  it('deletes the security group rule', query(`
+    DELETE FROM aws_security_group_rule WHERE description = 'testrule';
+  `));
+
+  it('applies the security group rule change (last time)', runApply);
+
+  it('deletes the security group', query(`
+    DELETE FROM aws_security_group
+    WHERE group_name = 'sgtest2';
+  `));
+
+  it('applies the security group change (last time)', runApply);
 
   it('deletes the test db', (done) => void iasql
     .remove('sgtest', 'not-needed')
-    .then(() => done(), (e) => { done(e); }));
+    .then(...finish(done)));
 });
