d69d0f80b0822dd2befd463dea48ecced88ab0d6
Alejandro Guillen <aguillenv93@gmail.com>
2022-01-18 14:14:25 +0100

    Update db values based on cloud record

--- engine/src/modules/aws_cloudwatch/index.ts
+++ engine/src/modules/aws_cloudwatch/index.ts
@@ -36,7 +36,7 @@ export const AwsCloudwatchModule: Module = new Module({
       }),
       equals: (a: LogGroup, b: LogGroup) => Object.is(a.logGroupName, b.logGroupName)
         && Object.is(a.logGroupArn, b.logGroupArn)
-        && Object.is(a.creationTime, b.creationTime),
+        && Object.is(a.creationTime?.getTime(), b.creationTime?.getTime()),
       source: 'db',
       db: new Crud({
         create: (e: LogGroup[], ctx: Context) => ctx.orm.save(LogGroup, e),
@@ -68,20 +68,22 @@ export const AwsCloudwatchModule: Module = new Module({
         },
         read: async (ctx: Context, ids?: string[]) => {
           const client = await ctx.getAwsClient() as AWS;
-          ids = Array.isArray(ids) ? ids : []
-          const logGroups = (await Promise.all(ids.map(id => client.getLogGroups(id)))).flat();
+          const logGroups = Array.isArray(ids) ?
+            await Promise.all(ids.map(id => client.getLogGroups(id))) :
+            (await client.getLogGroups()) ?? [];
           return await Promise.all(
             logGroups.map((lg: any) => AwsCloudwatchModule.utils.logGroupMapper(lg, ctx))
           );
         },
-        update: async (lg: LogGroup[], ctx: Context) => {
-          return await Promise.all(lg.map(async (e) => {
-            try {
-              return await AwsCloudwatchModule.mappers.logGroup.cloud.create(e, ctx);
-            } catch (_) {
-              e.logGroupName = Date.now().toString();
-              return await AwsCloudwatchModule.mappers.logGroup.cloud.create(e, ctx);
-            }
+        updateOrReplace: () => 'update',
+        update: async (es: LogGroup[], ctx: Context) => {
+          // Right now we can only modify AWS-generated fields in the database.
+          // This implies that on `update`s we only have to restore the values for those records.
+          return await Promise.all(es.map(async (e) => {
+            const cloudRecord = ctx?.memo?.cloud?.LogGroup?.[e.logGroupName ?? ''];
+            cloudRecord.id = e.id;
+            await AwsCloudwatchModule.mappers.logGroup.db.update(cloudRecord, ctx);
+            return cloudRecord;
           }));
         },
         delete: async (lg: LogGroup[], ctx: Context) => {
