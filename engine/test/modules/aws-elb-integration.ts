dcd059511b114f37b2abd6b4ae6f1367da733635
Alejandro Guillen <aguillenv93@gmail.com>
2022-01-31 19:06:05 +0100

    Update elb test

--- engine/test/modules/aws-elb-integration.ts
+++ engine/test/modules/aws-elb-integration.ts
@@ -1,4 +1,5 @@
-import { ProtocolEnum, TargetTypeEnum } from '../../src/modules/aws_elb/entity';
+import { LoadBalancerStateEnum } from '@aws-sdk/client-elastic-load-balancing-v2';
+import { IpAddressType, LoadBalancerSchemeEnum, LoadBalancerTypeEnum, ProtocolEnum, TargetTypeEnum } from '../../src/modules/aws_elb/entity';
 import * as iasql from '../../src/services/iasql'
 import { getPrefix, runQuery, runApply, finish, execComposeUp, execComposeDown, } from '../helpers'
 
@@ -10,15 +11,21 @@ afterAll(execComposeDown);
 
 const prefix = getPrefix();
 const dbAlias = 'elbtest';
-const tgName = `${prefix}${dbAlias}tg`;
-const tgType = TargetTypeEnum.IP;
-const port = 5678;
-const protocol = ProtocolEnum.TCP;
 const apply = runApply.bind(null, dbAlias);
 const query = runQuery.bind(null, dbAlias);
 
+// Test constants
+const tgName = `${prefix}${dbAlias}tg`;
+const lbName = `${prefix}${dbAlias}lb`;
+const tgType = TargetTypeEnum.IP;
+const port = 5678;
+const protocol = ProtocolEnum.TCP;
+const lbScheme = LoadBalancerSchemeEnum.INTERNAL;
+const lbType = LoadBalancerTypeEnum.NETWORK;
+const lbIPAddressType = IpAddressType.IPV4;
+
 describe('ELB Integration Testing', () => {
-  it('creates a new test db', (done) => void iasql.add(
+  it('creates a new test db elb', (done) => void iasql.add(
     dbAlias,
     'us-west-2',
     process.env.AWS_ACCESS_KEY_ID ?? 'barf',
@@ -26,23 +33,25 @@ describe('ELB Integration Testing', () => {
     'not-needed').then(...finish(done)));
 
   it('installs the elb module', (done) => void iasql.install(
-    ['aws_elb'],
+    ['aws_security_group', 'aws_elb'],
     dbAlias,
     'not-needed').then(...finish(done)));
 
+  // TODO: add tests with stored procedures
+  // Target group
   it('adds a new targetGroup', query(`
     INSERT INTO aws_target_group (target_group_name, target_type, protocol, port, vpc_id, health_check_path)
-    SELECT '${tgName}', '${tgType}', '${protocol}', ${port}, id, '/'
+    SELECT '${tgName}', '${tgType}', '${protocol}', ${port}, id, '/health'
     FROM aws_vpc
     WHERE vpc_id = 'default'
-    order by id desc
-    limit 1;
+    ORDER BY id DESC
+    LIMIT 1;
   `));
 
   it('applies the change', apply);
 
   it('tries to update a target group field', query(`
-    UPDATE aws_target_group SET health_check_path = '/health' WHERE target_group_name = '${tgName}';
+    UPDATE aws_target_group SET health_check_path = '/fake-health' WHERE target_group_name = '${tgName}';
   `));
 
   it('applies the change', apply);
@@ -53,7 +62,85 @@ describe('ELB Integration Testing', () => {
 
   it('applies the change', apply);
 
-  // TODO: LISTENERS AND LOAD LANACER TESTS USING THE CREATED TARGET GROUP
+  // Load balancer
+  // TODO: add security groups insert when testing application load balancer integration
+  it('adds a new load balancer', query(`
+    INSERT INTO aws_load_balancer (load_balancer_name, scheme, vpc_id, load_balancer_type, ip_address_type)
+    SELECT '${lbName}', '${lbScheme}', id,  '${lbType}', '${lbIPAddressType}'
+    FROM aws_vpc
+    WHERE vpc_id = 'default'
+    order by id desc
+    limit 1;
+    INSERT INTO aws_load_balancer_subnets_aws_subnet (aws_load_balancer_id, aws_subnet_id)
+    SELECT aws_load_balancer.id, aws_subnet.id
+    FROM aws_load_balancer
+    INNER JOIN aws_vpc ON aws_vpc.id = aws_load_balancer.vpc_id
+    INNER JOIN aws_subnet ON aws_vpc.id = aws_subnet.vpc_id
+    WHERE aws_load_balancer.load_balancer_name = '${lbName}'
+    LIMIT 1;
+    INSERT INTO aws_load_balancer_availability_zones_availability_zone (aws_load_balancer_id, availability_zone_id)
+    SELECT aws_load_balancer.id, availability_zone.id
+    FROM aws_load_balancer
+    INNER JOIN aws_load_balancer_subnets_aws_subnet ON aws_load_balancer_subnets_aws_subnet.aws_load_balancer_id = aws_load_balancer.id
+    INNER JOIN aws_subnet ON aws_load_balancer_subnets_aws_subnet.aws_subnet_id = aws_subnet.id
+    INNER JOIN availability_zone ON availability_zone.id = aws_subnet.availability_zone_id
+    WHERE aws_load_balancer.load_balancer_name = '${lbName}'
+    LIMIT 1;
+  `));
+
+  it('applies the change', apply);
+
+  it('tries to update a load balancer field', query(`
+    UPDATE aws_load_balancer SET state = '${LoadBalancerStateEnum.FAILED}' WHERE load_balancer_name = '${lbName}';
+  `));
+
+  it('applies the change and restore it', apply);
+
+  // TODO: add load balancer update of subnets or security group
+
+  it('tries to update a target group field (replace)', query(`
+    UPDATE aws_load_balancer SET scheme = '${LoadBalancerSchemeEnum.INTERNET_FACING}' WHERE load_balancer_name = '${lbName}';
+  `));
+
+  it('applies the change', apply);
+
+  // TODO: LOAD BALANCER TESTS USING THE CREATED TARGET GROUP AND LISTENER
+  // it('adds a new targetGroup', query(`
+  //   INSERT INTO aws_target_group (target_group_name, target_type, protocol, port, vpc_id, health_check_path)
+  //   SELECT '${tgName}', '${tgType}', '${protocol}', ${port}, id, '/'
+  //   FROM aws_vpc
+  //   WHERE vpc_id = 'default'
+  //   order by id desc
+  //   limit 1;
+  // `));
+
+  // it('applies the change', apply);
+
+  // it('tries to update a target group field', query(`
+  //   UPDATE aws_target_group SET health_check_path = '/health' WHERE target_group_name = '${tgName}';
+  // `));
+
+  // it('applies the change', apply);
+
+  // it('tries to update a target group field (replace)', query(`
+  //   UPDATE aws_target_group SET port = 5677 WHERE target_group_name = '${tgName}';
+  // `));
+
+  // it('applies the change', apply);
+
+  // it('deletes the target group', query(`
+  //   DELETE FROM aws_target_group
+  //   WHERE target_group_name = '${tgName}';
+  // `));
+
+  // it('applies the change (last time)', apply);
+
+  it('deletes the load balancer', query(`
+    DELETE FROM aws_load_balancer
+    WHERE load_balancer_name = '${lbName}';
+  `));
+
+  it('applies the change', apply);
 
   it('deletes the target group', query(`
     DELETE FROM aws_target_group
