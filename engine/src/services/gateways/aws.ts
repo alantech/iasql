e9ae937b3bbf1cf7d82d8a0fb30cb5eb4c8ab756
Alejandro Guillen <aguillenv93@gmail.com>
2022-02-14 19:41:44 +0100

    Fix repository name for private ECR (#413)
    * Fix repository name
    
    * Add ecr path tests
    
    * Fix query
    
    * Fix delete dependencies
    
    * Add service delete waiter
    
    * Refactor task def and services

--- engine/src/services/gateways/aws.ts
+++ engine/src/services/gateways/aws.ts
@@ -925,6 +925,29 @@ export class AWS {
         cluster,
       })
     )
+    // We wait it is completely deleted to avoid issues deleting dependent resources.
+    const input = new DescribeServicesCommand({
+      services: [name],
+      cluster,
+    });
+    await createWaiter<ECSClient, DescribeServicesCommand>(
+      {
+        client: this.ecsClient,
+        // all in seconds
+        maxWaitTime: 30,
+        minDelay: 1,
+        maxDelay: 4,
+      },
+      input,
+      async (client, cmd) => {
+        const data = await client.send(cmd);
+        if (data.services?.length && data.services[0].status === 'DRAINING') {
+          return { state: WaiterState.RETRY };
+        } else {
+          return { state: WaiterState.SUCCESS };
+        }
+      },
+    );
   }
 
   async getEngineVersions() {
