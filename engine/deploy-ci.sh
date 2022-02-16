34951749b65a36e632d31ee2e54184dac8476457
depombo <lfdepombo@gmail.com>
2022-02-16 20:30:37 +0000

    temporarily apply the database contents before pushing the image to see if the ecr repo is created

--- engine/deploy-ci.sh
+++ engine/deploy-ci.sh
@@ -17,6 +17,18 @@ export $(cat .deploy-env | xargs) && cargo run --manifest-path=../cli/Cargo.toml
 echo "\Installing modules in iasql db..."
 cargo run --manifest-path=../cli/Cargo.toml -- install aws_security_group aws_elb aws_cloudwatch aws_ecr aws_ecs aws_rds --db=iasql --noninteractive
 
+# Prepare iasql-on-iasql.sql script
+echo "\nPreparing iasql script..."
+export $(cat .deploy-env | xargs) && sed "s/<DB_PASSWORD>/${DB_PASSWORD}/g;s/<IRONPLANS_TOKEN>/${IRONPLANS_TOKEN}/g" ./src/script/iasql-on-iasql.sql > ./src/script/iasql-on-iasql.out.sql
+
+# Update service. Set PGPASSWORD environment variable to avoid interaction
+echo "\nUpdating iasql db..."
+psql -h localhost -p 5432 -U postgres -d iasql -f ./src/script/iasql-on-iasql.out.sql
+
+# IaSQL db apply. Using local debug version
+echo "\nApplying changes from iasql db..."
+cargo run --manifest-path=../cli/Cargo.toml -- apply iasql --noninteractive
+
 # Login. Review your profile
 echo "\nDocker login..."
 aws ecr get-login-password | docker login --username AWS --password-stdin 547931376551.dkr.ecr.us-east-2.amazonaws.com
@@ -32,18 +44,6 @@ docker tag iasql-engine-repository:latest 547931376551.dkr.ecr.us-east-2.amazona
 # Push
 echo "\nPushing image..."
 docker push 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository:latest
-
-# Prepare iasql-on-iasql.sql script
-echo "\nPreparing iasql script..."
-export $(cat .deploy-env | xargs) && sed "s/<DB_PASSWORD>/${DB_PASSWORD}/g;s/<IRONPLANS_TOKEN>/${IRONPLANS_TOKEN}/g" ./src/script/iasql-on-iasql.sql > ./src/script/iasql-on-iasql.out.sql
-
-# Update service. Set PGPASSWORD environment variable to avoid interaction
-echo "\nUpdating iasql db..."
-psql -h localhost -p 5432 -U postgres -d iasql -f ./src/script/iasql-on-iasql.out.sql
-
-# IaSQL db apply. Using local debug version
-echo "\nApplying changes from iasql db..."
-cargo run --manifest-path=../cli/Cargo.toml -- apply iasql --noninteractive
 
 # Clean and leave just the last image
 echo "\nCleaning docker images..."
