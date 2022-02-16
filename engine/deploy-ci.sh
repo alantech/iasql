bc29fdf5f3e71e19b016a5b182a091ecaab1b158
depombo <lfdepombo@gmail.com>
2022-02-16 22:13:00 +0000

    generate the private registry URI;

--- engine/deploy-ci.sh
+++ engine/deploy-ci.sh
@@ -5,10 +5,17 @@
 ## - DB_PASSWORD environment variable defined in .deploy-env
 ## - IRONPLANS_TOKEN environment variable defined in .deploy-env
 ## - AUTH_TOKEN environment variable defined in .deploy-env
+## - REGION environment variable defined in .deploy-env
 #!/bin/bash
 
 # Fail on error
 set -e
+
+## get aws account id and create private registry and image URIs with it
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r .Account)
+export REGISTRY_URI=${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+export IMAGE_URI=${REGISTRY_URI}/iasql-engine-repository
+export LATEST_IMAGE_URI=${IMAGE_URI}:latest
 
 # IaSQL db new. Using local debug version
 echo "\Creating an iasql db..."
@@ -16,6 +23,22 @@ export $(cat .deploy-env | xargs) && cargo run --manifest-path=../cli/Cargo.toml
 
 echo "\Installing modules in iasql db..."
 cargo run --manifest-path=../cli/Cargo.toml -- install aws_security_group aws_elb aws_cloudwatch aws_ecr aws_ecs aws_rds --db=iasql --noninteractive
+
+# Login. Review your profile
+echo "\nDocker login..."
+aws ecr get-login-password | docker login --username AWS --password-stdin ${REGISTRY_URI}
+
+# Build
+echo "\nBuilding image..."
+docker build -t iasql-engine-repository .
+
+# Tag
+echo "\nTagging image..."
+docker tag iasql-engine-repository:latest ${LATEST_IMAGE_URI}
+
+# Push
+echo "\nPushing image..."
+docker push ${LATEST_IMAGE_URI}
 
 # Prepare iasql-on-iasql.sql script
 echo "\nPreparing iasql script..."
@@ -29,22 +52,6 @@ psql -h localhost -p 5432 -U postgres -d iasql -f ./src/script/iasql-on-iasql.ou
 echo "\nApplying changes from iasql db..."
 cargo run --manifest-path=../cli/Cargo.toml -- apply iasql --noninteractive
 
-# Login. Review your profile
-echo "\nDocker login..."
-aws ecr get-login-password | docker login --username AWS --password-stdin 547931376551.dkr.ecr.us-east-2.amazonaws.com
-
-# Build
-echo "\nBuilding image..."
-docker build -t iasql-engine-repository .
-
-# Tag
-echo "\nTagging image..."
-docker tag iasql-engine-repository:latest 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository:latest
-
-# Push
-echo "\nPushing image..."
-docker push 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository:latest
-
 # Clean and leave just the last image
 echo "\nCleaning docker images..."
-if [ $(docker images -q 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository | wc -l) -gt 2 ]; then docker rmi $(docker images -q 547931376551.dkr.ecr.us-east-2.amazonaws.com/iasql-engine-repository | tail -n +2); fi
+if [ $(docker images -q ${IMAGE_URI} | wc -l) -gt 2 ]; then docker rmi $(docker images -q ${IMAGE_URI} | tail -n +2); fi
