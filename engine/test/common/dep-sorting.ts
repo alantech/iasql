89767509dde1a2bb09d1f31a626d8ec94ff04c48
David Ellis <isv.damocles@gmail.com>
2022-02-21 14:45:28 -0600

    Try to fix the tests

--- engine/test/common/dep-sorting.ts
+++ engine/test/common/dep-sorting.ts
@@ -3,15 +3,16 @@ import { ModuleInterface, } from '../../src/modules/interfaces'
 
 const fakeMod = (name: string, dependencies: string[]) => ({
   name,
+  version: '0.0.1',
   dependencies,
 } as ModuleInterface);
 
 describe('Module Sorting', () => {
   it('should sort from root to leaf order', () => {
     const root = fakeMod('root', []);
-    const middle = fakeMod('middle', ['root']);
-    const leaf1 = fakeMod('leaf1', ['root']);
-    const leaf2 = fakeMod('leaf2', ['middle']);
+    const middle = fakeMod('middle', ['root@0.0.1']);
+    const leaf1 = fakeMod('leaf1', ['root@0.0.1']);
+    const leaf2 = fakeMod('leaf2', ['middle@0.0.1']);
     const sortedList = sortModules([leaf1, leaf2, middle, root], []);
     expect(sortedList[0]).toBe(root);
     expect(sortedList.indexOf(middle)).toBeLessThan(sortedList.indexOf(leaf2));
@@ -19,10 +20,10 @@ describe('Module Sorting', () => {
 
   it('should sort child nodes correctly if root is excluded', () => {
     const root = fakeMod('root', []);
-    const middle = fakeMod('middle', ['root']);
-    const leaf1 = fakeMod('leaf1', ['root']);
-    const leaf2 = fakeMod('leaf2', ['middle']);
-    const sortedList = sortModules([leaf1, leaf2, middle, root], ['root']);
+    const middle = fakeMod('middle', ['root@0.0.1']);
+    const leaf1 = fakeMod('leaf1', ['root@0.0.1']);
+    const leaf2 = fakeMod('leaf2', ['middle@0.0.1']);
+    const sortedList = sortModules([leaf1, leaf2, middle, root], ['root@0.0.1']);
     expect(sortedList.indexOf(middle)).toBeLessThan(sortedList.indexOf(leaf2));
   });
 });
