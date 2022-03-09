1c4b12a7c3d63384b9e256f5838911a0a46f5e9d
David Ellis <isv.damocles@gmail.com>
2022-03-08 21:39:40 -0600

    Restore migrations on 'main' as we don't need to regenerate them right now

--- src/modules/aws_account@0.0.1/migration/1644463885917-aws_account.ts
+++ src/modules/aws_account@0.0.1/migration/1644463885917-aws_account.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class awsAccount1646785425132 implements MigrationInterface {
-    name = 'awsAccount1646785425132'
+export class awsAccount1644463885917 implements MigrationInterface {
+    name = 'awsAccount1644463885917'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "aws_account" ("id" SERIAL NOT NULL, "access_key_id" character varying NOT NULL, "secret_access_key" character varying NOT NULL, "region" character varying NOT NULL, CONSTRAINT "PK_9ad897024d8c36c541d7fe84084" PRIMARY KEY ("id"))`);
