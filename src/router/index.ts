c683a8387d6854d0efca05d60cfdc2d2662beb8e
depombo <lfdepombo@gmail.com>
2022-03-10 15:40:31 +0000

    use falsy

--- src/router/index.ts
+++ src/router/index.ts
@@ -28,7 +28,7 @@ v1.use(express.json({ limit: '10000MB' }));
 v1.use((req, res, next) => {
   const headers = req.headers;
   const cliVersion = headers["cli-version"] as string;
-  if (cliVersion === undefined || semver.compare(MIN_CLI_VERSION, cliVersion) > 0) {
+  if (!cliVersion || semver.compare(MIN_CLI_VERSION, cliVersion) > 0) {
     const error = {
       message: `Outdated CLI version. Must use version ${MIN_CLI_VERSION} at least. Please refer to https://docs.iasql.com/install to upgrade.`
     };
