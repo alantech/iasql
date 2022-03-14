8130dff9eadee0eb880382a5078a368c27f17f20
Alejandro Guillen <aguillenv93@gmail.com>
2022-03-14 18:59:52 +0100

    Add install/uninstall test suite (#511)
    * Test install/uninstall
    
    * Update ci
    
    * Test parallel
    
    * Override module list on all flag
    
    * Fix install all modules
    
    * Fix cloudwatch test
    
    * Fix ecr test
    
    * Update security groups test
    
    * Fix ec2
    
    * Just install module test
    
    * Rename variable
    
    * Restore ci
    
    * Add sync and reimport test
    
    * Add sync
    
    * Fix ec2 tests
    
    * Update ecr tests
    
    * Update rds tests
    
    * Update sg tests
    
    * Update vpc test
    
    * update elb test
    
    * Update ecs test
    
    * Restore ci

--- test/modules/aws-ec2-integration.ts
+++ test/modules/aws-ec2-integration.ts
@@ -1,5 +1,5 @@
 import * as iasql from '../../src/services/iasql'
-import { runQuery, runApply, finish, execComposeUp, execComposeDown, } from '../helpers'
+import { runQuery, runApply, finish, execComposeUp, execComposeDown, runSync, } from '../helpers'
 
 jest.setTimeout(240000);
 
@@ -14,6 +14,7 @@ const amznAmiId = 'ami-06cffe063efe892ad';
 const ubuntuAmiId = 'ami-0892d3c7ee96c0bf7';
 
 const apply = runApply.bind(null, dbAlias);
+const sync = runSync.bind(null, dbAlias);
 const query = runQuery.bind(null, dbAlias);
 
 describe('EC2 Integration Testing', () => {
@@ -52,12 +53,44 @@ describe('EC2 Integration Testing', () => {
     });
   });
 
-  it('applies the created instances', apply);
+  it('Undo changes', sync);
 
   it('check number of instances', query(`
     SELECT *
-    FROM instance;
+    FROM instance
+    WHERE name = ANY(array['i-1', 'i-2']);
+  `, (res: any[]) => expect(res.length).toBe(0)));
+
+  it('adds two ec2 instance', (done) => {
+    query(`
+      BEGIN;
+        INSERT INTO instance (name, ami, instance_type)
+          VALUES ('i-1','${ubuntuAmiId}', 't2.micro');
+        INSERT INTO instance_security_groups (instance_id, aws_security_group_id) SELECT
+          (SELECT id FROM instance WHERE name='i-1'),
+          (SELECT id FROM aws_security_group WHERE group_name='default');
+      COMMIT;
+
+      BEGIN;
+        INSERT INTO instance (name, ami, instance_type)
+          VALUES ('i-2','${amznAmiId}', 't2.micro');
+        INSERT INTO instance_security_groups (instance_id, aws_security_group_id) SELECT
+          (SELECT id FROM instance WHERE name='i-2'),
+          (SELECT id FROM aws_security_group WHERE group_name='default');
+      COMMIT;
+    `)((e?: any) => {
+      if (!!e) return done(e);
+      done();
+    });
+  });
+
+  it('check number of instances', query(`
+    SELECT *
+    FROM instance
+    WHERE name = ANY(array['i-1', 'i-2']);
   `, (res: any[]) => expect(res.length).toBe(2)));
+
+  it('applies the created instances', apply);
 
   it('set both ec2 instances to the same ami', query(`
     UPDATE instance SET ami = '${amznAmiId}' WHERE name = 'i-1';
@@ -101,6 +134,45 @@ describe('EC2 Integration Testing', () => {
     SELECT *
     FROM instance;
   `, (res: any[]) => expect(res.length).toBe(0)));
+
+  it('deletes the test db', (done) => void iasql
+    .remove(dbAlias, 'not-needed')
+    .then(...finish(done)));
+});
+
+describe('EC2 install/uninstall', () => {
+  it('creates a new test db', (done) => void iasql.add(
+    dbAlias,
+    'us-east-1', // Share region with common tests
+    process.env.AWS_ACCESS_KEY_ID ?? 'barf',
+    process.env.AWS_SECRET_ACCESS_KEY ?? 'barf',
+    'not-needed').then(...finish(done)));
+
+  it('installs the EC2 module', (done) => void iasql.install(
+    ['aws_ec2@0.0.1', 'aws_security_group@0.0.1'],
+    dbAlias,
+    'not-needed').then(...finish(done)));
+
+  it('uninstalls the EC2 module', (done) => void iasql.uninstall(
+    ['aws_ec2@0.0.1', 'aws_security_group@0.0.1'],
+    dbAlias,
+    'not-needed').then(...finish(done)));
+
+  it('installs all modules', (done) => void iasql.install(
+    [],
+    dbAlias,
+    'not-needed',
+    true).then(...finish(done)));
+
+  it('uninstalls the EC2 module', (done) => void iasql.uninstall(
+    ['aws_ec2@0.0.1'],
+    dbAlias,
+    'not-needed').then(...finish(done)));
+
+  it('installs the EC2 module', (done) => void iasql.install(
+    ['aws_ec2@0.0.1'],
+    dbAlias,
+    'not-needed').then(...finish(done)));
 
   it('deletes the test db', (done) => void iasql
     .remove(dbAlias, 'not-needed')
