71be1dd3065309ba24bebe785fb345a300977547
depombo <lfdepombo@gmail.com>
2022-03-15 05:57:55 +0000

    remove unused stored procedures

--- src/modules/aws_ec2@0.0.1/migration/1646173335775-aws_ec2.ts
+++ src/modules/aws_ec2@0.0.1/migration/1646173335775-aws_ec2.ts
@@ -10,70 +10,9 @@ export class awsEc21646173335775 implements MigrationInterface {
         await queryRunner.query(`CREATE INDEX "IDX_8212b4abce910f4524d3af339e" ON "instance_security_groups" ("aws_security_group_id") `);
         await queryRunner.query(`ALTER TABLE "instance_security_groups" ADD CONSTRAINT "FK_fa3c179d5090cb1309c63b5e20a" FOREIGN KEY ("instance_id") REFERENCES "instance"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "instance_security_groups" ADD CONSTRAINT "FK_8212b4abce910f4524d3af339e8" FOREIGN KEY ("aws_security_group_id") REFERENCES "aws_security_group"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        // Example of use: call create_or_update_ec2_instance('i-1', 'ami-0fdffa9be142bf7f4', 't2.micro', array['default'])
-        await queryRunner.query(`
-            create or replace procedure create_or_update_ec2_instance(_instance_name text, _ami_id text, _instance_type text, _security_group_names text[])
-            language plpgsql
-            as $$
-                declare
-                    instance_id integer;
-                    sg record;
-                begin
-                    insert into instance
-                        (ami, instance_type, name)
-                    values
-                        (_ami_id, _instance_type, _instance_name)
-                    on conflict (name)
-                    do update set ami = _ami_id, instance_type = _instance_type;
-            
-                    select id into instance_id
-                    from instance
-                    order by id desc
-                    limit 1;
-
-                    for sg in
-                        select id
-                        from aws_security_group
-                        where group_name = any(_security_group_names)
-                    loop
-                        insert into
-                            instance_security_groups (instance_id, aws_security_group_id)
-                        values
-                            (instance_id, sg.id)
-                        on conflict do nothing;
-                    end loop;
-
-                    raise info 'ec2_instance_id = %', instance_id;
-                end;
-            $$;
-        `);
-        // Example of use: call delete_ec2_instance('i-1')
-        await queryRunner.query(`
-            create or replace procedure delete_ec2_instance(_instance_name text)
-            language plpgsql
-            as $$
-                declare
-                    instance_id integer;
-                begin
-                    select id into instance_id
-                    from instance
-                    where name = _instance_name
-                    order by id desc
-                    limit 1;
-
-                    delete from instance_security_groups
-                    where instance_id = instance_id;
-
-                    delete from instance
-                    where name = _instance_name;
-                end;
-            $$;
-        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP PROCEDURE delete_ec2_instance`);
-        await queryRunner.query(`DROP PROCEDURE create_or_update_ec2_instance`);
         await queryRunner.query(`ALTER TABLE "instance_security_groups" DROP CONSTRAINT "FK_8212b4abce910f4524d3af339e8"`);
         await queryRunner.query(`ALTER TABLE "instance_security_groups" DROP CONSTRAINT "FK_fa3c179d5090cb1309c63b5e20a"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_8212b4abce910f4524d3af339e"`);
--- src/modules/aws_rds@0.0.1/migration/1646760541170-aws_rds.ts
+++ src/modules/aws_rds@0.0.1/migration/1646760541170-aws_rds.ts
@@ -10,80 +10,9 @@ export class awsRds1646760541170 implements MigrationInterface {
         await queryRunner.query(`CREATE INDEX "IDX_7b0d4af7000a31b4657220db78" ON "rds_vpc_security_groups_aws_security_group" ("aws_security_group_id") `);
         await queryRunner.query(`ALTER TABLE "rds_vpc_security_groups_aws_security_group" ADD CONSTRAINT "FK_bf5fdc058ec5db521f32d4d6dd0" FOREIGN KEY ("rds_id") REFERENCES "rds"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
         await queryRunner.query(`ALTER TABLE "rds_vpc_security_groups_aws_security_group" ADD CONSTRAINT "FK_7b0d4af7000a31b4657220db78e" FOREIGN KEY ("aws_security_group_id") REFERENCES "aws_security_group"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-        // Example of use: call create_or_update_rds('test-sp-sg', 50, 'db.t3.micro', 'postgres', '13.4', 'test', 'Alanus3r', 'eu-west-1a', array['default']);
-        await queryRunner.query(`
-            create or replace procedure create_or_update_rds(
-                _name text,
-                _allocated_storage integer,
-                _db_instance_class text,
-                _engine text,
-                _engine_version text,
-                _master_username text,
-                _master_user_password text,
-                _availability_zone_name text,
-                _security_group_names text[] default null
-            )
-            language plpgsql
-            as $$
-                declare
-                    rds_instance_id integer;
-                    sg record;
-                begin
-                    insert into rds
-                        (db_instance_identifier, allocated_storage, db_instance_class, master_user_password, master_username, availability_zone, engine)
-                    values
-                        (_name, _allocated_storage, _db_instance_class, _master_user_password, _master_username, _availability_zone_name, _engine || ':' || _engine_version)
-                    on conflict (db_instance_identifier)
-                    do update set allocated_storage = _allocated_storage,
-                        db_instance_class = _db_instance_class,
-                        master_user_password = _master_user_password,
-                        master_username = _master_username,
-                        availability_zone = _availability_zone_name,
-                        engine = _engine || ':' || _engine_version;
-            
-                    select id into rds_instance_id
-                    from rds
-                    where db_instance_identifier = _name
-                    order by id desc
-                    limit 1;
-
-                    for sg in
-                        select id
-                        from aws_security_group
-                        where group_name = any(_security_group_names)
-                    loop
-                        insert into rds_vpc_security_groups_aws_security_group
-                            (rds_id, aws_security_group_id)
-                        values
-                            (rds_instance_id, sg.id)
-                        on conflict do nothing;
-                    end loop;
-
-                    delete from rds_vpc_security_groups_aws_security_group
-                    using aws_security_group
-                    where rds_id = rds_instance_id and aws_security_group.id = rds_vpc_security_groups_aws_security_group.aws_security_group_id and not (group_name = any(_security_group_names));
-            
-                    raise info 'rds_instance_id = %', rds_instance_id;
-                end;
-            $$;
-        `);
-        // Example of use: call delete_rds('test-sp-sg');
-        await queryRunner.query(`
-            create or replace procedure delete_rds(_name text)
-            language plpgsql
-            as $$
-                begin
-                    delete
-                    from rds
-                    where db_instance_identifier = _name;
-                end;
-            $$;
-        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP procedure delete_rds;`);
-        await queryRunner.query(`DROP procedure create_or_update_rds;`);
         await queryRunner.query(`ALTER TABLE "rds_vpc_security_groups_aws_security_group" DROP CONSTRAINT "FK_7b0d4af7000a31b4657220db78e"`);
         await queryRunner.query(`ALTER TABLE "rds_vpc_security_groups_aws_security_group" DROP CONSTRAINT "FK_bf5fdc058ec5db521f32d4d6dd0"`);
         await queryRunner.query(`DROP INDEX "public"."IDX_7b0d4af7000a31b4657220db78"`);
