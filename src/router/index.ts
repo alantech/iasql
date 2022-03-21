8e4d8adaf3f006ce2bdef4ac0af96ccbd472edea
depombo <lfdepombo@gmail.com>
2022-03-21 18:53:36 -0400

    remove cli version check

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,32 +1,16 @@
 import * as express from 'express'
 import jwt from 'express-jwt'
 import jwksRsa from 'jwks-rsa'
-import * as semver from 'semver';
 
 import config from '../config'
-import * as logger from '../services/logger'
 
 // routes
 import { db, } from './db'
 import { mod, } from './module'
 
-export const MIN_CLI_VERSION = '0.2.5';
-
 const v1 = express.Router();
 // 10 GB post payload limit for import dumps
 v1.use(express.json({ limit: '10000MB' }));
-// check the user is on a minimum CLI version
-v1.use((req, res, next) => {
-  const headers = req.headers;
-  const cliVersion = headers["cli-version"] as string;
-  if (!cliVersion || semver.compare(MIN_CLI_VERSION, cliVersion) > 0) {
-    const error = {
-      message: `Outdated CLI version. Must use version ${MIN_CLI_VERSION} at least. Please refer to https://docs.iasql.com/install to upgrade.`
-    };
-    return res.status(500).end(logger.error(error));
-  }
-  next();
-});
 if (config.a0Enabled) {
   const checkJwt = jwt({
     secret: jwksRsa.expressJwtSecret({
