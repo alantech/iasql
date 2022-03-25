82eb89b31ebfad5f62e22959c34c1f4727225257
luisfer <lfdepombo@gmail.com>
2022-03-25 08:55:29 -0700

    Use sql files for iasql fns (#565)
    * Use sql files for iasql fns
    
    * rm invokation

---
+++ sql/drop_fns.sql
@@ -0,0 +1,6 @@
+DROP FUNCTION "iasql_uninstall";
+DROP FUNCTION "iasql_install";
+DROP FUNCTION "iasql_sync";
+DROP FUNCTION "iasql_plan";
+DROP FUNCTION "iasql_apply";
+DROP FUNCTION "until_iasql_operation";
