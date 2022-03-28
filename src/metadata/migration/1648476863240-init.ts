ef0fb2bd5bc51b1e972a77e4ad80fbc403778688
Alejandro Guillen <aguillenv93@gmail.com>
2022-03-28 16:14:55 +0200

    Add db status migration

--- src/metadata/migration/1648476863240-init.ts
+++ src/metadata/migration/1648476863240-init.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class init1647661129900 implements MigrationInterface {
-    name = 'init1647661129900'
+export class init1648476863240 implements MigrationInterface {
+    name = 'init1648476863240'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "iasql_database" ("pg_name" character varying NOT NULL, "alias" character varying NOT NULL, "region" character varying NOT NULL, "pg_user" character varying NOT NULL, CONSTRAINT "PK_a70daf5038ee78e1a4e5cb78990" PRIMARY KEY ("pg_name"))`);
+        await queryRunner.query(`CREATE TABLE "iasql_database" ("pg_name" character varying NOT NULL, "alias" character varying NOT NULL, "region" character varying NOT NULL, "pg_user" character varying NOT NULL, "is_ready" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_a70daf5038ee78e1a4e5cb78990" PRIMARY KEY ("pg_name"))`);
         await queryRunner.query(`CREATE TABLE "iasql_user" ("id" character varying NOT NULL, "email" character varying NOT NULL, CONSTRAINT "PK_3324df41f4e885452f09f3e147e" PRIMARY KEY ("id"))`);
         await queryRunner.query(`CREATE TABLE "iasql_user_databases" ("iasql_user_id" character varying NOT NULL, "iasql_database_pg_name" character varying NOT NULL, CONSTRAINT "PK_6792136f2a5b23bd9efd2c3e4d8" PRIMARY KEY ("iasql_user_id", "iasql_database_pg_name"))`);
         await queryRunner.query(`CREATE INDEX "IDX_94a9d6689be180267b380def44" ON "iasql_user_databases" ("iasql_user_id") `);
