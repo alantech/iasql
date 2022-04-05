58daca41b14025dcffbfe1f6e8dc63a69c024cdb
David Ellis <isv.damocles@gmail.com>
2022-04-04 22:44:15 -0500

    Forgot that only entities that have a mapper are included in some of the checks, adding one for IasqlModule

--- src/modules/iasql_platform@0.0.1/index.ts
+++ src/modules/iasql_platform@0.0.1/index.ts
@@ -1,9 +1,28 @@
 /* THIS MODULE IS A SPECIAL SNOWFLAKE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE */
+import { In, } from 'typeorm'
 
-import { Module, } from '../interfaces'
+import { Context, Crud, Mapper, Module, } from '../interfaces'
 import * as metadata from './module.json'
+import { IasqlModule, } from './entity'
 
 export const IasqlPlatform: Module = new Module({
   ...metadata,
-  mappers: {},
+  mappers: {
+    iasqlModule: new Mapper<IasqlModule>({
+      entity: IasqlModule,
+      equals: (_a: IasqlModule, _b: IasqlModule) => true,
+      source: 'db',
+      cloud: new Crud({
+        // We don't actually connect to AWS for this module, because it's meta
+        create: async (_e: IasqlModule[], _ctx: Context) => { /* Do nothing */ },
+        read: (ctx: Context, names?: string[]) => ctx.orm.find(IasqlModule, names ? {
+          where: {
+            name: In(names),
+          },
+        } : undefined),
+        update: async (_e: IasqlModule[], _ctx: Context) => { /* Do nothing */ },
+        delete: async (_e: IasqlModule[], _ctx: Context) => { /* Do nothing */ },
+      }),
+    }),
+  },
 }, __dirname);
