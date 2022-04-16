c6cd82a4301342a1b582ba21b19e60af38b8409a
depombo <lfdepombo@gmail.com>
2022-04-16 14:51:06 -0700

    log error

--- src/services/telemetry.ts
+++ src/services/telemetry.ts
@@ -1,61 +1,74 @@
 import * as Amplitude from '@amplitude/node';
 
 import config from '../config';
+import logger from './logger';
 import { IasqlOperationType } from '../modules/iasql_functions@0.0.1/entity';
 
 const singleton = config.telemetry ? Amplitude.init(config.telemetry.amplitudeKey) : undefined;
 
-export function logDbConnect(dbId: string, dbAlias: string, uid: string, email: string, directConnect: boolean) {
+export async function logDbConnect(dbId: string, dbAlias: string, uid: string, email: string, directConnect: boolean) {
   if (!singleton) return;
-  singleton.logEvent({
-    event_type: 'CONNECT',
-    // a user can have multiple devices in amplitude
-    // so we map a database to a device
-    device_id: dbId,
-    user_id: uid,
-    user_properties: {
-      email,
-    },
-    device_model: dbAlias,
-    event_properties: {
-      directConnect
-    },
-  });
+  try {
+    await singleton.logEvent({
+      event_type: 'CONNECT',
+      // a user can have multiple devices in amplitude
+      // so we map a database to a device
+      device_id: dbId,
+      user_id: uid,
+      user_properties: {
+        email,
+      },
+      device_model: dbAlias,
+      event_properties: {
+        directConnect
+      },
+    });
+  } catch(e: any) {
+    logger.error('failed to log CONNECT event', e);
+  }
 }
 
-export function logDbList(uid: string, dbCount: number, totalRecordCount: number) {
+export async function logDbList(uid: string, dbCount: number, totalRecordCount: number) {
   if (!singleton) return;
-  singleton.logEvent({
-    user_id: uid,
-    event_type: 'LIST',
-    event_properties: {
-      dbCount,
-      totalRecordCount,
-    }
-  });
+  try {
+    await singleton.logEvent({
+      user_id: uid,
+      event_type: 'LIST',
+      event_properties: {
+        dbCount,
+        totalRecordCount,
+      }
+    });
+  } catch(e: any) {
+    logger.error('failed to log LIST event', e);
+  }
 }
 
-export function logDbDisconnect(dbId: string) {
-  logDbEvent(dbId, 'DISCONNECT');
+export async function logDbDisconnect(dbId: string) {
+  await logDbEvent(dbId, 'DISCONNECT');
 }
 
-export function logDbExport(dbId: string, dataOnly: boolean) {
-  logDbEvent(dbId, 'EXPORT', {
+export async function logDbExport(dbId: string, dataOnly: boolean) {
+  await logDbEvent(dbId, 'EXPORT', {
     dataOnly,
   });
 }
 
-export function logDbOp(dbId: string, opType: IasqlOperationType, eventProps: any) {
-  logDbEvent(dbId, opType, eventProps);
+export async function logDbOp(dbId: string, opType: IasqlOperationType, eventProps: any) {
+  await logDbEvent(dbId, opType, eventProps);
 }
 
-function logDbEvent(dbId: string, eventType: string, eventProps?: any) {
+async function logDbEvent(dbId: string, eventType: string, eventProps?: any) {
   if (!singleton) return;
-  singleton.logEvent({
-    event_type: eventType,
-    device_id: dbId,
-    event_properties: eventProps,
-  });
+  try {
+    singleton.logEvent({
+      event_type: eventType,
+      device_id: dbId,
+      event_properties: eventProps,
+    });
+  } catch(e: any) {
+    logger.error(`failed to log ${eventType} event`, e);
+  }
 }
 
 export default singleton;
