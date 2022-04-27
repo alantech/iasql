338d422d9fe6be4a107be3b67046cbac0a01b5bc
Alejandro Guillen <aguillenv93@gmail.com>
2022-04-27 20:08:42 +0200

    Fix lambda

--- src/cloudformation/lambda/index.js
+++ src/cloudformation/lambda/index.js
@@ -9,15 +9,15 @@ exports.handler = async function (event) {
   console.log('Lambda received event:');
   console.log(event);
   try {
-    const client = new Client({ CONNECTION_STRING, });
+    const client = new Client(CONNECTION_STRING);
     await client.connect();
     const insertCreds = {
       name: 'insert-creds',
       text: `INSERT INTO aws_account (access_key_id, secret_access_key, region)
-             VALUES ('$1', '$2', '$3');`,
+             VALUES ($1, $2, $3);`,
       values: [ACCESS_KEY_ID, SECRET_ACCESS_KEY, AWS_REGION],
     };
-    await client.query('SELECT * FROM iasql_install(aws_account)');
+    await client.query("SELECT * FROM iasql_install('aws_account')");
     await client.query(insertCreds);
     const result = { result: 'Success' };
     return JSON.stringify(result);
