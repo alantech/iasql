6b1524d2494b0aaadb9e9d8fb0d972308b60a6a4
David Ellis <isv.damocles@gmail.com>
2022-04-28 12:57:45 -0500

    Fix the migration scripts

--- src/scripts/module-json-utils.ts
+++ src/scripts/module-json-utils.ts
@@ -7,18 +7,18 @@ export type ModJson = {
 }
 
 export const getModJsons = () => {
-  const modDir = fs.readdirSync(`${__dirname}/../modules`).filter(r => !/.ts$/.test(r));
+  const modDir = fs.readdirSync(`${__dirname}/../modules/0.0.1`).filter(r => !/.ts$/.test(r));
   const modJsons: ModJson[] = modDir
-    .map(r => fs.readFileSync(`${__dirname}/../modules/${r}/module.json`, 'utf8'))
+    .map(r => fs.readFileSync(`${__dirname}/../modules/0.0.1/${r}/module.json`, 'utf8'))
     .map(r => JSON.parse(r) as ModJson);
   const mods: { [key: string]: ModJson, } = {};
-  modJsons.forEach(m => mods[`${m.name}@${m.version}`] = m);
+  modJsons.forEach(m => mods[m.name] = m);
   return mods;
 }
 
 // TODO: Figure out some way to DRY this logic with `interfaces.ts`
 export const getModMigration = (name: string) => {
-  const migrationDir = `${__dirname}/../modules/${name}/migration`;
+  const migrationDir = `${__dirname}/../modules/0.0.1/${name}/migration`;
   const files = fs.readdirSync(migrationDir)
     .filter(r => !/.map$/.test(r));
   if (files.length !== 1) throw new Error('Cannot determine migration file');
@@ -35,9 +35,10 @@ export const getModMigration = (name: string) => {
 export const sortMods = (name: string, includeRoot: boolean) => {
   const mods = getModJsons();
   const rootMod = mods[name];
+  console.log({ mods, rootMod, });
   const modSubset: { [key: string]: ModJson, } = includeRoot ? { name: rootMod, } : {};
   const processMod = (mod: ModJson) => {
-    mod.dependencies.forEach(dep => {
+    mod.dependencies.map(d => d.split('@')[0]).forEach(dep => {
       const depMod = mods[dep];
       modSubset[dep] = depMod;
       processMod(depMod);
@@ -57,10 +58,10 @@ export const sortMods = (name: string, includeRoot: boolean) => {
     if (!m) break;
     if (
       (m.dependencies.length ?? 0) === 0 ||
-      m.dependencies.every(dep => sortedModuleNames[dep])
+      m.dependencies.map(d => d.split('@')[0]).every(dep => sortedModuleNames[dep])
     ) {
-      if (!includeRoot && `${m.name}@${m.version}` === name) continue;
-      sortedModuleNames[`${m.name}@${m.version}`] = true;
+      if (!includeRoot && m.name === name) continue;
+      sortedModuleNames[m.name] = true;
       sortedModules.push(m);
     } else {
       moduleList.push(m);
