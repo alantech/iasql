42805ea9f8138ca6be5037dcf6986c898a994bb1
David Ellis <isv.damocles@gmail.com>
2022-05-03 10:00:04 -0500

    Fix lint

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,5 +1,8 @@
 import config from '../config'
 
+// No good way to re-export a require without this, and no way to determine what to import by the
+// config variable without require
+// tslint:disable-next-line:no-var-requires
 export const latest = require(`./${config.modules.latestVersion}`);
 
 export * as v0_0_1 from './0.0.1'
