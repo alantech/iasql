76515d68fc32b12f471e701af4ae6889f7d00d1d
Alejandro Guillen <aguillenv93@gmail.com>
2022-05-05 20:23:20 +0200

    Update migration scripts with module version

--- src/scripts/migrate-dep-order.ts
+++ src/scripts/migrate-dep-order.ts
@@ -1,16 +1,17 @@
 import { sortMods, getModMigration, } from './module-json-utils'
 import { TypeormWrapper, } from '../services/typeorm'
 
-const moduleName = process.argv[process.argv.length - 1];
+const moduleName = process.argv[process.argv.length - 2];
+const moduleVersion = process.argv[process.argv.length - 1];
 
-let sortedDeps = sortMods(moduleName, false);
+let sortedDeps = sortMods(moduleName, moduleVersion, false);
 
 // TODO: Remove this hackery once typeorm migration doesn't do weird alter table crap
 if (moduleName !== 'iasql_platform') {
   sortedDeps = sortedDeps.filter(d => d.name !== 'iasql_platform');
 }
 
-const entities = sortedDeps.map(d => `${__dirname}/../modules/0.0.1/${d.name}/entity/*.ts`) as any[]
+const entities = sortedDeps.map(d => `${__dirname}/../modules/${moduleVersion}/${d.name}/entity/*.ts`) as any[]
 
 (async () => {
   const conn = await TypeormWrapper.createConn('__example__', {
@@ -24,7 +25,7 @@ const entities = sortedDeps.map(d => `${__dirname}/../modules/0.0.1/${d.name}/en
 
   for (const dep of sortedDeps) {
     console.log(`Adding ${dep.name}...`);
-    const migrationClass = getModMigration(dep.name);
+    const migrationClass = getModMigration(dep.name, dep.version);
     await migrationClass.prototype.up(qr);
   }
   console.log('Done!');
