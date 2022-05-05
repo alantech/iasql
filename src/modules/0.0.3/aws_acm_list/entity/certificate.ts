a8503f064d477bee42069f2f39cd0eaeb14676e1
Alejandro Guillen <aguillenv93@gmail.com>
2022-05-05 20:24:31 +0200

    Add initial version of acm module list

---
+++ src/modules/0.0.3/aws_acm_list/entity/certificate.ts
@@ -0,0 +1,76 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm'
+
+import { cloudId, } from '../../../../services/cloud-id'
+
+export enum certificateStatusEnum {
+  EXPIRED = "EXPIRED",
+  FAILED = "FAILED",
+  INACTIVE = "INACTIVE",
+  ISSUED = "ISSUED",
+  PENDING_VALIDATION = "PENDING_VALIDATION",
+  REVOKED = "REVOKED",
+  VALIDATION_TIMED_OUT = "VALIDATION_TIMED_OUT",
+}
+
+export enum certificateTypeEnum {
+  AMAZON_ISSUED = "AMAZON_ISSUED",
+  IMPORTED = "IMPORTED",
+  // TODO: add private certs support
+  // PRIVATE = "PRIVATE", 
+}
+
+export enum certificateRenewalEligibilityEnum {
+  ELIGIBLE = "ELIGIBLE",
+  INELIGIBLE = "INELIGIBLE",
+}
+
+@Entity()
+export class Certificate {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: true,
+  })
+  @cloudId
+  arn?: string;
+
+  @Column({
+    nullable: true,
+  })
+  certificateId?: string;
+
+  @Column()
+  domainName: string;
+
+  @Column({
+    nullable: true,
+    type: 'enum',
+    enum: certificateTypeEnum,
+  })
+  certificateType?: certificateTypeEnum;
+
+  @Column({
+    nullable: true,
+    type: 'enum',
+    enum: certificateStatusEnum,
+  })
+  status?: certificateStatusEnum;
+
+  @Column({
+    nullable: true,
+    type: 'enum',
+    enum: certificateRenewalEligibilityEnum,
+  })
+  renewalEligibility?: certificateRenewalEligibilityEnum;
+
+  @Column({
+    default: false,
+  })
+  inUse: boolean;
+
+}
---
+++ src/modules/0.0.3/aws_acm_list/index.ts
@@ -0,0 +1,71 @@
+import { AWS, } from '../../../services/gateways/aws'
+import { Context, Crud, Mapper, Module, } from '../../interfaces'
+import { Certificate } from './entity'
+import * as metadata from './module.json'
+
+export const AwsAcmListModule: Module = new Module({
+  ...metadata,
+  utils: {
+    certificateMapper: (e: any) => {
+      const out = new Certificate();
+      if (!e?.CertificateArn) throw new Error('No CertificateArn defined');
+      out.arn = e.CertificateArn;
+      out.certificateId = e.Serial;
+      out.certificateType = e.Type;
+      out.domainName = e.DomainName;
+      out.inUse = !!e.InUseBy?.length;
+      out.renewalEligibility = e.RenewalEligibility;
+      out.status = e.Status;
+      return out;
+    },
+  },
+  mappers: {
+    certificate: new Mapper<Certificate>({
+      entity: Certificate,
+      entityId: (e: Certificate) => e.arn ?? e.id.toString(),
+      equals: (a: Certificate, b: Certificate) => Object.is(a.certificateId, b.certificateId) &&
+        Object.is(a.certificateType, b.certificateType) &&
+        Object.is(a.domainName, b.domainName) &&
+        Object.is(a.inUse, b.inUse) &&
+        Object.is(a.renewalEligibility, b.renewalEligibility) &&
+        Object.is(a.status, b.status),
+      source: 'db', // TODO: source cloud??
+      cloud: new Crud({
+        create: async (es: Certificate[], ctx: Context) => {
+          // Do not cloud create, just restore database
+          await AwsAcmListModule.mappers.certificate.db.delete(es, ctx);
+        },
+        read: async (ctx: Context, ids?: string[]) => {
+          const client = await ctx.getAwsClient() as AWS;
+          let out = [];
+          if (Array.isArray(ids)) {
+            for (const id of ids) {
+              out.push(await client.getCertificate(id));
+            }
+          } else {
+            out = (await client.getCertificates()) ?? [];
+          }
+          return out.map(AwsAcmListModule.utils.certificateMapper);
+        },
+        updateOrReplace: () => 'update',
+        update: async (es: Certificate[], ctx: Context) => {
+          // Right now we can only modify AWS-generated fields in the database.
+          // This implies that on `update`s we only have to restore the values for those records.
+          const out = [];
+          for (const e of es) {
+            const cloudRecord = ctx?.memo?.cloud?.Certificate?.[e.arn ?? ''];
+            await AwsAcmListModule.mappers.certificate.db.update(cloudRecord, ctx);
+            out.push(cloudRecord);
+          }
+          return out;
+        },
+        delete: async (es: Certificate[], ctx: Context) => {
+          const client = await ctx.getAwsClient() as AWS;
+          for (const e of es) {
+            await client.deleteCertificate(e.arn ?? '');
+          }
+        },
+      }),
+    }),
+  },
+}, __dirname);
