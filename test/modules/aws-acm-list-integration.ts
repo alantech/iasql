8d8662d7894e7476ff34bd9cfdac77d8d3adf1f4
Alejandro Guillen <aguillenv93@gmail.com>
2022-05-06 10:14:08 +0200

    Add simple tests

---
+++ test/modules/aws-acm-list-integration.ts
@@ -0,0 +1,109 @@
+import * as iasql from '../../src/services/iasql'
+import { getPrefix, runQuery, runApply, finish, execComposeUp, execComposeDown, runSync, runInstall, runUninstall } from '../helpers'
+
+const prefix = getPrefix();
+const dbAlias = 'acmlisttest';
+const domainName = `${prefix}${dbAlias}.com`;
+
+const apply = runApply.bind(null, dbAlias);
+const sync = runSync.bind(null, dbAlias);
+const query = runQuery.bind(null, dbAlias);
+const install = runInstall.bind(null, dbAlias);
+const uninstall = runUninstall.bind(null, dbAlias);
+const modules = ['aws_acm_list'];
+
+jest.setTimeout(240000);
+beforeAll(async () => await execComposeUp());
+afterAll(async () => await execComposeDown(modules));
+
+describe('AwsAcmList Integration Testing', () => {
+  it('creates a new test db', (done) => void iasql.connect(
+    dbAlias,
+    'not-needed', 'not-needed').then(...finish(done)));
+
+  it('installs the aws_account module', install(['aws_account']));
+
+  it('inserts aws credentials', query(`
+    INSERT INTO aws_account (region, access_key_id, secret_access_key)
+    VALUES ('${process.env.AWS_REGION}', '${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `));
+
+  it('installs the acm_list module', install(modules));
+
+  it('adds a new certificate', query(`
+    INSERT INTO certificate (domain_name)
+    VALUES ('${domainName}');
+  `));
+
+  it('sync before apply (should restore)', sync());
+
+  it('check no new certificate', query(`
+    SELECT *
+    FROM certificate
+    WHERE domain_name = '${domainName}';
+  `, (res: any[]) => expect(res.length).toBe(0)));
+    
+  it('adds a new certificate', query(`
+    INSERT INTO certificate (domain_name)
+    VALUES ('${domainName}');
+  `));
+
+  it('check adds new certificate', query(`
+    SELECT *
+    FROM certificate
+    WHERE domain_name = '${domainName}';
+  `, (res: any[]) => expect(res.length).toBe(1)));
+
+  it('applies the new certificate (should delete the record)', apply());
+
+  it('check adds new certificate', query(`
+    SELECT *
+    FROM certificate
+    WHERE domain_name = '${domainName}';
+  `, (res: any[]) => expect(res.length).toBe(0)));
+
+  it('uninstalls the acm_list module', uninstall(modules));
+
+  it('installs the acm_list module', install(modules));
+
+  it('check count after uninstall/install', query(`
+    SELECT *
+    FROM certificate
+    WHERE domain_name = '${domainName}';
+  `, (res: any[]) => expect(res.length).toBe(0)));
+
+  it('deletes the test db', (done) => void iasql
+    .disconnect(dbAlias, 'not-needed')
+    .then(...finish(done)));
+});
+
+describe('AwsAcmList install/uninstall', () => {
+  it('creates a new test db', (done) => void iasql.connect(
+    dbAlias,
+    'not-needed', 'not-needed').then(...finish(done)));
+
+  it('installs the aws_account module', install(['aws_account']));
+
+  it('inserts aws credentials', query(`
+    INSERT INTO aws_account (region, access_key_id, secret_access_key)
+    VALUES ('us-east-1', '${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `));
+
+  it('installs the modules', install(modules));
+
+  it('uninstalls the modules', uninstall(modules));
+
+  it('installs all modules', (done) => void iasql.install(
+    [],
+    dbAlias,
+    'postgres',
+    true).then(...finish(done)));
+
+  it('uninstalls the module', uninstall(modules));
+
+  it('installs the module', install(modules));
+
+  it('deletes the test db', (done) => void iasql
+    .disconnect(dbAlias, 'not-needed')
+    .then(...finish(done)));
+});
