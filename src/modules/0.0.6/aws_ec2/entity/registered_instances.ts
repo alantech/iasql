002fca056b14b7b33571887e2b485ff02466e34d
Alejandro Guillen <aguillenv93@gmail.com>
2022-05-25 11:14:41 +0200

    Update ec2@0.0.6

---
+++ src/modules/0.0.6/aws_ec2/entity/registered_instances.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
+
+import { Instance } from '../../aws_ec2/entity';
+import { TargetGroup } from '../../aws_elb/entity';
+
+// TODO: add constraint to only join with target groups with type 'instance'  @Check(`target_group.target_type = "instance"`)
+@Unique(['instance', 'targetGroup'])
+@Entity()
+export class RegisteredInstances {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => Instance, instance => instance.id)
+  @JoinColumn({ name: 'instance', })
+  instance: Instance;
+
+  @ManyToOne(() => TargetGroup, targetGroup => targetGroup.targetGroupName)
+  @JoinColumn({ name: 'target_group', })
+  targetGroup: TargetGroup;
+}
