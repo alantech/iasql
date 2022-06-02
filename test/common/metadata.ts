aff6ac285040218e4b962d77194ad72e53268717
luisfer <lfdepombo@gmail.com>
2022-06-02 10:22:12 -0400

    exclude iasql_ tables from rec count and add op count to iasql_databases (#816)
    * exclude iasql_ tables from rec count and add op count to iasql_databases
    
    * fix typo

--- test/common/metadata.ts
+++ test/common/metadata.ts
@@ -1,18 +1,21 @@
 import * as iasql from '../../src/services/iasql'
-import { runQuery, finish, execComposeUp, execComposeDown } from '../helpers'
+import { runApply, runInstall, runQuery, finish, execComposeUp, execComposeDown } from '../helpers'
 import MetadataRepo from '../../src/services/repositories/metadata'
 
-const query = runQuery.bind(null, 'iasql_metadata');
+const metadataQuery = runQuery.bind(null, 'iasql_metadata');
 const dbAlias = 'metadatatest';
-const uid = '12345'
-const email = 'test@example.com'
+const uid = '12345';
+const email = 'test@example.com';
+const dbQuery = runQuery.bind(null, dbAlias);
+const install = runInstall.bind(null, dbAlias);
+const apply = runApply.bind(null, dbAlias);
 
 jest.setTimeout(240000);
 beforeAll(async () => await execComposeUp());
 afterAll(async () => await execComposeDown());
 
 describe('Testing metadata repo', () => {
-  it('no users should exist', query(`
+  it('no users should exist', metadataQuery(`
     SELECT *
     FROM iasql_user
   `, (row: any[]) => expect(row.length).toBe(0)));
@@ -20,12 +23,12 @@ describe('Testing metadata repo', () => {
   it('list dbs', (done) => void MetadataRepo.getDbs(
     uid, email).then(...finish(done)));
 
-  it('user should exist after listing databases', query(`
+  it('user should exist after listing databases', metadataQuery(`
     SELECT *
     FROM iasql_user;
   `, (row: any[]) => expect(row.length).toBe(1)));
 
-  it('no database should exist', query(`
+  it('no database should exist', metadataQuery(`
     SELECT *
     FROM iasql_database;
   `, (row: any[]) => expect(row.length).toBe(0)));
@@ -34,13 +37,25 @@ describe('Testing metadata repo', () => {
     dbAlias,
     uid, email).then(...finish(done)));
 
-  it('check row in iasql database table exists', query(`
+  it('check row in iasql database table exists', metadataQuery(`
     SELECT *
     FROM iasql_database
     WHERE pg_name = '${dbAlias}';
   `, (row: any[]) => expect(row.length).toBe(1)));
 
-  it('check row in join table exists', query(`
+  it('check op db count', metadataQuery(`
+    SELECT operation_count
+    FROM iasql_database
+    WHERE pg_name = '${dbAlias}';
+  `, (row: any[]) => expect(row[0].operation_count).toBe(0)));
+
+  it('check rec db count', metadataQuery(`
+    SELECT record_count
+    FROM iasql_database
+    WHERE pg_name = '${dbAlias}';
+  `, (row: any[]) => expect(row[0].record_count).toBe(0)));
+
+  it('check row in join table exists', metadataQuery(`
     SELECT *
     FROM iasql_user_databases
     WHERE iasql_database_pg_name = '${dbAlias}';
@@ -50,17 +65,44 @@ describe('Testing metadata repo', () => {
     expect(user.id).toBe(uid);
   }));
 
+  it('installs the aws_account module', install(['aws_account']));
+
+  it('check op db count', metadataQuery(`
+    SELECT operation_count
+    FROM iasql_database
+    WHERE pg_name = '${dbAlias}';
+  `, (row: any[]) => expect(row[0].operation_count).toBe(1)));
+
+  it('inserts aws credentials', dbQuery(`
+    INSERT INTO aws_account (region, access_key_id, secret_access_key)
+    VALUES ('us-east-1', '${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `));
+
+  it('apply updates db counts', apply());
+
+  it('check op db count', metadataQuery(`
+    SELECT operation_count
+    FROM iasql_database
+    WHERE pg_name = '${dbAlias}';
+  `, (row: any[]) => expect(row[0].operation_count).toBe(2)));
+
+  it('check rec db count', metadataQuery(`
+    SELECT record_count
+    FROM iasql_database
+    WHERE pg_name = '${dbAlias}';
+  `, (row: any[]) => expect(row[0].record_count).toBe(1)));
+
   it('deletes the test db', (done) => void iasql
     .disconnect(dbAlias, uid)
     .then(...finish(done)));
 
-  it('check there is no row in iasql database', query(`
+  it('check there is no row in iasql database', metadataQuery(`
     SELECT *
     FROM iasql_database
     WHERE pg_name = '${dbAlias}';
   `, (row: any[]) => expect(row.length).toBe(0)));
 
-  it('check there is no row in join table', query(`
+  it('check there is no row in join table', metadataQuery(`
     SELECT *
     FROM iasql_user_databases
     WHERE iasql_database_pg_name = '${dbAlias}';
