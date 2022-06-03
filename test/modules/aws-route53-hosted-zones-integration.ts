edac9f669e2ebab80e5955a0b0e616ffcd05bf28
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-03 17:03:03 +0200

    Update test

--- test/modules/aws-route53-hosted-zones-integration.ts
+++ test/modules/aws-route53-hosted-zones-integration.ts
@@ -149,18 +149,20 @@ describe('Route53 Integration Testing', () => {
     WHERE domain_name = '${domainName}';
   `, (res: any[]) => expect(res.length).toBe(3)));
 
-  // Load balancer
-  it('adds a new load balancer', query(`
-    INSERT INTO load_balancer (load_balancer_name, scheme, load_balancer_type, ip_address_type)
-    VALUES ('${lbName}', '${lbScheme}', '${lbType}', '${lbIPAddressType}');
-  `));
-
   it('adds a new A record to hosted zone', query(`
-    INSERT INTO resource_record_set (name, record_type, ttl, parent_hosted_zone_id, alias_target_id)
-    SELECT '${resourceRecordSetName}', '${resourceRecordSetTypeA}', ${resourceRecordSetTtl}, hosted_zone.id, alias_target.id
-    FROM hosted_zone, alias_target
-    INNER JOIN load_balancer ON load_balancer.load_balancer_name = alias_target.load_balancer_name
-    WHERE domain_name = '${domainName}' AND load_balancer.load_balancer_name = '${lbName}';
+    BEING;
+      INSERT INTO load_balancer (load_balancer_name, scheme, load_balancer_type, ip_address_type)
+      VALUES ('${lbName}', '${lbScheme}', '${lbType}', '${lbIPAddressType}');
+
+      INSERT INTO alias_target (load_balancer_name)
+      VALUES ('${lbName}');
+
+      INSERT INTO resource_record_set (name, record_type, ttl, parent_hosted_zone_id, alias_target_id)
+      SELECT '${resourceRecordSetName}', '${resourceRecordSetTypeA}', ${resourceRecordSetTtl}, hosted_zone.id, alias_target.id
+      FROM hosted_zone, alias_target
+      INNER JOIN load_balancer ON load_balancer.load_balancer_name = alias_target.load_balancer_name
+      WHERE domain_name = '${domainName}' AND load_balancer.load_balancer_name = '${lbName}';
+    COMMIT;
   `));
 
   it('check alias target record has been added', query(`
