69e758ca8402bae6c10817c5d11890cd451569e0
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-06 19:33:12 +0200

    Add 0.0.8

---
+++ src/modules/0.0.8/aws_account/migration/1644463885917-aws_account.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class awsAccount1644463885917 implements MigrationInterface {
+    name = 'awsAccount1644463885917'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "aws_account" ("id" SERIAL NOT NULL, "access_key_id" character varying NOT NULL, "secret_access_key" character varying NOT NULL, "region" character varying NOT NULL, CONSTRAINT "PK_9ad897024d8c36c541d7fe84084" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "aws_account"`);
+    }
+
+}
