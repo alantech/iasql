eee76b59b2c2aee26ee0cdfe357bb97dd2991067
David Ellis <isv.damocles@gmail.com>
2022-06-13 18:30:31 -0500

    Prepare development of v0.0.9 and drop v0.0.5

--- src/services/iasql.ts
+++ src/services/iasql.ts
@@ -1058,7 +1058,7 @@ export async function upgrade(dbId: string, dbUser: string) {
       }
     })();
     // TODO: Drop this conditional once these versions are no longer supported.
-    if (['v0_0_5', 'v0_0_6', 'v0_0_7'].includes(versionString)) {
+    if (['v0_0_6', 'v0_0_7'].includes(versionString)) {
       throw new Error('Upgrading. Please disconnect and reconnect to the database');
     } else {
       return 'Upgrading. Please disconnect and reconnect to the database';
