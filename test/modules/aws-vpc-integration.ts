dd547fa62458ffcd6e083aa58a24ef4a934e3c3c
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-16 17:12:01 +0200

    Private NAT Gateway (#855)
    * Extract vpc and subnet
    
    * Add nat gateway table
    
    * Add nat gateway logic
    
    * Update constraint
    
    * Fix constraint
    
    * Fix creation
    
    * Filter some nat states
    
    * Inverse filter
    
    * Remove public ip flow for now
    
    * Fix creation
    
    * add vpc tests
    
    * Remove logger
    
    * Fix lint
    
    * Update security group tests
    
    * Restore aws_vpc@0.0.8
    
    * Add nat gateway to aws_vpc@0.0.9
    
    * Simplify ec2 client usage

--- test/modules/aws-vpc-integration.ts
+++ test/modules/aws-vpc-integration.ts
@@ -1,8 +1,10 @@
 import config from '../../src/config';
 import * as iasql from '../../src/services/iasql'
-import { runQuery, runInstall, runUninstall, runApply, finish, execComposeUp, execComposeDown, runSync, } from '../helpers'
+import { runQuery, runInstall, runUninstall, runApply, finish, execComposeUp, execComposeDown, runSync, getPrefix, } from '../helpers'
 
+const prefix = getPrefix();
 const dbAlias = 'vpctest';
+const ng = `${prefix}${dbAlias}-ng`;
 const apply = runApply.bind(null, dbAlias);
 const sync = runSync.bind(null, dbAlias);
 const query = runQuery.bind(null, dbAlias);
@@ -57,11 +59,35 @@ describe('VPC Integration Testing', () => {
 
   it('applies the subnet change', apply());
 
+  it('adds a new vpc', query(`  
+    INSERT INTO vpc (cidr_block)
+    VALUES ('192.${randIPBlock}.0.0/16');
+  `));
+
+  it('applies the vpc change', apply());
+
+  it('adds a nat gateway', query(`
+    INSERT INTO nat_gateway (connectivity_type, subnet_id, tags)
+    SELECT 'private', id, '{"Name":"${ng}"}'
+    FROM subnet
+    WHERE cidr_block = '192.${randIPBlock}.0.0/16';
+  `));
+
+  it('applies the nat gateway change', apply());
+
+  it('check nat gateway count', query(`
+    SELECT * FROM nat_gateway WHERE tags ->> 'Name' = '${ng}';
+  `, (res: any) => expect(res.length).toBe(1)));
+
   it('uninstalls the vpc module', uninstall(
     modules));
 
   it('installs the vpc module again (to make sure it reloads stuff)', install(
     modules));
+
+  it('check nat gateway count', query(`
+    SELECT * FROM nat_gateway WHERE tags ->> 'Name' = '${ng}';
+  `, (res: any) => expect(res.length).toBe(1)));
 
   it('queries the subnets to confirm the record is present', query(`
     SELECT * FROM subnet WHERE cidr_block = '192.${randIPBlock}.0.0/16'
@@ -70,6 +96,33 @@ describe('VPC Integration Testing', () => {
   it('queries the vpcs to confirm the record is present', query(`
     SELECT * FROM vpc WHERE cidr_block = '192.${randIPBlock}.0.0/16'
   `, (res: any) => expect(res.length).toBeGreaterThan(0)));
+
+  it('updates a nat gateway', query(`
+    UPDATE nat_gateway
+    SET state = 'failed'
+    WHERE tags ->> 'Name' = '${ng}';
+  `));
+
+  it('applies the nat gateway change', apply());
+
+  it('check nat gateway count', query(`
+    SELECT * FROM nat_gateway WHERE tags ->> 'Name' = '${ng}';
+  `, (res: any) => expect(res.length).toBe(1)));
+
+  it('check nat gateway state', query(`
+    SELECT * FROM nat_gateway WHERE tags ->> 'Name' = '${ng}';
+  `, (res: any) => expect(res[0]['state']).toBe('available')));
+
+  it('deletes a nat gateway', query(`
+    DELETE FROM nat_gateway
+    WHERE tags ->> 'Name' = '${ng}';
+  `));
+
+  it('applies the nat gateway change', apply());
+
+  it('check nat gateway count', query(`
+    SELECT * FROM nat_gateway WHERE tags ->> 'Name' = '${ng}';
+  `, (res: any) => expect(res.length).toBe(0)));
 
   it('deletes the subnet', query(`
     WITH vpc as (
