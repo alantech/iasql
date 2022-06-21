25fe0ff398d7671f8a85469ed08cde57582f6097
depombo <lfdepombo@gmail.com>
2022-06-20 23:42:35 -0700

    fix test and use sdk type

--- src/modules/0.0.9/aws_ec2/index.ts
+++ src/modules/0.0.9/aws_ec2/index.ts
@@ -1,4 +1,4 @@
-import { Instance as AWSInstance, RunInstancesCommandInput, Tag as AWSTag } from '@aws-sdk/client-ec2'
+import { Instance as AWSInstance, InstanceLifecycle, RunInstancesCommandInput, Tag as AWSTag } from '@aws-sdk/client-ec2'
 
 import { Instance, RegisteredInstance, State, } from './entity'
 import { AwsSecurityGroupModule, } from '../aws_security_group'
@@ -112,7 +112,7 @@ export const AwsEc2Module: Module2 = new Module2({
           if (id) {
             const rawInstance = await client.getInstance(id);
             // exclude spot instances
-            if (!rawInstance || rawInstance.InstanceLifecycle === 'spot') return;
+            if (!rawInstance || rawInstance.InstanceLifecycle === InstanceLifecycle.SPOT) return;
             if (rawInstance.State?.Name === 'terminated' || rawInstance.State?.Name === 'shutting-down') return;
             return AwsEc2Module.utils.instanceMapper(rawInstance, ctx);
           } else {
