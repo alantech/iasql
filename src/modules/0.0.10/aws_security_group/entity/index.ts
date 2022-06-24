aaa317e2f7d3b8aa0780caf7cf94025a70ff45c1
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-24 11:39:34 +0200

    Use hook

--- src/modules/0.0.10/aws_security_group/entity/index.ts
+++ src/modules/0.0.10/aws_security_group/entity/index.ts
@@ -121,4 +121,14 @@ export class SecurityGroupRule {
     nullable: true,
   })
   description?: string;
+  
+  @AfterLoad()
+  @AfterInsert()
+  @AfterUpdate()
+  updateNulls() {
+    const that: any = this;
+    Object.keys(this).forEach(k => {
+      if (that[k] === null) that[k] = undefined;
+    });
+  }
 }
