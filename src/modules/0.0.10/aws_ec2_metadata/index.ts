99b6fe5ef3b91ed10c83a33aa2c8a9552d01f08f
David Ellis <isv.damocles@gmail.com>
2022-06-24 22:20:50 -0500

    Update ec2 metadata module

--- src/modules/0.0.10/aws_ec2_metadata/index.ts
+++ src/modules/0.0.10/aws_ec2_metadata/index.ts
@@ -1,10 +1,28 @@
-import { Instance as AWSInstance } from '@aws-sdk/client-ec2'
+import {
+  EC2,
+  Instance as AWSInstance,
+  paginateDescribeInstances,
+  DescribeInstanceTypesCommandOutput,
+} from '@aws-sdk/client-ec2'
 
 import { AwsEc2Module, } from '../aws_ec2'
 import { Architecture, InstanceMetadata, RootDeviceType } from './entity'
-import { AWS } from '../../../services/gateways/aws_2'
+import { AWS, crudBuilder, paginateBuilder, } from '../../../services/aws_macros'
 import { Context, Crud2, Mapper2, Module2, } from '../../interfaces'
 import * as metadata from './module.json'
+
+const getInstanceType = crudBuilder<EC2>('describeInstanceTypes', (instanceType: string) => ({
+  InstanceTypes: [instanceType],
+}), (res: DescribeInstanceTypesCommandOutput) => res.InstanceTypes?.[0]);
+const describeInstances = crudBuilder<EC2>(
+  'describeInstances',
+  (InstanceIds: string[]) => ({ InstanceIds, }),
+);
+const getInstance = async (client: EC2, id: string) => {
+  const reservations = await describeInstances(client, [id]);
+  return (reservations?.Reservations?.map((r: any) => r.Instances) ?? []).pop()?.pop();
+}
+const getInstances = paginateBuilder<EC2>(paginateDescribeInstances, 'Instances', 'Reservations');
 
 export const AwsEc2MetadataModule: Module2 = new Module2({
   ...metadata,
@@ -26,7 +44,7 @@ export const AwsEc2MetadataModule: Module2 = new Module2({
       out.launchTime = instance.LaunchTime as Date;
       out.cpuCores = instance.CpuOptions?.CoreCount as number;
       if (!instance.InstanceType) return undefined;
-      const instanceType = await client.getInstanceType(instance.InstanceType);
+      const instanceType = await getInstanceType(client.ec2client, instance.InstanceType);
       out.memSizeMB = instanceType?.MemoryInfo?.SizeInMiB as number;
       out.ebsOptimized = instance.EbsOptimized ?? false;
       out.rootDeviceName = instance.RootDeviceName ?? '';
@@ -51,12 +69,12 @@ export const AwsEc2MetadataModule: Module2 = new Module2({
         read: async (ctx: Context, id?: string) => {
           const client = await ctx.getAwsClient() as AWS;
           if (id) {
-            const rawInstance = await client.getInstance(id);
+            const rawInstance = await getInstance(client.ec2client, id);
             if (!rawInstance) return;
             if (rawInstance.State?.Name === 'terminated' || rawInstance.State?.Name === 'shutting-down') return;
             return AwsEc2MetadataModule.utils.instanceMetadataMapper(rawInstance, ctx);
           } else {
-            const rawInstances = (await client.getInstances()).Instances ?? [];
+            const rawInstances = (await getInstances(client.ec2client)) ?? [];
             const out = [];
             for (const i of rawInstances) {
               if (i?.State?.Name === 'terminated' || i?.State?.Name === 'shutting-down') continue;
