794d5bd7296057457848eed7be55c31607533f97
David Ellis <isv.damocles@gmail.com>
2022-06-30 14:06:25 -0500

    Macroify ELB

--- src/modules/0.0.11/aws_elb/index.ts
+++ src/modules/0.0.11/aws_elb/index.ts
@@ -1,12 +1,25 @@
 import {
   CreateListenerCommandInput,
   CreateLoadBalancerCommandInput,
+  DescribeLoadBalancersCommandInput,
+  ElasticLoadBalancingV2,
   Listener as ListenerAws,
   LoadBalancer as LoadBalancerAws,
   ModifyListenerCommandInput,
+  TargetGroup as TargetGroupAws,
+  paginateDescribeListeners,
+  paginateDescribeLoadBalancers,
+  paginateDescribeTargetGroups,
 } from '@aws-sdk/client-elastic-load-balancing-v2'
+import {
+  EC2,
+  paginateDescribeSubnets,
+  DescribeNetworkInterfacesCommandInput,
+  paginateDescribeVpcs,
+} from '@aws-sdk/client-ec2'
+import { createWaiter, WaiterState } from '@aws-sdk/util-waiter'
 
-import { AWS, } from '../../../services/gateways/aws_2'
+import { AWS, crudBuilder2, crudBuilderFormat, paginateBuilder, mapLin, } from '../../../services/aws_macros'
 import {
   ActionTypeEnum,
   Listener,
@@ -25,6 +38,203 @@ import { AwsVpcModule, } from '../aws_vpc'
 import { Context, Crud2, Mapper2, Module2, } from '../../interfaces'
 import { AwsAcmListModule, AwsSecurityGroupModule } from '..'
 import * as metadata from './module.json'
+
+const createListener = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'createListener',
+  ListenerAws | undefined
+>(
+  'createListener',
+  (input) => input,
+  (res) => res?.Listeners?.pop(),
+);
+const getListener = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'describeListeners',
+  ListenerAws | undefined
+>(
+  'describeListeners',
+  (arn) => ({ ListenerArns: [arn], }),
+  (res) => res?.Listeners?.[0],
+);
+const getListenersForArn = paginateBuilder<ElasticLoadBalancingV2>(
+  paginateDescribeListeners,
+  'Listeners',
+  undefined,
+  undefined,
+  (LoadBalancerArn) => ({ LoadBalancerArn, }),
+);
+const getListeners = async (client: ElasticLoadBalancingV2, loadBalancerArns: string[]) => (
+  await mapLin(loadBalancerArns, getListenersForArn.bind(null, client))
+).flat();
+const updateListener = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'modifyListener',
+  ListenerAws | undefined
+>(
+  'modifyListener',
+  (input) => input,
+  (res) => res?.Listeners?.pop(),
+);
+const deleteListener = crudBuilder2<ElasticLoadBalancingV2, 'deleteListener'>(
+  'deleteListener',
+  (ListenerArn) => ({ ListenerArn, }),
+);
+const getSubnets = paginateBuilder<EC2>(paginateDescribeSubnets, 'Subnets');
+const getLoadBalancer = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'describeLoadBalancers',
+  LoadBalancerAws | undefined
+>(
+  'describeLoadBalancers',
+  (arn) => ({ LoadBalancerArns: [arn], }),
+  (res) => res?.LoadBalancers?.[0],
+);
+const getLoadBalancers = paginateBuilder<ElasticLoadBalancingV2>(
+  paginateDescribeLoadBalancers,
+  'LoadBalancers',
+);
+const updateLoadBalancerIpAddressType = crudBuilder2<ElasticLoadBalancingV2, 'setIpAddressType'>(
+  'setIpAddressType',
+  (input) => input,
+);
+const updateLoadBalancerSubnets = crudBuilder2<ElasticLoadBalancingV2, 'setSubnets'>(
+  'setSubnets',
+  (input) => input,
+);
+const updateLoadBalancerSecurityGroups = crudBuilder2<ElasticLoadBalancingV2, 'setSecurityGroups'>(
+  'setSecurityGroups',
+  (input) => input,
+);
+const getVpcs = paginateBuilder<EC2>(paginateDescribeVpcs, 'Vpcs');
+const createTargetGroup = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'createTargetGroup',
+  TargetGroupAws | undefined
+>(
+  'createTargetGroup',
+  (input) => input,
+  (res) => res?.TargetGroups?.pop(),
+);
+const getTargetGroup = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'describeTargetGroups',
+  TargetGroupAws | undefined
+>(
+  'describeTargetGroups',
+  (arn) => ({ TargetGroupArns: [arn], }),
+  (res) => res?.TargetGroups?.[0],
+);
+const getTargetGroups = paginateBuilder<ElasticLoadBalancingV2>(
+  paginateDescribeTargetGroups,
+  'TargetGroups',
+);
+const updateTargetGroup = crudBuilderFormat<
+  ElasticLoadBalancingV2,
+  'modifyTargetGroup',
+  TargetGroupAws | undefined
+>(
+  'modifyTargetGroup',
+  (input) => input,
+  (res) => res?.TargetGroups?.pop(),
+);
+const deleteTargetGroup = crudBuilder2<ElasticLoadBalancingV2, 'deleteTargetGroup'>(
+  'deleteTargetGroup',
+  (TargetGroupArn) => ({ TargetGroupArn, }),
+);
+
+// TODO: Create a waiter macro function
+async function createLoadBalancer(
+  client: ElasticLoadBalancingV2,
+  input: CreateLoadBalancerCommandInput
+) {
+  const create = await client.createLoadBalancer(input);
+  let loadBalancer = create?.LoadBalancers?.pop() ?? null;
+  if (!loadBalancer) return loadBalancer;
+  const waiterInput: DescribeLoadBalancersCommandInput = {
+    LoadBalancerArns: [loadBalancer?.LoadBalancerArn!],
+  };
+  // TODO: should we use the paginator instead?
+  await createWaiter<ElasticLoadBalancingV2, DescribeLoadBalancersCommandInput>(
+    {
+      client,
+      // all in seconds
+      maxWaitTime: 600,
+      minDelay: 1,
+      maxDelay: 4,
+    },
+    waiterInput,
+    async (cl, cmd) => {
+      try {
+        const data = await cl.describeLoadBalancers(cmd);
+        for (const lb of data?.LoadBalancers ?? []) {
+          if (lb.State?.Code !== 'active')
+            return { state: WaiterState.RETRY };
+          loadBalancer = lb;
+        }
+        return { state: WaiterState.SUCCESS };
+      } catch (e: any) {
+        return { state: WaiterState.RETRY };
+      }
+    },
+  );
+  return loadBalancer;
+}
+// TODO: Really refactor the client access in this thing later
+async function deleteLoadBalancer(client: { elbClient: ElasticLoadBalancingV2, ec2client: EC2, }, arn: string) {
+  await client.elbClient.deleteLoadBalancer({ LoadBalancerArn: arn, });
+  // We wait it is completely deleted to avoid issues deleting dependent resources.
+  const input: DescribeLoadBalancersCommandInput = { LoadBalancerArns: [arn], };
+  await createWaiter<ElasticLoadBalancingV2, DescribeLoadBalancersCommandInput>(
+    {
+      client: client.elbClient,
+      // all in seconds
+      maxWaitTime: 400,
+      minDelay: 1,
+      maxDelay: 4,
+    },
+    input,
+    async (cl, cmd) => {
+      try {
+        await cl.describeLoadBalancers(cmd);
+        return { state: WaiterState.RETRY };
+      } catch (_) {
+        return { state: WaiterState.SUCCESS };
+      }
+    },
+  );
+  // Now we need wait the load balancer to be fully deattached from any network interface
+  const loadBalancerName = arn.split(':loadbalancer/')?.[1] ?? '';
+  const describeEniCommand: DescribeNetworkInterfacesCommandInput = {
+    Filters: [
+      {
+        Name: 'description',
+        Values: [`*${loadBalancerName}`]
+      }
+    ]
+  };
+  await createWaiter<EC2, DescribeNetworkInterfacesCommandInput>(
+    {
+      client: client.ec2client,
+      // all in seconds
+      maxWaitTime: 1200,
+      minDelay: 1,
+      maxDelay: 4,
+    },
+    describeEniCommand,
+    async (cl, cmd) => {
+      try {
+        const eni = await cl.describeNetworkInterfaces(cmd);
+        if (loadBalancerName && eni.NetworkInterfaces?.length) {
+          return { state: WaiterState.RETRY };
+        }
+        return { state: WaiterState.SUCCESS };
+      } catch (e) {
+        return { state: WaiterState.RETRY };
+      }
+    },
+  );
+}
 
 export const AwsElbModule: Module2 = new Module2({
   ...metadata,
@@ -140,13 +350,13 @@ export const AwsElbModule: Module2 = new Module2({
                 CertificateArn: e.certificate.arn
               }];
             }
-            const result = await client.createListener(listenerInput);
+            const result = await createListener(client.elbClient, listenerInput);
             // TODO: Handle if it fails (somehow)
             if (!result?.hasOwnProperty('ListenerArn')) { // Failure
               throw new Error('what should we do here?');
             }
             // Re-get the inserted record to get all of the relevant records we care about
-            const newObject = await client.getListener(result.ListenerArn ?? '');
+            const newObject = await getListener(client.elbClient, result.ListenerArn ?? '');
             // We map this into the same kind of entity as `obj`
             const newEntity = await AwsElbModule.utils.listenerMapper(newObject, ctx);
             // We attach the original object's ID to this new one, indicating the exact record it is
@@ -161,7 +371,7 @@ export const AwsElbModule: Module2 = new Module2({
         read: async (ctx: Context, id?: string) => {
           const client = await ctx.getAwsClient() as AWS;
           if (id) {
-            const rawListener = await client.getListener(id);
+            const rawListener = await getListener(client.elbClient, id);
             if (!rawListener) return;
             return await AwsElbModule.utils.listenerMapper(rawListener, ctx);
           } else {
@@ -171,7 +381,7 @@ export const AwsElbModule: Module2 = new Module2({
                 Object.values(ctx.memo?.cloud?.LoadBalancer) :
                 await AwsElbModule.mappers.loadBalancer.cloud.read(ctx);
               const loadBalancerArns = loadBalancers.map((lb: any) => lb.loadBalancerArn);
-              return (await client.getListeners(loadBalancerArns)).Listeners;
+              return (await getListeners(client.elbClient, loadBalancerArns));
             })();
             const out = [];
             for (const l of listeners) {
@@ -205,7 +415,7 @@ export const AwsElbModule: Module2 = new Module2({
                   CertificateArn: e.certificate.arn
                 }];
               }
-              const updatedListener = await client.updateListener(listenerInput);
+              const updatedListener = await updateListener(client.elbClient, listenerInput);
               out.push(AwsElbModule.utils.listenerMapper(updatedListener, ctx));
             } else {
               // We need to delete the current cloud record and create the new one.
@@ -219,7 +429,7 @@ export const AwsElbModule: Module2 = new Module2({
         delete: async (es: Listener[], ctx: Context) => {
           const client = await ctx.getAwsClient() as AWS;
           for (const e of es) {
-            await client.deleteListener(e.listenerArn!);
+            await deleteListener(client.elbClient, e.listenerArn!);
           }
         },
       }),
@@ -248,7 +458,7 @@ export const AwsElbModule: Module2 = new Module2({
       cloud: new Crud2({
         create: async (es: LoadBalancer[], ctx: Context) => {
           const client = await ctx.getAwsClient() as AWS;
-          const subnets = (await client.getSubnets()).Subnets.map(s => s.SubnetId ?? '');
+          const subnets = (await getSubnets(client.ec2client)).map(s => s.SubnetId ?? '');
           const out = [];
           for (const e of es) {
             const securityGroups = e.securityGroups?.map(sg => {
@@ -266,13 +476,13 @@ export const AwsElbModule: Module2 = new Module2({
             if (e.loadBalancerType === LoadBalancerTypeEnum.APPLICATION) {
               input.SecurityGroups = securityGroups;
             }
-            const result = await client.createLoadBalancer(input);
+            const result = await createLoadBalancer(client.elbClient, input);
             // TODO: Handle if it fails (somehow)
             if (!result?.hasOwnProperty('LoadBalancerArn')) { // Failure
               throw new Error('what should we do here?');
             }
             // Re-get the inserted record to get all of the relevant records we care about
-            const newObject = await client.getLoadBalancer(result.LoadBalancerArn ?? '');
+            const newObject = await getLoadBalancer(client.elbClient, result.LoadBalancerArn ?? '');
             // We map this into the same kind of entity as `obj`
             const newEntity = await AwsElbModule.utils.loadBalancerMapper(newObject, ctx);
             // Save the record back into the database to get the new fields updated
@@ -284,11 +494,11 @@ export const AwsElbModule: Module2 = new Module2({
         read: async (ctx: Context, id?: string) => {
           const client = await ctx.getAwsClient() as AWS;
           if (id) {
-            const rawLoadBalancer = await client.getLoadBalancer(id);
+            const rawLoadBalancer = await getLoadBalancer(client.elbClient, id);
             if (!rawLoadBalancer) return;
             return await AwsElbModule.utils.loadBalancerMapper(rawLoadBalancer, ctx);
           } else {
-            const lbs = (await client.getLoadBalancers()).LoadBalancers;
+            const lbs = (await getLoadBalancers(client.elbClient));
             const out = [];
             for (const lb of lbs) {
               out.push(await AwsElbModule.utils.loadBalancerMapper(lb, ctx));
@@ -317,27 +527,30 @@ export const AwsElbModule: Module2 = new Module2({
             if (isUpdate) {
               // Update ip address type
               if (!Object.is(cloudRecord.ipAddressType, e.ipAddressType)) {
-                const updatedLoadBalancer = await client.updateLoadBalancerIPAddressType({
+                await updateLoadBalancerIpAddressType(client.elbClient, {
                   LoadBalancerArn: e.loadBalancerArn,
                   IpAddressType: e.ipAddressType,
                 });
+                const updatedLoadBalancer = await getLoadBalancer(client.elbClient, e.loadBalancerArn ?? '');
                 updatedRecord = AwsElbModule.utils.loadBalancerMapper(updatedLoadBalancer, ctx);
               }
               // Update subnets
               if (!(Object.is(cloudRecord.subnets?.length, e.subnets?.length)
                 && (cloudRecord.subnets?.every((csn: any) => !!e.subnets?.find(esn => Object.is(csn, esn))) ?? false))) {
-                const updatedLoadBalancer = await client.updateLoadBalancerSubnets({
+                await updateLoadBalancerSubnets(client.elbClient, {
                   LoadBalancerArn: e.loadBalancerArn,
                   Subnets: e.subnets?.filter(sn => !!sn),
                 });
+                const updatedLoadBalancer = await getLoadBalancer(client.elbClient, e.loadBalancerArn ?? '');
                 updatedRecord = AwsElbModule.utils.loadBalancerMapper(updatedLoadBalancer, ctx);
               }
               // Update security groups
               if (!(Object.is(cloudRecord.securityGroups?.length, e.securityGroups?.length) && (cloudRecord.securityGroups?.every((csg: any) => !!e.securityGroups?.find(esg => Object.is(csg.groupId, esg.groupId))) ?? false))) {
-                const updatedLoadBalancer = await client.updateLoadBalancerSecurityGroups({
+                await updateLoadBalancerSecurityGroups(client.elbClient, {
                   LoadBalancerArn: e.loadBalancerArn,
                   SecurityGroups: e.securityGroups?.filter(sg => !!sg.groupId).map(sg => sg.groupId!),
                 });
+                const updatedLoadBalancer = await getLoadBalancer(client.elbClient, e.loadBalancerArn ?? '');
                 updatedRecord = AwsElbModule.utils.loadBalancerMapper(updatedLoadBalancer, ctx);
               }
               await AwsElbModule.mappers.loadBalancer.db.update(updatedRecord, ctx);
@@ -354,7 +567,7 @@ export const AwsElbModule: Module2 = new Module2({
         delete: async (es: LoadBalancer[], ctx: Context) => {
           const client = await ctx.getAwsClient() as AWS;
           for (const e of es) {
-            await client.deleteLoadBalancer(e.loadBalancerArn!);
+            await deleteLoadBalancer({ ...client, }, e.loadBalancerArn!);
           }
         },
       }),
@@ -381,11 +594,11 @@ export const AwsElbModule: Module2 = new Module2({
       cloud: new Crud2({
         create: async (es: TargetGroup[], ctx: Context) => {
           const client = await ctx.getAwsClient() as AWS;
-          const vpcs = (await client.getVpcs()).Vpcs;
+          const vpcs = await getVpcs(client.ec2client);
           const defaultVpc = vpcs.find(vpc => vpc.IsDefault === true) ?? {};
           const out = [];
           for (const e of es) {
-            const result = await client.createTargetGroup({
+            const result = await createTargetGroup(client.elbClient, {
               Name: e.targetGroupName,
               TargetType: e.targetType,
               Port: e.port,
@@ -407,7 +620,7 @@ export const AwsElbModule: Module2 = new Module2({
               throw new Error('what should we do here?');
             }
             // Re-get the inserted record to get all of the relevant records we care about
-            const newObject = await client.getTargetGroup(result.TargetGroupArn ?? '');
+            const newObject = await getTargetGroup(client.elbClient, result.TargetGroupArn ?? '');
             // We map this into the same kind of entity as `obj`
             const newEntity = await AwsElbModule.utils.targetGroupMapper(newObject, ctx);
             // Save the record back into the database to get the new fields updated
@@ -419,11 +632,11 @@ export const AwsElbModule: Module2 = new Module2({
         read: async (ctx: Context, id?: string) => {
           const client = await ctx.getAwsClient() as AWS;
           if (id) {
-            const rawTargetGroup = await client.getTargetGroup(id);
+            const rawTargetGroup = await getTargetGroup(client.elbClient, id);
             if (!rawTargetGroup) return;
             return await AwsElbModule.utils.targetGroupMapper(rawTargetGroup, ctx);
           } else {
-            const tgs = (await client.getTargetGroups()).TargetGroups;
+            const tgs = await getTargetGroups(client.elbClient);
             const out = [];
             for (const tg of tgs) {
               out.push(await AwsElbModule.utils.targetGroupMapper(tg, ctx));
@@ -456,7 +669,7 @@ export const AwsElbModule: Module2 = new Module2({
             }
             const isUpdate = AwsElbModule.mappers.targetGroup.cloud.updateOrReplace(cloudRecord, e) === 'update';
             if (isUpdate) {
-              const updatedTargetGroup = await client.updateTargetGroup({
+              const updatedTargetGroup = await updateTargetGroup(client.elbClient, {
                 TargetGroupArn: e.targetGroupArn,
                 // TODO: make this properties not nullable but with default values
                 HealthCheckProtocol: e.healthCheckProtocol, // TODO: this one defaults to protocol
@@ -482,7 +695,7 @@ export const AwsElbModule: Module2 = new Module2({
         delete: async (es: TargetGroup[], ctx: Context) => {
           const client = await ctx.getAwsClient() as AWS;
           for (const e of es) {
-            await client.deleteTargetGroup(e.targetGroupArn!);
+            await deleteTargetGroup(client.elbClient, e.targetGroupArn!);
           }
         },
       }),
