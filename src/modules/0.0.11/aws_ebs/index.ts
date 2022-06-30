2238a8ee80d3da7a97fb627a824aa79244aa1270
Alejandro Guillen <aguillenv93@gmail.com>
2022-06-30 19:35:05 +0200

    Basic logic

--- src/modules/0.0.11/aws_ebs/index.ts
+++ src/modules/0.0.11/aws_ebs/index.ts
@@ -1,7 +1,8 @@
-import { Volume } from '@aws-sdk/client-ec2'
+import { CreateVolumeCommandInput, Tag, Volume } from '@aws-sdk/client-ec2'
 import { Context, Crud2, Mapper2, Module2, } from '../../interfaces'
 import { AwsEc2Module } from '../aws_ec2';
 import { AvailabilityZone } from '../aws_vpc/entity';
+import { AWS, createVolume, deleteVolume, getVolume, getVolumes } from './aws_helper';
 import { GeneralPurposeVolume, GeneralPurposeVolumeType, VolumeState } from './entity'
 import * as metadata from './module.json'
 
@@ -35,58 +36,85 @@ export const AwsEbsModule: Module2 = new Module2({
     eqTags: (a: { [key: string]: string }, b: { [key: string]: string }) => Object.is(Object.keys(a ?? {})?.length, Object.keys(b ?? {})?.length) &&
       Object.keys(a ?? {})?.every(ak => (a ?? {})[ak] === (b ?? {})[ak]),
   },
-  // mappers: {
-  //   generalPurposeVolume: new Mapper2<GeneralPurposeVolume>({
-  //     entity: GeneralPurposeVolume,
-  //     equals: (a: GeneralPurposeVolume, b: GeneralPurposeVolume) => Object.is(a.logGroupName, b.logGroupName)
-  //       && Object.is(a.logGroupArn, b.logGroupArn)
-  //       && Object.is(a.creationTime?.getTime(), b.creationTime?.getTime()),
-  //     source: 'db',
-  //     cloud: new Crud2({
-  //       create: async (lg: GeneralPurposeVolume[], ctx: Context) => {
-  //         const client = await ctx.getAwsClient() as AWS;
-  //         const out = []
-  //         for (const e of lg) {
-  //           await createLogGroup(client.cwClient, e.logGroupName);
-  //           // Re-get the inserted record to get all of the relevant records we care about
-  //           const newObject = await getLogGroup(client.cwClient, e.logGroupName);
-  //           // We map this into the same kind of entity as `obj`
-  //           const newEntity = await AwsCloudwatchModule.utils.logGroupMapper(newObject, ctx);
-  //           // Save the record back into the database to get the new fields updated
-  //           await AwsCloudwatchModule.mappers.logGroup.db.update(newEntity, ctx);
-  //           out.push(newEntity);
-  //         }
-  //         return out;
-  //       },
-  //       read: async (ctx: Context, id?: string) => {
-  //         const client = await ctx.getAwsClient() as AWS;
-  //         if (id) {
-  //           const rawLogGroup = await getLogGroup(client.cwClient, id);
-  //           if (!rawLogGroup) return;
-  //           return AwsCloudwatchModule.utils.logGroupMapper(rawLogGroup);
-  //         } else {
-  //           const logGroups = (await getLogGroups(client.cwClient)) ?? [];
-  //           return logGroups.map((lg: any) => AwsCloudwatchModule.utils.logGroupMapper(lg));
-  //         }
-  //       },
-  //       update: async (es: GeneralPurposeVolume[], ctx: Context) => {
-  //         // Right now we can only modify AWS-generated fields in the database.
-  //         // This implies that on `update`s we only have to restore the values for those records.
-  //         const out = [];
-  //         for (const e of es) {
-  //           const cloudRecord = ctx?.memo?.cloud?.GeneralPurposeVolume?.[e.logGroupName ?? ''];
-  //           await AwsCloudwatchModule.mappers.logGroup.db.update(cloudRecord, ctx);
-  //           out.push(cloudRecord);
-  //         }
-  //         return out;
-  //       },
-  //       delete: async (lg: GeneralPurposeVolume[], ctx: Context) => {
-  //         const client = await ctx.getAwsClient() as AWS;
-  //         for (const e of lg) {
-  //           await deleteLogGroup(client.cwClient, e.logGroupName);
-  //         }
-  //       },
-  //     }),
-  //   }),
-  // },
+  mappers: {
+    generalPurposeVolume: new Mapper2<GeneralPurposeVolume>({
+      entity: GeneralPurposeVolume,
+      equals: (a: GeneralPurposeVolume, b: GeneralPurposeVolume) => Object.is(a.attachedInstance?.instanceId, b.attachedInstance?.instanceId)
+        && Object.is(a.availabilityZone, b.availabilityZone)
+        && Object.is(a.iops, b.iops)
+        && Object.is(a.size, b.size)
+        && Object.is(a.state, b.state)
+        && Object.is(a.throughput, b.throughput)
+        && Object.is(a.volumeType, b.volumeType)
+        && AwsEbsModule.utils.eqTags(a.tags, b.tags),
+      source: 'db',
+      cloud: new Crud2({
+        create: async (es: GeneralPurposeVolume[], ctx: Context) => {
+          const client = await ctx.getAwsClient() as AWS;
+          const out = []
+          for (const e of es) {
+            const input: CreateVolumeCommandInput = {
+              AvailabilityZone: e.availabilityZone,
+              VolumeType: e.volumeType,
+              Size: e.size,
+              Iops: e.volumeType === GeneralPurposeVolumeType.GP3 ? e.iops : undefined,
+              Throughput:  e.volumeType === GeneralPurposeVolumeType.GP3 ? e.throughput : undefined,
+            };
+            if (e.tags && Object.keys(e.tags).length) {
+              const tags: Tag[] = Object.keys(e.tags).map((k: string) => {
+                return {
+                  Key: k, Value: e.tags![k],
+                }
+              });
+              input.TagSpecifications = [
+                {
+                  ResourceType: 'volume',
+                  Tags: tags,
+                },
+              ]
+            }
+            const newVolumeId = await createVolume(client.ec2client, input);
+            // TODO: attach instance if necessary
+            // Re-get the inserted record to get all of the relevant records we care about
+            const newObject = await getVolume(client.ec2client, newVolumeId);
+            // We map this into the same kind of entity as `obj`
+            const newEntity = await AwsEbsModule.utils.generalPurposeVolumeMapper(newObject, ctx);
+            // Save the record back into the database to get the new fields updated
+            await AwsEbsModule.mappers.generalPurposeVolume.db.update(newEntity, ctx);
+            out.push(newEntity);
+          }
+          return out;
+        },
+        read: async (ctx: Context, id?: string) => {
+          const client = await ctx.getAwsClient() as AWS;
+          if (id) {
+            const rawVolume = await getVolume(client.ec2client, id);
+            if (!rawVolume) return;
+            return AwsEbsModule.utils.generalPurposeVolumeMapper(rawVolume);
+          } else {
+            const logGroups = (await getVolumes(client.ec2client)) ?? [];
+            return logGroups.map((vol: any) => AwsEbsModule.utils.generalPurposeVolumeMapper(vol));
+          }
+        },
+        update: async (es: GeneralPurposeVolume[], ctx: Context) => {
+          // Right now we can only modify AWS-generated fields in the database.
+          // This implies that on `update`s we only have to restore the values for those records.
+          const out = [];
+          for (const e of es) {
+            const cloudRecord = ctx?.memo?.cloud?.GeneralPurposeVolume?.[e.volumeId ?? ''];
+            // TODO: implement update/restore. Do not let replace until we handle correctly snapshots
+            await AwsEbsModule.mappers.generalPurposeVolume.db.update(cloudRecord, ctx);
+            out.push(cloudRecord);
+          }
+          return out;
+        },
+        delete: async (vol: GeneralPurposeVolume[], ctx: Context) => {
+          const client = await ctx.getAwsClient() as AWS;
+          for (const e of vol) {
+            await deleteVolume(client.ec2client, e.volumeId);
+          }
+        },
+      }),
+    }),
+  },
 }, __dirname);
