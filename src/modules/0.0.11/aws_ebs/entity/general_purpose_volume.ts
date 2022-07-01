5aeee262ac6c9774ea267d425ee60fe3a25c624b
Alejandro Guillen <aguillenv93@gmail.com>
2022-07-01 13:11:19 +0200

    Update constraints

--- src/modules/0.0.11/aws_ebs/entity/general_purpose_volume.ts
+++ src/modules/0.0.11/aws_ebs/entity/general_purpose_volume.ts
@@ -50,7 +50,7 @@ export class GeneralPurposeVolume {
   })
   availabilityZone: AvailabilityZone;
 
-  @Check('Check_size_min_max', `"size" > 0 AND "size" < 16385`)
+  @Check('Check_gp_volume_size_min_max', `"size" > 0 AND "size" < 16385`)
   @Column({
     type: 'int',
     default: 8,  // AWS default when creating an instance
@@ -71,22 +71,20 @@ export class GeneralPurposeVolume {
   @JoinColumn({ name: 'attached_instance_id', })
   attachedInstance?: Instance;
 
-  @Check('Check_instance_device', `("instance_device_name" IS NULL AND "attached_instance_id" IS NULL) OR ("instance_device_name" IS NOT NULL AND "attached_instance_id" IS NOT NULL)`)
+  @Check('Check_gp_volume_instance_device', `("instance_device_name" IS NULL AND "attached_instance_id" IS NULL) OR ("instance_device_name" IS NOT NULL AND "attached_instance_id" IS NOT NULL)`)
   @Column({
     nullable: true,
   })
   instanceDeviceName?: string;
 
-  @Check('Check_gp3_iops', `"iops" is NOT NULL AND "volume_type" = 'gp3' AND "iops" <= 16000 AND "iops" >= 3000`)
-  @Check('Check_gp2_iops', `"iops" is NOT NULL AND "volume_type" = 'gp2' AND "iops" > 0`)
+  @Check('Check_gp_volume_iops', `"iops" is NULL OR ("iops" is NOT NULL AND (("volume_type" = 'gp3' AND "iops" <= 16000 AND "iops" >= 3000) OR ("volume_type" = 'gp2' AND "iops" > 0)))`)
   @Column({
     nullable: true,
     type: 'int',
   })
   iops?: number;
 
-  @Check('Check_no_gp2_throughput', `"throughput" IS NULL AND "volume_type" = ANY('{gp2, gp3}')`)
-  @Check('Check_gp3_throughput', `"throughput" IS NOT NULL AND "volume_type" = 'gp3' AND "throughput" >= 125 AND "throughput" <= 1000`)
+  @Check('Check_gp_volume_throughput', `"throughput" IS NULL OR ("throughput" IS NOT NULL AND "volume_type" = 'gp3' AND "throughput" >= 125 AND "throughput" <= 1000)`)
   @Column({
     nullable: true,
     type: 'int',
