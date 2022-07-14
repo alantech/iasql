7ce975c99e10d8534839724ee4f84d7b5e4e1391
David Ellis <isv.damocles@gmail.com>
2022-07-14 10:50:48 -0500

    Eliminate the static 'latest' export (#985)
    * Eliminate the static 'latest' export
    
    * Missed one (why did compile and lint miss this?)

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,16 +2,8 @@ import fs from 'fs'
 
 import * as semver from 'semver'
 
-import config from '../config'
-
-// No good way to re-export a require without this, and no way to determine what to import by the
-// config variable without require
-// tslint:disable-next-line:no-var-requires
-export const latest = require(`./${config.modules.latestVersion}`);
-
 export const modules = Object.fromEntries(
-  fs.readdirSync(__dirname, 'utf8').filter(m => semver.valid(m)).map(v => [`v${v.replace(/\./g, '_')}`, require(`./${v}`)])
+  fs.readdirSync(__dirname, 'utf8').filter(m => semver.valid(m)).map(v => [v, require(`./${v}`)])
 );
-modules.latest = latest;
 
 export * from './interfaces'
