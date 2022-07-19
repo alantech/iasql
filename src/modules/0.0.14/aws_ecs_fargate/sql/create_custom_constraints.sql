d2f1e65ce8aab14095091ac080520fbcb8dc7127
Alejandro Guillen <aguillenv93@gmail.com>
2022-07-19 18:01:39 +0200

    Fix subnets constraint

--- src/modules/0.0.14/aws_ecs_fargate/sql/create_custom_constraints.sql
+++ src/modules/0.0.14/aws_ecs_fargate/sql/create_custom_constraints.sql
@@ -6,9 +6,9 @@ declare
   _subnets_count integer;
 begin
   select COUNT(*) into _subnets_count
-  from subnets
+  from subnet
   where id = any(_subnets);
-  return _target_group_type = array_length(_subnets, 1);
+  return _subnets_count = array_length(_subnets, 1);
 end;
 $$;
 ALTER TABLE service ADD CONSTRAINT check_service_subnets CHECK (check_service_subnets(subnets));
