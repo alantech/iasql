b8e0c3127562e41e77d379f0113926145466061e
David Ellis <isv.damocles@gmail.com>
2022-07-21 11:42:28 -0500

    Postgres' 'array_length' has the weirdest call signature of a length function that I've ever seen

--- src/modules/0.0.14/aws_elb/sql/create_custom_constraints.sql
+++ src/modules/0.0.14/aws_elb/sql/create_custom_constraints.sql
@@ -5,7 +5,7 @@ declare
   _number_of_records integer;
 begin
   select count(*) from availability_zone where name = any(_availability_zones) into _number_of_records;
-  if _number_of_records != array_length(_availability_zones) then
+  if _number_of_records != array_length(_availability_zones, 1) then
     raise exception 'Load balancer % includes one or more invalid availability zones', _load_balancer_name;
   end if;
   return true;
