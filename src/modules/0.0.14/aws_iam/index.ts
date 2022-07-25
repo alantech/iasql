b2c42d3c6cc50b5c774b5437c11b59ef7e276aaa
Alejandro Guillen <aguillenv93@gmail.com>
2022-07-25 16:05:05 +0200

    Wait more time for aws role

--- src/modules/0.0.14/aws_iam/index.ts
+++ src/modules/0.0.14/aws_iam/index.ts
@@ -181,8 +181,9 @@ export const AwsIamModule: Module2 = new Module2({
             await AwsIamModule.mappers.role.db.update(newRole, ctx);
             out.push(newRole);
             // TODO: find a way to fix this better?
-            // wait a second to let aws get the role ready to use
-            await new Promise(r => setTimeout(r, 1000));
+            // wait 10 seconds to let aws get the role ready to use :/
+            // https://stackoverflow.com/a/37438525
+            await new Promise(r => setTimeout(r, 10000));
           }
           return out;
         },
