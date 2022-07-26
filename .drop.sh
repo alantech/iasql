7592ffde4b30e4ddf242c8d3d7ccca94220064b2
David Ellis <isv.damocles@gmail.com>
2022-07-26 09:36:06 -0500

    Fix the drop and develop scripts

--- .drop.sh
+++ .drop.sh
@@ -7,10 +7,20 @@ set -vex
 git checkout main
 git pull origin main
 
+# The current version
+VERSION=`ts-node src/scripts/latestVersion.ts`
+
 # The version to drop
-VERSION=`ts-node src/scripts/oldestVersion.ts`
+LASTVERSION=`ts-node src/scripts/oldestVersion.ts`
+
+# Update the config versions
+CONFIGCI="$(cat src/config/ci.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGCI}" > src/config/ci.ts
+CONFIGLOCAL="$(cat src/config/local.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGLOCAL}" > src/config/local.ts
+CONFIGPRODUCTION="$(cat src/config/production.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGPRODUCTION}" > src/config/production.ts
+CONFIGSTAGING="$(cat src/config/staging.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGSTAGING}" > src/config/staging.ts
+CONFIGTEST="$(cat src/config/test.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGTEST}" > src/config/test.ts
 
 # Drop the version and push to main
-git rm -r src/modules/${VERSION}
-git commit -m "Drop version ${VERSION}"
+git rm -r src/modules/${LASTVERSION}
+git commit -m "Drop version ${LASTVERSION}"
 git push origin main
