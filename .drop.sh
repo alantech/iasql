cf79821864d27c8811740c8457375b4f9d51d26b
David Ellis <isv.damocles@gmail.com>
2022-07-26 15:39:11 -0500

    Fix the drop script

--- .drop.sh
+++ .drop.sh
@@ -11,17 +11,22 @@ git pull origin main
 VERSION=`ts-node src/scripts/latestVersion.ts`
 
 # The version to drop
+OLDESTVERSION=`ts-node src/scripts/oldestVersion.ts`
+
+# Drop the version
+git rm -r src/modules/${OLDESTVERSION}
+
+# Run again to get the new last version
 LASTVERSION=`ts-node src/scripts/oldestVersion.ts`
 
 # Update the config versions
 CONFIGCI="$(cat src/config/ci.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGCI}" > src/config/ci.ts
 CONFIGLOCAL="$(cat src/config/local.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGLOCAL}" > src/config/local.ts
-CONFIGPRODUCTION="$(cat src/config/production.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGPRODUCTION}" > src/config/production.ts
+CONFIGPRODUCTION="$(cat src/config/production.ts | sed "s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGPRODUCTION}" > src/config/production.ts # Production doesn't get the latest version edited
 CONFIGSTAGING="$(cat src/config/staging.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGSTAGING}" > src/config/staging.ts
 CONFIGTEST="$(cat src/config/test.ts | sed "s/latestVersion:.*/latestVersion: '${VERSION}',/;s/oldestVersion:.*/oldestVersion: '${LASTVERSION}'/")" && echo "${CONFIGTEST}" > src/config/test.ts
 
 # Drop the version and push to main
-git rm -r src/modules/${LASTVERSION}
 git add src/config/*.ts
-git commit -m "Drop version ${LASTVERSION}"
+git commit -m "Drop version ${OLDESTVERSION}"
 git push origin main
