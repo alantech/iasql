c851d57daf1f46833b1d18a9deed16dd28c45d02
David Ellis <isv.damocles@gmail.com>
2022-08-10 09:46:09 -0500

    Class-ify 'aws_acm_import'

--- src/modules/0.0.17/aws_acm_import/index.ts
+++ src/modules/0.0.17/aws_acm_import/index.ts
@@ -1,63 +1,78 @@
 import { ACM, ImportCertificateCommandInput, paginateListCertificates, } from '@aws-sdk/client-acm'
 import { AWS, paginateBuilder, } from '../../../services/aws_macros'
-import { Context, Crud2, Mapper2, Module2, } from '../../interfaces'
+import { Context, Crud2, MapperBase, ModuleBase, } from '../../interfaces'
 import { AwsAcmListModule } from '../aws_acm_list'
 import { CertificateImport } from './entity'
 import * as metadata from './module.json'
 
-const getCertificatesSummary = paginateBuilder<ACM>(paginateListCertificates, 'CertificateSummaryList');
+class CertificateImportMapper extends MapperBase<CertificateImport> {
+  module: AwsAcmImportModule;
+  entity = CertificateImport;
+  entityId = (e: CertificateImport) => e.id?.toString() ?? '';
+  equals = () => true; // only database values
 
-// TODO: Can I macro this somehow?
-async function importCertificate(client: ACM, input: ImportCertificateCommandInput) {
-  const res = await client.importCertificate(input);
-  const arn = res.CertificateArn ?? '';
-  let certificates: string[] = [];
-  let i = 0;
-   // Wait for ~1min until imported cert is available
-  do {
-    await new Promise(r => setTimeout(r, 2000));
-    certificates = (await getCertificatesSummary(client))?.map(c => c.CertificateArn ?? '') ?? [];
-    i++;
-  } while (!certificates.includes(arn) && i < 30);
-  return arn;
+  getCertificatesSummary = paginateBuilder<ACM>(paginateListCertificates, 'CertificateSummaryList');
+
+  // TODO: Can I macro this somehow?
+  async importCertificate(client: ACM, input: ImportCertificateCommandInput) {
+    const res = await client.importCertificate(input);
+    const arn = res.CertificateArn ?? '';
+    let certificates: string[] = [];
+    let i = 0;
+     // Wait for ~1min until imported cert is available
+    do {
+      await new Promise(r => setTimeout(r, 2000));
+      certificates = (await this.getCertificatesSummary(client))
+        ?.map(c => c.CertificateArn ?? '') ?? [];
+      i++;
+    } while (!certificates.includes(arn) && i < 30);
+    return arn;
+  }
+
+  cloud = new Crud2<CertificateImport>({
+    create: async (es: CertificateImport[], ctx: Context) => {
+      const client = await ctx.getAwsClient() as AWS;
+      const textEncoder = new TextEncoder();
+      for (const e of es) {
+        const input: ImportCertificateCommandInput = {
+          Certificate: textEncoder.encode(e.certificate),
+          PrivateKey: textEncoder.encode(e.privateKey),
+        };
+        if (e.chain) {
+          input.CertificateChain = textEncoder.encode(e.chain);
+        }
+        const importedCertArn = await this.importCertificate(client.acmClient, input);
+        if (!importedCertArn) throw new Error('Error importing certificate');
+        const importedCert = await AwsAcmListModule.mappers.certificate.cloud.read(ctx, importedCertArn);
+        await this.module.certificateImport.db.delete(e, ctx);
+        try {
+          await AwsAcmListModule.mappers.certificate.db.create(importedCert, ctx);
+        } catch (e) {
+          // Do nothing
+          // AwsAcmListModule could not be installed and that's ok since there's no table dependency between them
+        }
+      }
+    },
+    read: async () => { return; },
+    update: async () => { return; },
+    delete: async () => { return; },
+  });
+
+  constructor(module: AwsAcmImportModule) {
+    super();
+    this.module = module;
+    super.init();
+  }
 }
 
-export const AwsAcmImportModule: Module2 = new Module2({
-  ...metadata,
-  mappers: {
-    certificateImport: new Mapper2<CertificateImport>({
-      entity: CertificateImport,
-      entityId: (e: CertificateImport) => e.id?.toString() ?? '',
-      equals: () => true, // only database values
-      source: 'db',
-      cloud: new Crud2({
-        create: async (es: CertificateImport[], ctx: Context) => {
-          const client = await ctx.getAwsClient() as AWS;
-          const textEncoder = new TextEncoder();
-          for (const e of es) {
-            const input: ImportCertificateCommandInput = {
-              Certificate: textEncoder.encode(e.certificate),
-              PrivateKey: textEncoder.encode(e.privateKey),
-            };
-            if (e.chain) {
-              input.CertificateChain = textEncoder.encode(e.chain);
-            }
-            const importedCertArn = await importCertificate(client.acmClient, input);
-            if (!importedCertArn) throw new Error('Error importing certificate');
-            const importedCert = await AwsAcmListModule.mappers.certificate.cloud.read(ctx, importedCertArn);
-            await AwsAcmImportModule.mappers.certificateImport.db.delete(e, ctx);
-            try {
-              await AwsAcmListModule.mappers.certificate.db.create(importedCert, ctx);
-            } catch (e) {
-              // Do nothing
-              // AwsAcmListModule could not be installed and that's ok since there's no table dependency between them
-            }
-          }
-        },
-        read: async () => { return; },
-        update: async () => { return; },
-        delete: async () => { return; },
-      }),
-    }),
-  },
-}, __dirname);
+class AwsAcmImportModule extends ModuleBase {
+  dependencies = metadata.dependencies;
+  certificateImport: CertificateImportMapper;
+
+  constructor() {
+    super();
+    this.certificateImport = new CertificateImportMapper(this);
+    super.init();
+  }
+}
+export const awsAcmImportModule = new AwsAcmImportModule();
