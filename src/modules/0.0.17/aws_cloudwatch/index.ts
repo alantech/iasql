5add6869618f1d8b310a5174b65e0b8b6cabe89b
David Ellis <isv.damocles@gmail.com>
2022-08-12 18:06:56 -0500

    Class-ify 'aws_cloudwatch' (#1095)
    * Class-ify 'aws_cloudwatch'
    
    * Fix cloudwatch import in fargate module
    
    * Use the typed path for calling the cloudwatch logGroup mapper functions

--- src/modules/0.0.17/aws_cloudwatch/index.ts
+++ src/modules/0.0.17/aws_cloudwatch/index.ts
@@ -1,92 +1,111 @@
 import { CloudWatchLogs, paginateDescribeLogGroups, } from '@aws-sdk/client-cloudwatch-logs'
 import { AWS, crudBuilderFormat, paginateBuilder, } from '../../../services/aws_macros'
-import { Context, Crud2, Mapper2, Module2, } from '../../interfaces'
+import { Context, Crud2, MapperBase, ModuleBase, } from '../../interfaces'
 import { LogGroup } from './entity'
 import * as metadata from './module.json'
 
-const createLogGroup = crudBuilderFormat<CloudWatchLogs, 'createLogGroup', undefined>(
-  'createLogGroup',
-  (logGroupName) => ({ logGroupName, }),
-  (_lg) => undefined,
-);
-const getLogGroups = paginateBuilder<CloudWatchLogs>(
-  paginateDescribeLogGroups,
-  'logGroups',
-);
-const getLogGroup = async (client: CloudWatchLogs, groupName: string) => (
-  await getLogGroups(client)
-).find(lg => lg.logGroupName === groupName);
-const deleteLogGroup = crudBuilderFormat<CloudWatchLogs, 'deleteLogGroup', undefined>(
-  'deleteLogGroup',
-  (logGroupName) => ({ logGroupName, }),
-  (_lg) => undefined,
-);
+class LogGroupMapper extends MapperBase<LogGroup> {
+  module: AwsCloudwatchModule;
+  entity = LogGroup;
+  equals = (a: LogGroup, b: LogGroup) => Object.is(a.logGroupName, b.logGroupName)
+    && Object.is(a.logGroupArn, b.logGroupArn)
+    && Object.is(a.creationTime?.getTime(), b.creationTime?.getTime());
 
-export const AwsCloudwatchModule: Module2 = new Module2({
-  ...metadata,
-  utils: {
-    logGroupMapper: (lg: any) => {
-      const out = new LogGroup();
-      if (!lg?.logGroupName) return undefined;
-      out.logGroupName = lg.logGroupName;
-      out.logGroupArn = lg.arn;
-      out.creationTime = lg.creationTime ? new Date(lg.creationTime) : lg.creationTime;
+  createLogGroup = crudBuilderFormat<CloudWatchLogs, 'createLogGroup', undefined>(
+    'createLogGroup',
+    (logGroupName) => ({ logGroupName, }),
+    (_lg) => undefined,
+  );
+  getLogGroups = paginateBuilder<CloudWatchLogs>(
+    paginateDescribeLogGroups,
+    'logGroups',
+  );
+  async getLogGroup(client: CloudWatchLogs, groupName: string) {
+    return (await this.getLogGroups(client))
+      .find(lg => lg.logGroupName === groupName);
+  }
+  deleteLogGroup = crudBuilderFormat<CloudWatchLogs, 'deleteLogGroup', undefined>(
+    'deleteLogGroup',
+    (logGroupName) => ({ logGroupName, }),
+    (_lg) => undefined,
+  );
+  logGroupMapper(lg: any) {
+    const out = new LogGroup();
+    if (!lg?.logGroupName) return undefined;
+    out.logGroupName = lg.logGroupName;
+    out.logGroupArn = lg.arn;
+    out.creationTime = lg.creationTime ? new Date(lg.creationTime) : lg.creationTime;
+    return out;
+  };
+
+  cloud = new Crud2<LogGroup>({
+    create: async (lg: LogGroup[], ctx: Context) => {
+      const client = await ctx.getAwsClient() as AWS;
+      const out = []
+      for (const e of lg) {
+        await this.createLogGroup(client.cwClient, e.logGroupName);
+        // Re-get the inserted record to get all of the relevant records we care about
+        const newObject = await this.getLogGroup(client.cwClient, e.logGroupName);
+        // We map this into the same kind of entity as `obj`
+        const newEntity = this.logGroupMapper(newObject);
+        if (!newEntity) continue;
+        // Save the record back into the database to get the new fields updated
+        await this.module.logGroup.db.update(newEntity, ctx);
+        out.push(newEntity);
+      }
       return out;
     },
-  },
-  mappers: {
-    logGroup: new Mapper2<LogGroup>({
-      entity: LogGroup,
-      equals: (a: LogGroup, b: LogGroup) => Object.is(a.logGroupName, b.logGroupName)
-        && Object.is(a.logGroupArn, b.logGroupArn)
-        && Object.is(a.creationTime?.getTime(), b.creationTime?.getTime()),
-      source: 'db',
-      cloud: new Crud2({
-        create: async (lg: LogGroup[], ctx: Context) => {
-          const client = await ctx.getAwsClient() as AWS;
-          const out = []
-          for (const e of lg) {
-            await createLogGroup(client.cwClient, e.logGroupName);
-            // Re-get the inserted record to get all of the relevant records we care about
-            const newObject = await getLogGroup(client.cwClient, e.logGroupName);
-            // We map this into the same kind of entity as `obj`
-            const newEntity = await AwsCloudwatchModule.utils.logGroupMapper(newObject, ctx);
-            // Save the record back into the database to get the new fields updated
-            await AwsCloudwatchModule.mappers.logGroup.db.update(newEntity, ctx);
-            out.push(newEntity);
-          }
-          return out;
-        },
-        read: async (ctx: Context, id?: string) => {
-          const client = await ctx.getAwsClient() as AWS;
-          if (id) {
-            const rawLogGroup = await getLogGroup(client.cwClient, id);
-            if (!rawLogGroup) return;
-            return AwsCloudwatchModule.utils.logGroupMapper(rawLogGroup);
-          } else {
-            const logGroups = (await getLogGroups(client.cwClient)) ?? [];
-            return logGroups.map((lg: any) => AwsCloudwatchModule.utils.logGroupMapper(lg));
-          }
-        },
-        updateOrReplace: () => 'update',
-        update: async (es: LogGroup[], ctx: Context) => {
-          // Right now we can only modify AWS-generated fields in the database.
-          // This implies that on `update`s we only have to restore the values for those records.
-          const out = [];
-          for (const e of es) {
-            const cloudRecord = ctx?.memo?.cloud?.LogGroup?.[e.logGroupName ?? ''];
-            await AwsCloudwatchModule.mappers.logGroup.db.update(cloudRecord, ctx);
-            out.push(cloudRecord);
-          }
-          return out;
-        },
-        delete: async (lg: LogGroup[], ctx: Context) => {
-          const client = await ctx.getAwsClient() as AWS;
-          for (const e of lg) {
-            await deleteLogGroup(client.cwClient, e.logGroupName);
-          }
-        },
-      }),
-    }),
-  },
-}, __dirname);
+    read: async (ctx: Context, id?: string) => {
+      const client = await ctx.getAwsClient() as AWS;
+      if (id) {
+        const rawLogGroup = await this.getLogGroup(client.cwClient, id);
+        if (!rawLogGroup) return;
+        return this.logGroupMapper(rawLogGroup);
+      } else {
+        const logGroups = (await this.getLogGroups(client.cwClient)) ?? [];
+        const out = [];
+        for (const logGroup of logGroups) {
+          const outLog = this.logGroupMapper(logGroup);
+          if (outLog) out.push(outLog);
+        }
+        return out;
+      }
+    },
+    updateOrReplace: () => 'update',
+    update: async (es: LogGroup[], ctx: Context) => {
+      // Right now we can only modify AWS-generated fields in the database.
+      // This implies that on `update`s we only have to restore the values for those records.
+      const out = [];
+      for (const e of es) {
+        const cloudRecord = ctx?.memo?.cloud?.LogGroup?.[e.logGroupName ?? ''];
+        await this.module.logGroup.db.update(cloudRecord, ctx);
+        out.push(cloudRecord);
+      }
+      return out;
+    },
+    delete: async (lg: LogGroup[], ctx: Context) => {
+      const client = await ctx.getAwsClient() as AWS;
+      for (const e of lg) {
+        await this.deleteLogGroup(client.cwClient, e.logGroupName);
+      }
+    },
+  });
+
+  constructor(module: AwsCloudwatchModule) {
+    super();
+    this.module = module;
+    super.init();
+  }
+}
+
+class AwsCloudwatchModule extends ModuleBase {
+  dependencies = metadata.dependencies;
+  logGroup: LogGroupMapper;
+
+  constructor() {
+    super();
+    this.logGroup = new LogGroupMapper(this);
+    super.init();
+  }
+}
+export const awsCloudwatchModule = new AwsCloudwatchModule();
