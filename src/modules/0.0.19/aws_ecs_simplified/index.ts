59fd623bf6a267bf736cad3e56fc8bc5fa272562
depombo <lfdepombo@gmail.com>
2022-08-30 08:27:22 -0400

    yarn format

--- src/modules/0.0.19/aws_ecs_simplified/index.ts
+++ src/modules/0.0.19/aws_ecs_simplified/index.ts
@@ -1,8 +1,10 @@
 /* THIS MODULE IS A PURE SQL MODULE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE WITH MAPPERS */
-
-import { ModuleBase, } from '../../interfaces'
+import { ModuleBase } from '../../interfaces';
 
 class AwsEcsSimplified extends ModuleBase {
-  constructor() { super(); super.init(); }
+  constructor() {
+    super();
+    super.init();
+  }
 }
 export const ecsSimplified = new AwsEcsSimplified();
