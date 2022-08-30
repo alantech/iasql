3f099ed8f0e264b77b934ea52a4ebf5052b5a5e4
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2022-08-30 11:23:22 +0300

    Call scheduler functions locally when on tests, fixing CI

--- src/services/scheduler-api.ts
+++ src/services/scheduler-api.ts
@@ -1,7 +1,14 @@
 import fetch from 'node-fetch';
 import logger from '../services/logger';
+import { readdirSync } from "fs";
+import * as scheduler from './scheduler';
 
 const schedulerAddress = 'http://localhost:14527';
+
+// call the scheduler functions locally when on tests
+const files = readdirSync(__dirname);
+const isTs = files.some(file => /.*ts$/.test(file));
+const shouldRpc = !isTs;
 
 async function fetchOrRaise(url: string) {
   const response = await fetch(url);
@@ -13,7 +20,8 @@ async function fetchOrRaise(url: string) {
   throw new Error(error);
 }
 
-export const init = () => fetchOrRaise(`${schedulerAddress}/init/`);
-export const start = (dbId: string, dbUser: string) => fetchOrRaise(`${schedulerAddress}/start/${dbId}/${dbUser}/`);
-export const stop = (dbId: string) => fetchOrRaise(`${schedulerAddress}/stop/${dbId}/`);
-export const stopAll = () => fetchOrRaise(`${schedulerAddress}/stopAll/`);
+
+export const init = () => (shouldRpc ? fetchOrRaise(`${schedulerAddress}/init/`) : scheduler.init());
+export const start = (dbId: string, dbUser: string) => (shouldRpc ? fetchOrRaise(`${schedulerAddress}/start/${dbId}/${dbUser}/`) : scheduler.start(dbId, dbUser));
+export const stop = (dbId: string) => (shouldRpc ? fetchOrRaise(`${schedulerAddress}/stop/${dbId}/`) : scheduler.stop(dbId));
+export const stopAll = () => (shouldRpc ? fetchOrRaise(`${schedulerAddress}/stopAll/`) : scheduler.stopAll());
