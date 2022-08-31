56404353df1f74652bf1cdba1358b061ef575c55
Joseph Aje <ajejoseph22@gmail.com>
2022-08-31 14:01:20 +0100

    chore: refactored diff module

--- src/util/policy-diff.ts
+++ src/util/policy-diff.ts
@@ -1,89 +1,99 @@
-function isStringArray(obj: unknown): boolean {
-    return Array.isArray(obj) && obj.every(it => typeof it === 'string');
+enum IAM_KEY {
+  Version = 'Version',
+  Statement = 'Statement',
+  Effect = 'Effect',
+  Principal = 'Principal',
+  Action = 'Action',
+  Resource = 'Resource',
+  Sid = 'Sid',
+  Condition = 'Condition',
 }
 
-function isString(obj: unknown): boolean {
-    return typeof obj === 'string';
+function isStringArray(obj: unknown): obj is string[] {
+  return Array.isArray(obj) && obj.every(isString);
 }
 
-function isObject(obj: unknown): boolean {
-    return typeof obj === 'object';
+function isString(obj: unknown): obj is string {
+  return typeof obj === 'string';
 }
 
-function objectCompare(obj1: unknown = {}, obj2: unknown = {}): boolean {
-    // One is array of a single string and the other is string
-    if (isStringArray(obj1) && (obj1 as string[]).length === 1 && isString(obj2)) return (obj1 as string[])[0] === obj2;
-    if (isStringArray(obj2) && (obj2 as string[]).length === 1 && isString(obj1)) return (obj2 as string[])[0] === obj1;
+function isObject(obj: unknown): obj is object {
+  return typeof obj === 'object';
+}
 
-    // Both are array of strings
-    if (Array.isArray(obj1) && obj1.every(isString) &&
-        Array.isArray(obj2) && obj1.every(isString)) {
-        if (obj1.length !== obj2.length) return false;
-        obj1.sort();
-        obj2.sort();
-        return obj1.every((element, i) => element === obj2[i]);
+function objectsAreSame(obj1: any = {}, obj2: any = {}): boolean {
+  // One is array of a single string and the other is string
+  if (isStringArray(obj1) && obj1.length === 1 && isString(obj2)) return obj1[0] === obj2;
+  if (isStringArray(obj2) && obj2.length === 1 && isString(obj1)) return obj2[0] === obj1;
+
+  // Both are array of strings
+  if (isStringArray(obj1) && isStringArray(obj2)) {
+    if (obj1.length !== obj2.length) return false;
+    obj1.sort();
+    obj2.sort();
+    return obj1.every((element, i) => element === obj2[i]);
+  }
+
+  // From https://stackoverflow.com/questions/44792629/how-to-compare-two-objects-with-nested-array-of-object-using-loop
+  let same = Object.keys(obj1).length === Object.keys(obj2).length;
+  if (!same) return same;
+
+  for (const key of Object.keys(obj1)) {
+    if (isObject(obj1[key])) {
+      same = objectsAreSame(obj1[key], obj2[key]);
+    } else {
+      if (obj1[key] !== obj2[key]) {
+        same = false;
+        break;
+      }
     }
-
-    // From https://stackoverflow.com/questions/44792629/how-to-compare-two-objects-with-nested-array-of-object-using-loop
-    let same = Object.keys((obj1 as ObjectType)).length === Object.keys((obj2 as ObjectType)).length;
-    if (!same) return same;
-
-    for (const key of Object.keys((obj1 as Object))) {
-        if (isObject((obj1 as ObjectType)[key as string])) {
-            same = objectCompare((obj1 as ObjectType)[key], (obj2 as ObjectType)[key]);
-        } else {
-            if ((obj1 as ObjectType)[key] !== (obj2 as ObjectType)[key]) {
-                same = false;
-                break;
-            }
-        }
-    }
-    return same;
+  }
+  return same;
 }
 
 // Returns true if the policies are structurally equal, and false if they aren't
-export default function policiesAreSame(obj1: unknown, obj2: unknown): boolean {
-    if (Array.isArray(obj1) && obj1.length === 1) return policiesAreSame(obj1[0], obj2);
-    if (Array.isArray(obj2) && obj2.length === 1) return policiesAreSame(obj1, obj2[0]);
+export default function policiesAreSame(obj1: any, obj2: any): boolean {
+  if (Array.isArray(obj1) && obj1.length === 1) return policiesAreSame(obj1[0], obj2);
+  if (Array.isArray(obj2) && obj2.length === 1) return policiesAreSame(obj1, obj2[0]);
 
-    let same = Object.keys((obj1 as ObjectType) || {}).length === Object.keys((obj2 as ObjectType) || {}).length;
-    if (!same) return same;
+  let same = Object.keys(obj1).length === Object.keys(obj2).length;
+  if (!same) return same;
 
-    same = true;
-    for (const key of Object.keys((obj1 as ObjectType))) {
-        switch (key) {
-            // Can be String-only
-            case IAM_KEY.Version:
-            case IAM_KEY.Effect:
-            case IAM_KEY.Sid:
-                if ((obj1 as ObjectType)[key] !== (obj2 as ObjectType)[key]) {
-                    same = false;
-                }
-                break;
-            // Can be an array of single object or just an object
-            case IAM_KEY.Statement:
-                same = policiesAreSame((obj1 as ObjectType)[key], (obj2 as ObjectType)[key])
-                break;
-            // Can be JSON or string
-            case IAM_KEY.Principal:
-            // Can be string or array of strings
-            case IAM_KEY.Action:
-            case IAM_KEY.Resource:
-            // Can only be JSON
-            case IAM_KEY.Condition:
-            default:
-                if (isString((obj1 as ObjectType)[key]) && isString((obj2 as ObjectType)[key]) && (obj1 as ObjectType)[key] !== (obj2 as ObjectType)[key]) {
-                    same = false;
-                    break;
-                }
-
-                same = objectCompare((obj1 as ObjectType)[key], (obj2 as ObjectType)[key]);
-                break;
+  same = true;
+  for (const key of Object.keys(obj1)) {
+    switch (key) {
+      // Can be String-only
+      case IAM_KEY.Version:
+      case IAM_KEY.Effect:
+      case IAM_KEY.Sid:
+        if (obj1[key] !== obj2[key]) {
+          same = false;
+        }
+        break;
+      // Can be an array of single object or just an object
+      case IAM_KEY.Statement:
+        same = policiesAreSame(obj1[key], obj2[key]);
+        break;
+      // Can be JSON or string
+      case IAM_KEY.Principal:
+      // Can be string or array of strings
+      case IAM_KEY.Action:
+      case IAM_KEY.Resource:
+      // Can only be JSON
+      case IAM_KEY.Condition:
+      default:
+        if (isString(obj1[key]) && isString(obj2[key]) && obj1[key] !== obj2[key]) {
+          same = false;
+          break;
         }
 
-        // If we've found a difference, stop looping.
-        if (!same) break;
+        same = objectsAreSame(obj1[key], obj2[key]);
+        break;
     }
 
-    return same;
+    // If we've found a difference, stop looping.
+    if (!same) break;
+  }
+
+  return same;
 }
