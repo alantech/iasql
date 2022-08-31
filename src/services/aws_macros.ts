61aa0bc6c7ae6aac84759412b3568fa2c930dcde
Yolanda Robla Mota <info@ysoft.biz>
2022-08-31 18:46:20 +0200

    Issue #1084: Add api gateway coverage (#1128)
    * Add api gateway coverage
    
    * Issue #1084: Add api gateway coverage
    
    Adding the basic coverage for API gateway, with the
    possibility of creating REST API entries.
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    Closes #1084
    
    * Issue #1084: Add api gateway coverage
    
    Adding the basic coverage for API gateway, with the
    possibility of creating REST API entries.
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    Closes #1084
    
    * fix incorrect import for old version
    
    * fix query for integration testing
    
    * add extra check for id change
    
    * fixes in tests and retries
    
    * add new version with gateway v2
    
    * remove older module version
    
    * remove changes in older version
    
    * changes in styling
    
    * add slow retry strategy to apigateway
    
    * implement fixes asked in review

--- src/services/aws_macros.ts
+++ src/services/aws_macros.ts
@@ -1,4 +1,5 @@
 import { ACM } from '@aws-sdk/client-acm';
+import { ApiGatewayV2 } from '@aws-sdk/client-apigatewayv2';
 import { CloudFront } from '@aws-sdk/client-cloudfront';
 import { CloudWatchLogs } from '@aws-sdk/client-cloudwatch-logs';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
@@ -16,6 +17,7 @@ import { Route53 } from '@aws-sdk/client-route-53';
 import { S3 } from '@aws-sdk/client-s3';
 import { SecretsManager } from '@aws-sdk/client-secrets-manager';
 import { SSM } from '@aws-sdk/client-ssm';
+import { StandardRetryStrategy } from '@aws-sdk/middleware-retry';
 
 type AWSCreds = {
   accessKeyId: string;
@@ -39,8 +41,14 @@ type ArgumentTypes<F> = F extends (...args: infer A) => infer B ? [...A, B] : ne
 // break.
 type PromiseReturnType<F> = ArgumentTypes<ArgumentTypes<F>[2]>[1];
 
+// defaults for slow retries
+const SLOW_STRATEGY_RETRIES = 10; // retry a max of 10 times, it will allow to bypass quotas
+const SLOW_STRATEGY_BASE_DELAY = 1000; // wait 1 second - as only delete api has a 5 second quota
+const SLOW_STRATEGY_MAXIMUM_RETRY_DELAY = 20 * 5000;
+
 export class AWS {
   acmClient: ACM;
+  apiGatewayClient: ApiGatewayV2;
   cwClient: CloudWatchLogs;
   ec2client: EC2;
   ecrClient: ECR;
@@ -59,9 +67,26 @@ export class AWS {
   secretsClient: SecretsManager;
   cloudfrontClient: CloudFront;
   memoryDBClient: MemoryDB;
+  slowRetryStrategy: StandardRetryStrategy;
 
   constructor(config: AWSConfig) {
+    // declare an specific slow retry strategy, to reuse in slow apis
+    this.slowRetryStrategy = new StandardRetryStrategy(async () => SLOW_STRATEGY_RETRIES, {
+      delayDecider: (_, attempts) =>
+        Math.floor(
+          Math.min(
+            SLOW_STRATEGY_MAXIMUM_RETRY_DELAY,
+            Math.random() * 2 ** attempts * SLOW_STRATEGY_BASE_DELAY,
+          ),
+        ),
+    });
+
     this.region = config.region;
+    this.apiGatewayClient = new ApiGatewayV2({
+      credentials: config.credentials,
+      region: config.region,
+      retryStrategy: this.slowRetryStrategy,
+    });
     this.acmClient = new ACM(config);
     this.cloudfrontClient = new CloudFront(config);
     this.cwClient = new CloudWatchLogs(config);
---
+++ test/modules/aws-api-gateway-integration.ts
@@ -0,0 +1,206 @@
+import config from "../../src/config";
+import * as iasql from "../../src/services/iasql";
+import {
+  runQuery,
+  runInstall,
+  runUninstall,
+  runApply,
+  finish,
+  execComposeUp,
+  execComposeDown,
+  runSync,
+  getPrefix,
+} from "../helpers";
+
+const prefix = getPrefix();
+const dbAlias = `${prefix}apigatewaytest`;
+
+const apply = runApply.bind(null, dbAlias);
+const sync = runSync.bind(null, dbAlias);
+const query = runQuery.bind(null, dbAlias);
+const install = runInstall.bind(null, dbAlias);
+const uninstall = runUninstall.bind(null, dbAlias);
+const modules = ["aws_api_gateway"];
+const apiName = `${prefix}testApi`;
+
+jest.setTimeout(3600000);
+beforeAll(async () => await execComposeUp());
+afterAll(async () => await execComposeDown());
+
+describe("API Gateway Integration Testing", () => {
+  it("creates a new test db", (done) =>
+    void iasql
+      .connect(dbAlias, "not-needed", "not-needed")
+      .then(...finish(done)));
+
+  it("installs the aws_account module", install(["aws_account"]));
+
+  it(
+    "inserts aws credentials",
+    query(`
+    INSERT INTO aws_account (region, access_key_id, secret_access_key)
+    VALUES ('${process.env.AWS_REGION}', '${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `)
+  );
+
+  it("installs the API gateway module", install(modules));
+
+  it(
+    "adds a new API gateway",
+    query(`  
+    INSERT INTO api (name, description)
+    VALUES ('${apiName}', 'description');
+  `)
+  );
+
+  it("undo changes", sync());
+
+  it(
+    "adds a new API gateway",
+    query(`  
+    INSERT INTO api (name, description, disable_execute_api_endpoint, version)
+    VALUES ('${apiName}', 'description', false, '1.0');
+  `)
+  );
+
+  it("applies the API gateway change", apply());
+
+  it(
+    "check API gateway is available",
+    query(
+      `
+  SELECT * FROM api WHERE name='${apiName}';
+  `,
+      (res: any) => expect(res.length).toBe(1)
+    )
+  );
+
+  it(
+    "tries to update API description",
+    query(`
+  UPDATE api SET description='new description' WHERE name='${apiName}'
+  `)
+  );
+
+  it("applies the API description update", apply());
+
+  it(
+    "checks that API has been been modified",
+    query(
+      `
+  SELECT * FROM api WHERE description='new description' and name='${apiName}';
+`,
+      (res: any) => expect(res.length).toBe(1)
+    )
+  );
+
+  it(
+    "tries to update API ID",
+    query(`
+  UPDATE api SET api_id='fake' WHERE name='${apiName}'
+  `)
+  );
+
+  it("applies the API ID update", apply());
+
+  it(
+    "checks that API ID has not been been modified",
+    query(
+      `
+  SELECT * FROM api WHERE api_id='fake' AND name='${apiName}';
+`,
+      (res: any) => expect(res.length).toBe(0)
+    )
+  );
+
+  it(
+    "tries to update the API protocol",
+    query(`
+  UPDATE api SET protocol_type='WEBSOCKET' WHERE name='${apiName}'
+  `)
+  );
+
+  it("applies the API protocol update", apply());
+
+  it(
+    "checks that API protocol has not been been modified",
+    query(
+      `
+  SELECT * FROM api WHERE protocol_type='HTTP' AND name='${apiName}';
+`,
+      (res: any) => expect(res.length).toBe(1)
+    )
+  );
+  it(
+    "checks that API protocol has not been been modified",
+    query(
+      `
+  SELECT * FROM api WHERE protocol_type='WEBSOCKET' AND name='${apiName}';
+`,
+      (res: any) => expect(res.length).toBe(0)
+    )
+  );
+
+  it("uninstalls the API module", uninstall(modules));
+
+  it(
+    "installs the API module again (to make sure it reloads stuff)",
+    install(modules)
+  );
+
+  it(
+    "checks API count",
+    query(
+      `
+    SELECT * FROM api WHERE name='${apiName}';
+  `,
+      (res: any) => expect(res.length).toBe(1)
+    )
+  );
+
+  it(
+    "deletes the API",
+    query(`
+    DELETE FROM api
+    WHERE name = '${apiName}';
+  `)
+  );
+
+  it("applies the API removal", apply());
+
+  it("deletes the test db", (done) =>
+    void iasql.disconnect(dbAlias, "not-needed").then(...finish(done)));
+});
+
+describe("API install/uninstall", () => {
+  it("creates a new test db", (done) =>
+    void iasql
+      .connect(dbAlias, "not-needed", "not-needed")
+      .then(...finish(done)));
+
+  it("installs the aws_account module", install(["aws_account"]));
+
+  it(
+    "inserts aws credentials",
+    query(`
+    INSERT INTO aws_account (region, access_key_id, secret_access_key)
+    VALUES ('us-east-1', '${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `)
+  );
+
+  it("installs the API module", install(modules));
+
+  it("uninstalls the API module", uninstall(modules));
+
+  it("installs all modules", (done) =>
+    void iasql
+      .install([], dbAlias, config.db.user, true)
+      .then(...finish(done)));
+
+  it("uninstalls the API module", uninstall(["aws_api_gateway"]));
+
+  it("installs the API module", install(["aws_api_gateway"]));
+
+  it("deletes the test db", (done) =>
+    void iasql.disconnect(dbAlias, "not-needed").then(...finish(done)));
+});
