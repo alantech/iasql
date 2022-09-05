047b0ab53d6363e688a1fe29200cbce795ca3bc9
Joseph Aje <ajejoseph22@gmail.com>
2022-09-05 15:58:40 +0100

    removed nullable option on aws_security_group description (#1201)

--- src/modules/0.0.19/aws_security_group/migration/1662052010605-aws_security_group.ts
+++ src/modules/0.0.19/aws_security_group/migration/1662052010605-aws_security_group.ts
@@ -1,11 +1,11 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class awsSecurityGroup1657301748333 implements MigrationInterface {
-  name = 'awsSecurityGroup1657301748333';
+export class awsSecurityGroup1662052010605 implements MigrationInterface {
+  name = 'awsSecurityGroup1662052010605';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "security_group" ("id" SERIAL NOT NULL, "description" character varying, "group_name" character varying NOT NULL, "owner_id" character varying, "group_id" character varying, "vpc_id" integer, CONSTRAINT "UQ_groupNameByVpc" UNIQUE ("group_name", "vpc_id"), CONSTRAINT "PK_08670ec0c305866dbfbfe004cb8" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "security_group" ("id" SERIAL NOT NULL, "description" character varying NOT NULL, "group_name" character varying NOT NULL, "owner_id" character varying, "group_id" character varying, "vpc_id" integer, CONSTRAINT "UQ_groupNameByVpc" UNIQUE ("group_name", "vpc_id"), CONSTRAINT "PK_08670ec0c305866dbfbfe004cb8" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `CREATE TABLE "security_group_rule" ("id" SERIAL NOT NULL, "security_group_rule_id" character varying, "is_egress" boolean NOT NULL, "ip_protocol" character varying NOT NULL, "from_port" integer, "to_port" integer, "cidr_ipv4" cidr, "cidr_ipv6" cidr, "prefix_list_id" character varying, "description" character varying, "security_group_id" integer, CONSTRAINT "UQ_rule" UNIQUE ("is_egress", "ip_protocol", "from_port", "to_port", "cidr_ipv4", "security_group_id"), CONSTRAINT "PK_f4c5f95331113ce4a2e1978a076" PRIMARY KEY ("id"))`,
