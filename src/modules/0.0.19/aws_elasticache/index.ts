67a9cd3472f8ad9f457ddc63e9bd37b67224a2f2
Yolanda Robla Mota <info@ysoft.biz>
2022-09-06 21:14:22 +0200

    fix: use an available node type for elasticache update test (#1222)
    * fix: use an available node type for elasticache update test
    
    Not all node types are available to be used on elasticache,
    and they need to be taken from an api. While the test was using
    this workflow for creating the cache, it was not using it
    for updating it.
    Modify the tests to pick a valid node as well for that test.
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    
    * fix async behaviour on query
    
    * filter offer by light utilization
    
    * filter by small modules
    
    * fix elasticache update test
    
    * fixing async behaviour in test
    
    * increase waitier timeout
    
    * change node types, increase timeout
    
    * decrease waiter time
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>

--- src/modules/0.0.19/aws_elasticache/index.ts
+++ src/modules/0.0.19/aws_elasticache/index.ts
@@ -39,7 +39,7 @@ class CacheClusterMapper extends MapperBase<CacheCluster> {
       {
         client,
         // all in seconds
-        maxWaitTime: 300,
+        maxWaitTime: 1200,
         minDelay: 1,
         maxDelay: 4,
       },
