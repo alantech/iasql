da1e966b2c0bb0b3b987c34b3649f329632d0bd9
Alejandro Guillen <aguillenv93@gmail.com>
2022-09-07 17:34:07 +0200

    refactor isqlfunctions to test

--- src/modules/0.0.19/iasql_functions/index.ts
+++ src/modules/0.0.19/iasql_functions/index.ts
@@ -1,10 +1,15 @@
 /* THIS MODULE IS A SPECIAL SNOWFLAKE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE */
+import { readFileSync } from 'fs';
 import { ModuleBase } from '../../interfaces';
 import { IasqlOperationType } from './entity';
 
 class IasqlFunctions extends ModuleBase {
   constructor() {
     super();
+    this.sql = {
+      afterInstall: readFileSync(`${__dirname}/sql/create_fns.sql`, 'utf8'),
+      beforeUninstall: readFileSync(`${__dirname}/sql/drop_fns.sql`, 'utf8')
+    }
     super.init();
   }
   iasqlOperationType = IasqlOperationType;
--- src/modules/0.0.19/iasql_functions/migration/1649177003917-iasql_functions.ts
+++ src/modules/0.0.19/iasql_functions/migration/1649177003917-iasql_functions.ts
@@ -1,7 +1,5 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-import * as sql from '../sql';
-
 export class iasqlFunctions1649177003917 implements MigrationInterface {
   name = 'iasqlFunctions1649177003917';
 
@@ -12,11 +10,9 @@ export class iasqlFunctions1649177003917 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "iasql_operation" ("opid" uuid NOT NULL, "start_date" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "end_date" TIMESTAMP WITH TIME ZONE, "optype" "public"."iasql_operation_optype_enum" NOT NULL, "params" text array NOT NULL, "output" text, "err" text, CONSTRAINT "PK_edf11c327fef1bf78dd04cdf3ce" PRIMARY KEY ("opid"))`,
     );
-    await queryRunner.query(sql.createFns);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(sql.dropFns);
     await queryRunner.query(`DROP TABLE "iasql_operation"`);
     await queryRunner.query(`DROP TYPE "public"."iasql_operation_optype_enum"`);
   }
