b75015a828f7d3e14d8dc515826ef66866764408
Alejandro Guillen <aguillenv93@gmail.com>
2022-09-09 15:58:24 +0200

    move to 0.0.20

--- src/modules/0.0.19/iasql_functions/index.ts
+++ src/modules/0.0.19/iasql_functions/index.ts
@@ -1,16 +1,10 @@
 /* THIS MODULE IS A SPECIAL SNOWFLAKE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE */
-import { readFileSync } from 'fs';
-
 import { ModuleBase } from '../../interfaces';
 import { IasqlOperationType } from './entity';
 
 class IasqlFunctions extends ModuleBase {
   constructor() {
     super();
-    this.sql = {
-      afterInstallSqlPath: 'sql/create_fns.sql',
-      beforeUninstallSqlPath: 'sql/drop_fns.sql',
-    };
     super.init();
   }
   iasqlOperationType = IasqlOperationType;
--- src/modules/0.0.20/iasql_functions/index.ts
+++ src/modules/0.0.20/iasql_functions/index.ts
@@ -1,10 +1,16 @@
 /* THIS MODULE IS A SPECIAL SNOWFLAKE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE */
+import { readFileSync } from 'fs';
+
 import { ModuleBase } from '../../interfaces';
 import { IasqlOperationType } from './entity';
 
 class IasqlFunctions extends ModuleBase {
   constructor() {
     super();
+    this.sql = {
+      afterInstallSqlPath: 'sql/create_fns.sql',
+      beforeUninstallSqlPath: 'sql/drop_fns.sql',
+    };
     super.init();
   }
   iasqlOperationType = IasqlOperationType;
