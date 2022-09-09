59f181c927cf3a39f120734d7d8d9248e097ff21
Alejandro Guillen <aguillenv93@gmail.com>
2022-09-09 12:22:06 +0200

    refactor rpc class structure

--- src/modules/0.0.19/iasql_functions/index.ts
+++ src/modules/0.0.19/iasql_functions/index.ts
@@ -1,20 +1,17 @@
 /* THIS MODULE IS A SPECIAL SNOWFLAKE. DON'T LOOK AT IT FOR HOW TO WRITE A REAL MODULE */
-import { readFileSync } from 'fs';
-
 import { ModuleBase } from '../../interfaces';
 import { IasqlOperationType } from './entity';
 import { CustomCallRpc } from './rpcs';
 
 export class IasqlFunctions extends ModuleBase {
+  customCall: CustomCallRpc;
   constructor() {
     super();
     this.sql = {
       afterInstallSqlPath: 'sql/create_fns.sql',
       beforeUninstallSqlPath: 'sql/drop_fns.sql',
     };
-    this.rpc = {
-      customCall: new CustomCallRpc(this),
-    };
+    this.customCall = new CustomCallRpc(this);
     super.init();
   }
   iasqlOperationType = IasqlOperationType;
--- src/modules/0.0.19/iasql_functions/rpcs/index.ts
+++ src/modules/0.0.19/iasql_functions/rpcs/index.ts
@@ -2,15 +2,15 @@ import { IasqlFunctions } from '..';
 import { Context, RpcBase } from '../../../interfaces';
 
 export class CustomCallRpc extends RpcBase {
-  async customCall(ctx: Context, arg1: string, arg2: string) {
+  module: IasqlFunctions;
+  name = 'custom_call';
+  call = async (ctx: Context, arg1: string, arg2: string) => {
     return JSON.stringify([{ result: 'I have been called!', arg1, arg2, memo: ctx.memo }]);
-  }
+  };
 
   constructor(module: IasqlFunctions) {
     super();
     this.module = module;
-    this.name = 'custom_call';
-    this.call = this.customCall;
     super.init();
   }
 }
