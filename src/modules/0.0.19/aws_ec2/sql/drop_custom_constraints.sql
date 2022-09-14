581a605f3bce123b8f79bfb0d1b5d669d9a9245f
David Ellis <isv.damocles@gmail.com>
2022-09-14 11:10:49 -0500

    Erase v0.0.19 from the codebase (#1273)

--- src/modules/0.0.19/aws_ec2/sql/drop_custom_constraints.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE registered_instance DROP CONSTRAINT check_target_group_instance;
-DROP FUNCTION "check_target_group_instance";
-
-ALTER TABLE instance DROP CONSTRAINT check_role_ec2;
-DROP FUNCTION "check_role_ec2";
-DROP INDEX role_policy_document_gin_idx;
-
-ALTER TABLE general_purpose_volume DROP CONSTRAINT check_instance_ebs_availability_zone;
-DROP FUNCTION "check_instance_ebs_availability_zone";
--- src/modules/0.0.19/aws_ecs_fargate/sql/drop_custom_constraints.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE service DROP CONSTRAINT check_service_subnets;
-DROP FUNCTION "check_service_subnets";
-
-DROP TRIGGER IF EXISTS check_subnets_by_service ON subnet;
-DROP FUNCTION "check_subnets_by_service";
