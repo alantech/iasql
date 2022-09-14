29eda88a0d88f42604f6e1c0ca1b19a44ed29d8e
David Ellis <isv.damocles@gmail.com>
2022-09-14 10:34:47 -0500

    Erase v0.0.19 from the codebase

--- src/modules/0.0.19/aws_elb/sql/drop_custom_constraints.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE load_balancer DROP CONSTRAINT check_load_balancer_availability_zones;
-DROP FUNCTION "check_load_balancer_availability_zones";
-
-ALTER TABLE load_balancer DROP CONSTRAINT check_load_balancer_subnets;
-DROP FUNCTION "check_load_balancer_subnets";
-
-DROP TRIGGER IF EXISTS check_subnets_by_load_balancer ON subnet;
-DROP FUNCTION "check_subnets_by_load_balancer";
