4f3b145e453087a9b7e5458d4d253af848dd7630
Yolanda Robla Mota <info@ysoft.biz>
2022-09-16 16:53:48 +0200

    Issue #745: Add module to request certificates (#1200)
    * Issue #745: Add module to request certificates
    
    It allows to request a certificate for a given domain name
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    Closes #745
    
    * initial version of validation
    
    * updating tests
    
    * add version with cname creation, but failing on listener
    
    * use custom waiter
    
    * changes in query for cert status
    
    * changes in style
    
    * add missing dependency on acm_list
    
    * add acm_request dependency on acm_list
    
    * fix missing dependencies in tests
    
    * add missing dependency
    
    * add patch for policy comparison error
    
    * add missing dependencies for elb
    
    * add missing dependencies
    
    * fix memory db dependency for aws elb
    
    * add more dependency to elb
    
    * add route53 in the list of modules to install
    
    * modify order of uninstall
    
    * change order of modules
    
    * uninstalls route 53 on the first elb uninstall
    
    * add acm_request to list of modules to uninstall
    
    * remove duplicated module
    
    * rebase on latest branch
    
    * fixes in query credentials
    
    * fix creds on reinstall
    
    * move to latest version

---
+++ src/modules/0.0.21/aws_acm_request/migration/1663312042259-aws_acm_request.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class awsAcmRequest1663312042259 implements MigrationInterface {
+  name = 'awsAcmRequest1663312042259';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "load_balancer" DROP CONSTRAINT "check_load_balancer_availability_zones"`,
+    );
+    await queryRunner.query(`ALTER TABLE "load_balancer" DROP CONSTRAINT "check_load_balancer_subnets"`);
+    await queryRunner.query(
+      `CREATE TYPE "public"."certificate_request_validation_method_enum" AS ENUM('DNS', 'EMAIL')`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "certificate_request" ("id" SERIAL NOT NULL, "arn" character varying, "domain_name" character varying NOT NULL, "domain_validation_options" json, "subject_alternative_names" text array, "validation_method" "public"."certificate_request_validation_method_enum" NOT NULL DEFAULT 'DNS', CONSTRAINT "PK_0a053bf5bbb0f21727b0120761e" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "certificate_request"`);
+    await queryRunner.query(`DROP TYPE "public"."certificate_request_validation_method_enum"`);
+    await queryRunner.query(
+      `ALTER TABLE "load_balancer" ADD CONSTRAINT "check_load_balancer_subnets" CHECK (check_load_balancer_subnets((subnets)::text[]))`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "load_balancer" ADD CONSTRAINT "check_load_balancer_availability_zones" CHECK (check_load_balancer_availability_zones(load_balancer_name, availability_zones))`,
+    );
+  }
+}
