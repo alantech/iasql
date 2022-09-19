1d1de798b544f1c2493aa896e648574b71748793
Yolanda <info@ysoft.biz>
2022-09-19 17:04:53 +0200

    add script output

--- scripts/check_changes_in_right_version.sh
+++ scripts/check_changes_in_right_version.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Checking that changes are in the right version"
+
 # get the current development version
 CURRENT_VERSION=$(grep 'latestVersion' src/config/ci.ts | cut -d ':' -f 2 | sed "s/'//g" | sed "s/,//g")
+echo "Current version is $CURRENT_VERSION"
+echo "Current commit is $GITHUB_SHA"
 
 # check if there have been modifications on previous code
 MODIFIED_FILES=($(git diff-tree --no-commit-id --name-only -r $GITHUB_SHA))
@@ -12,6 +16,7 @@ for FILE in "${MODIFIED_FILES[@]}"; do
     VERSION=$(echo $FILE|cut -d '/' -f 3)
     if [ "$VERSION" != "$CURRENT_VERSION" ]; then
       # incorrect version
+      echo "Error, out of version modifications found in file $FILE"
       exit 1
     fi
   fi
