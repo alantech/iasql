109c92dba081e8cf6e8716598839d36adea88b8b
Yolanda <info@ysoft.biz>
2022-09-19 18:53:24 +0200

    add fix in ifs

--- scripts/check_changes_in_right_version.sh
+++ scripts/check_changes_in_right_version.sh
@@ -7,16 +7,16 @@ CURRENT_VERSION=$(npx ts-node src/scripts/latestVersion.ts)
 echo "Current version is $CURRENT_VERSION"
 
 # check if there have been modifications on previous code
-IFS='' MODIFIED_FILES=($(git diff --no-commit-id --name-only -r  origin/main...))
-echo "Modified files are"
-echo $MODIFIED_FILES
+IFS=$'\n' MODIFIED_FILES=($(git diff --no-commit-id --name-only -r  origin/main...))
 
 for FILE in "${MODIFIED_FILES[@]}"; do
-  echo "I pick $FILE"
+  echo "file is $FILE"
   # if file has the pattern src/modules/ check that is just for CURRENT_VERSION
-  if  [[ $FILE == src/modules* ]]; then    
+  if  [[ "$FILE" == src/modules* ]]; then
+    echo "check version"
     # need to check the version
     VERSION=$(echo $FILE|cut -d '/' -f 3)
+    echo "version is $VERSION"
     if [ "$VERSION" != "$CURRENT_VERSION" ]; then
       # incorrect version
       echo "Error, out of version modifications found in file $FILE"
