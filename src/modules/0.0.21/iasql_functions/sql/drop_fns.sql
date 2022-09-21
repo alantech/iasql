bb43d61db6c1f86a63833755265c67faaba273ab
Alejandro Guillen <aguillenv93@gmail.com>
2022-09-21 16:33:32 +0200

    clean scripts

--- src/modules/0.0.21/iasql_functions/sql/drop_fns.sql
+++ src/modules/0.0.21/iasql_functions/sql/drop_fns.sql
@@ -2,8 +2,6 @@ DROP FUNCTION "iasql_help";
 DROP FUNCTION "iasql_version";
 DROP FUNCTION "delete_all_records";
 DROP FUNCTION "iasql_modules_installed";
-DROP FUNCTION "iasql_cloud_manipulation";
-DROP FUNCTION "until_iasql_operation";
 DROP FUNCTION "until_iasql_rpc";
 DROP FUNCTION "try_cast";
 DROP FUNCTION "iasql_audit" CASCADE;
