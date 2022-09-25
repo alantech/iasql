b0dd1ea10398f63e42ba395eaf9647176a759d0d
depombo <lfdepombo@gmail.com>
2022-09-25 17:29:41 -0400

    add todo

--- examples/ecs-fargate/prisma/infra/index.js
+++ examples/ecs-fargate/prisma/infra/index.js
@@ -10,6 +10,7 @@ const region = process.env.AWS_REGION;
 const port = 8088;
 const codebuildPolicyArn = 'arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess';
 const cloudwatchLogsArn = 'arn:aws:iam::aws:policy/CloudWatchLogsFullAccess';
+// TODO provide ECR permissions once inline policies are supported in roles
 const pushEcrPolicyArn = 'arn:aws:iam::aws:policy/service-role/EC2InstanceProfileForImageBuilderECRContainerBuilds';
 const assumeServicePolicy = {
   "Statement": [
