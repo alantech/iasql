4c3a98b06ff86559024c520aa42eeece734ac57b
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2022-09-27 19:10:56 +0300

    Multi-region support for AWS CloudWatch

---
+++ src/modules/0.0.21/aws_cloudwatch/migration/1664294867830-aws_cloudwatch.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class awsCloudwatch1664294867830 implements MigrationInterface {
+    name = 'awsCloudwatch1664294867830'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "log_group" ("id" SERIAL NOT NULL, "log_group_name" character varying NOT NULL, "log_group_arn" character varying, "creation_time" TIMESTAMP WITH TIME ZONE, "region" character varying NOT NULL DEFAULT default_aws_region(), CONSTRAINT "PK_98524f243181f6e4ef712642235" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE UNIQUE INDEX "IDX_0debae85724e7e1b623c556fb0" ON "log_group" ("log_group_name", "region") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_0debae85724e7e1b623c556fb0"`);
+        await queryRunner.query(`DROP TABLE "log_group"`);
+    }
+
+}
