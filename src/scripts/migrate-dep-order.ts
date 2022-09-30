49a819b8d31d9d4f5e4d8372a2e3102bd35a356b
Alejandro Guillen <aguillenv93@gmail.com>
2022-09-30 10:35:44 +0200

    restore migration files

--- src/scripts/migrate-dep-order.ts
+++ src/scripts/migrate-dep-order.ts
@@ -2,7 +2,7 @@
 import { createConnection } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
-import { sortMods, getModMigration, getModBeforeInstall, getModAfterInstall } from './module-json-utils';
+import { sortMods, getModMigration, getModBeforeInstall } from './module-json-utils';
 
 const moduleName = process.argv[process.argv.length - 2];
 const moduleVersion = process.argv[process.argv.length - 1];
@@ -39,9 +39,6 @@ const entities = sortedDeps.map(
     console.log(`Adding ${dep.name} migration...`);
     const migrationClass = getModMigration(dep.name, moduleVersion);
     await migrationClass.prototype.up(qr);
-    console.log(`Adding ${dep.name} after install...`);
-    const afterInstallSql = getModAfterInstall(dep.name, moduleVersion);
-    await qr.query(afterInstallSql);
   }
   console.log('Done!');
   process.exit(0);
