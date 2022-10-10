321b1989b05ccb0031c38d488d7e7e4a1d66966e
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-10 12:29:11 +0200

    apply format

--- src/modules/0.0.22/aws_memory_db/sql/before_uninstall.sql
+++ src/modules/0.0.22/aws_memory_db/sql/before_uninstall.sql
@@ -4,4 +4,7 @@ DROP TRIGGER
 DROP FUNCTION
   "check_subnets_by_subnet_group";
 
-ALTER TABLE "memory_db_cluster_security_groups" DROP CONSTRAINT "check_memorydb_cluster_security_group_region"
+ALTER TABLE
+  "memory_db_cluster_security_groups"
+DROP
+  CONSTRAINT "check_memorydb_cluster_security_group_region"
