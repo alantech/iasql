514c6c23afa7dd37e0b7a41ef67fa79ed47eb46f
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-10 12:38:45 +0200

    add multi region test

---
+++ test/modules/aws-memory-db-multi-region-integration.ts
@@ -0,0 +1,261 @@
+import * as iasql from '../../src/services/iasql';
+import {
+  getPrefix,
+  runQuery,
+  runApply,
+  runInstall,
+  finish,
+  execComposeUp,
+  execComposeDown,
+  runSync,
+} from '../helpers';
+
+const prefix = getPrefix();
+const dbAlias = 'memorydbtest';
+
+const nonDefaultRegion = 'us-easst-1';
+
+const subnetGroupName = `${prefix}${dbAlias}sng`;
+const clusterName = `${prefix}${dbAlias}cl`;
+
+const apply = runApply.bind(null, dbAlias);
+const sync = runSync.bind(null, dbAlias);
+const query = runQuery.bind(null, dbAlias);
+const install = runInstall.bind(null, dbAlias);
+const modules = ['aws_memory_db'];
+
+jest.setTimeout(1800000);
+beforeAll(async () => await execComposeUp());
+afterAll(async () => await execComposeDown());
+
+describe('MemoryDB Multi-region Integration Testing', () => {
+  it('creates a new test db', done =>
+    void iasql.connect(dbAlias, 'not-needed', 'not-needed').then(...finish(done)));
+
+  it('installs the aws_account module', install(['aws_account']));
+
+  it(
+    'inserts aws credentials',
+    query(
+      `
+    INSERT INTO aws_credentials (access_key_id, secret_access_key)
+    VALUES ('${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `,
+      undefined,
+      false,
+    ),
+  );
+
+  it('syncs the regions', sync());
+
+  it(
+    'sets the default region',
+    query(`
+    UPDATE aws_regions SET is_default = TRUE WHERE region = '${process.env.AWS_REGION}';
+  `),
+  );
+
+  it('installs the memory db module', install(modules));
+
+  it(
+    'creates a subnet group',
+    query(`
+    INSERT INTO subnet_group (subnet_group_name)
+    VALUES ('${subnetGroupName}');
+  `),
+  );
+
+  it('applies the change', apply());
+
+  it(
+    'checks the subnet group was added',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(1),
+    ),
+  );
+
+  it(
+    'creates a memory db cluster',
+    query(`
+    INSERT INTO memory_db_cluster (cluster_name, subnet_group_id)
+    VALUES ('${clusterName}', (select id from subnet_group where subnet_group_name = '${subnetGroupName}'));
+  `),
+  );
+
+  it('applies the change', apply());
+
+  it(
+    'checks the memory db cluster was added',
+    query(
+      `
+    SELECT *
+    FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(1),
+    ),
+  );
+
+  it('cshould fail trying to update luster region without changing the subnet group', () => {
+    try {
+      query(`
+        UPDATE memory_db_cluster
+        SET region = '${nonDefaultRegion}'
+        WHERE cluster_name = '${clusterName}';
+      `);
+    } catch (e) {
+      expect(e.message).toContain('tbd');
+    }
+  });
+
+  it(
+    'changes the region',
+    query(`
+    WITH updated_subnet_group AS (
+      UPDATE subnet_group
+      SET region = '${nonDefaultRegion}'
+      WHERE subnet_group_name = '${subnetGroupName}'
+    )
+    UPDATE memory_db_cluster
+    SET region = '${nonDefaultRegion}'
+    WHERE cluster_name = '${clusterName}';
+  `),
+  );
+
+  it('applies the change', apply());
+
+  it(
+    'check memory db subnet group changed region',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}' and region = '${process.env.AWS_REGION}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it(
+    'check memory db subnet group changed region',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}' and region = '${nonDefaultRegion}';
+  `,
+      (res: any[]) => expect(res.length).toBe(1),
+    ),
+  );
+
+  it(
+    'check memory db cluster changed region',
+    query(
+      `
+    SELECT *
+    FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}' and region = '${process.env.AWS_REGION}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it(
+    'check memory db cluster changed region',
+    query(
+      `
+    SELECT *
+    FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}' and region = '${nonDefaultRegion}';
+  `,
+      (res: any[]) => expect(res.length).toBe(1),
+    ),
+  );
+
+  it(
+    'removes the memory db cluster',
+    query(`
+    DELETE FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}';
+  `),
+  );
+
+  it(
+    'checks the remaining memory db cluster count',
+    query(
+      `
+    SELECT *
+    FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it('applies the change', apply());
+
+  it(
+    'checks the remaining memory db cluster count again',
+    query(
+      `
+    SELECT *
+    FROM memory_db_cluster
+    WHERE cluster_name = '${clusterName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it(
+    'check subnet group count after uninstall',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(1),
+    ),
+  );
+
+  it(
+    'removes the subnet group',
+    query(`
+    DELETE FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}';
+  `),
+  );
+
+  it(
+    'checks the remaining subnet group count',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it('applies the change', apply());
+
+  it(
+    'checks the remaining subnet group count again',
+    query(
+      `
+    SELECT *
+    FROM subnet_group
+    WHERE subnet_group_name = '${subnetGroupName}';
+  `,
+      (res: any[]) => expect(res.length).toBe(0),
+    ),
+  );
+
+  it('deletes the test db', done => void iasql.disconnect(dbAlias, 'not-needed').then(...finish(done)));
+});
