111d36c0af4737a9b9fd919fd704ecf75874ae90
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-11 16:38:47 +0200

    fix defore uninstall script

--- src/modules/0.0.22/aws_memory_db/sql/before_uninstall.sql
+++ src/modules/0.0.22/aws_memory_db/sql/before_uninstall.sql
@@ -5,7 +5,7 @@ DROP FUNCTION
   "check_subnets_by_subnet_group";
 
 DROP TRIGGER
-  IF EXISTS check_subnet_group_subnets ON subnet;
+  IF EXISTS check_subnet_group_subnets ON subnet_group;
 
 DROP FUNCTION
   "check_subnet_group_subnets";
