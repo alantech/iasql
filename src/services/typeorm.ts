aad639baa477d59c46523baba6b283c8b78b12d5
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-13 16:51:14 +0200

    remove loggers

--- src/services/typeorm.ts
+++ src/services/typeorm.ts
@@ -8,7 +8,6 @@ import { v4 as uuidv4 } from 'uuid';
 import config from '../config';
 import { modules as AllModules } from '../modules';
 import { NullCheckerSubscriber } from '../modules/subscribers';
-import logger from '../services/logger';
 
 export class TypeormWrapper {
   private connection: Connection;
@@ -91,7 +90,6 @@ export class TypeormWrapper {
     };
 
     typeorm.connection = await createConnection({ ...connOpts, entities, name });
-    logger.info(`+-+ conn ${name} or ${dbname} with entities ${entities}`);
     return typeorm;
   }
 
