d4558237ada8db48e7373fe72ef1f0c29bcce14f
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-17 13:28:46 +0200

    use region in prisma example

--- examples/ecs-fargate/prisma/infra/index.js
+++ examples/ecs-fargate/prisma/infra/index.js
@@ -77,9 +77,10 @@ async function main() {
     service_role_name: cbRole,
     source_location: ghUrl,
     build_spec: buildSpec,
+    region
   };
   await prisma.codebuild_project.upsert({
-    where: { project_name: appName},
+    where: { project_name: appName, region },
     create: pjData,
     update: pjData,
   });
@@ -89,6 +90,7 @@ async function main() {
   await prisma.codebuild_build_import.create({
     data: {
       project_name: appName,
+      region
     }
   });
 
