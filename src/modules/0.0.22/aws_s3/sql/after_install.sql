39675924357c872c638b2bd1169133d18446b190
Yolanda <info@ysoft.biz>
2022-10-18 16:06:31 +0200

    add after_install rules

---
+++ src/modules/0.0.22/aws_s3/sql/after_install.sql
@@ -0,0 +1,23 @@
+CREATE
+OR REPLACE FUNCTION block_s3_primary_key_update () RETURNS TRIGGER AS $block_s3_primary_key_update$
+    BEGIN
+        RAISE EXCEPTION 'Bucket name or region cannot be modified'
+        USING detail = 'A bucket cannot be renamed or moved to another region', 
+        hint = 'If you want to rename or change bucket region, first remove it, and after a period of 2 hours you could re-create.';
+        RETURN OLD;
+    END;
+$block_s3_primary_key_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  block_s3_primary_key_update BEFORE
+UPDATE
+  ON bucket FOR EACH ROW
+  WHEN (
+    OLD.name IS DISTINCT
+    FROM
+      NEW.name
+
+      OR OLD.region is DISTINCT FROM NEW.region
+  )
+EXECUTE
+  FUNCTION block_s3_primary_key_update ();
