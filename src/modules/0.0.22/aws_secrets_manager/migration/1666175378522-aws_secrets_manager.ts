3639eaee97313598c206c04aebf359af50f62270
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-19 12:30:35 +0200

    aws secrets manager region decorators

--- src/modules/0.0.22/aws_secrets_manager/migration/1666175378522-aws_secrets_manager.ts
+++ src/modules/0.0.22/aws_secrets_manager/migration/1666175378522-aws_secrets_manager.ts
@@ -1,15 +1,19 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class awsSecretsManager1663988304479 implements MigrationInterface {
-  name = 'awsSecretsManager1663988304479';
+export class awsSecretsManager1666175378522 implements MigrationInterface {
+  name = 'awsSecretsManager1666175378522';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE "secret" ("name" character varying NOT NULL, "description" character varying, "value" character varying, "version_id" character varying, "region" character varying NOT NULL DEFAULT default_aws_region(), CONSTRAINT "PK_f05861f3e072021bda08543eb4c" PRIMARY KEY ("name"))`,
     );
+    await queryRunner.query(
+      `ALTER TABLE "secret" ADD CONSTRAINT "FK_df5b10b7a61df5b76a27f968ee2" FOREIGN KEY ("region") REFERENCES "aws_regions"("region") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "secret" DROP CONSTRAINT "FK_df5b10b7a61df5b76a27f968ee2"`);
     await queryRunner.query(`DROP TABLE "secret"`);
   }
 }
