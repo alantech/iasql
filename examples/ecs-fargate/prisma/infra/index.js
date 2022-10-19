07be7ee31deb6483ba580176db7acef97d90333e
Yolanda <info@ysoft.biz>
2022-10-19 08:58:00 +0200

    introduce codepipeline

--- examples/ecs-fargate/prisma/infra/index.js
+++ examples/ecs-fargate/prisma/infra/index.js
@@ -4,25 +4,8 @@ const pkg = require('./package.json');
 
 // TODO replace with your desired project name
 const appName = pkg.name;
-const cbRole = `${appName}codebuild`;
-const ghToken = process.env.GH_PAT;
 const region = process.env.AWS_REGION;
 const port = 8088;
-const codebuildPolicyArn = 'arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess';
-const cloudwatchLogsArn = 'arn:aws:iam::aws:policy/CloudWatchLogsFullAccess';
-const pushEcrPolicyArn = 'arn:aws:iam::aws:policy/EC2InstanceProfileForImageBuilderECRContainerBuilds';
-const assumeServicePolicy = {
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    },
-  ],
-  "Version": "2012-10-17"
-};
 const ghUrl = 'https://github.com/iasql/iasql-engine';
 
 const prisma = new PrismaClient()
@@ -42,14 +25,6 @@ async function main() {
 
   console.dir(await prisma.$queryRaw`SELECT * from iasql_apply();`)
 
-  await prisma.source_credentials_import.create({
-    data: {
-      token: ghToken,
-      source_type: 'GITHUB',
-      auth_type: 'PERSONAL_ACCESS_TOKEN',
-    }
-  })
-
   const repoUri = (await prisma.ecs_simplified.findFirst({
     where: { app_name: appName },
     select: { repository_uri: true }
@@ -66,10 +41,70 @@ async function main() {
     update: cbData,
   });
 
-  // In Prisma's queryRaw template variables cannot be used inside SQL string literals so pass the whole string as a variable
+  // generate buildspec and store into an s3 bucket
   const repoName = `${appName}-repository`;
   const buildSpecRes = await prisma.$queryRaw`SELECT generate_put_ecr_image_build_spec(${region}, 'latest', ${repoName}, ${repoUri}, 'examples/ecs-fargate/prisma/app')`;
   const buildSpec = buildSpecRes[0]['generate_put_ecr_image_build_spec'];
+
+  await prisma.
+
+  // generate a pipeline for building image
+  const stages = JSON.stringify([
+    {
+      name: 'Source',
+      actions: [
+        {
+          name: 'SourceAction',
+          actionTypeId: {
+            category: 'Source',
+            owner: 'ThirdParty',
+            version: '1',
+            provider: 'GitHub',
+          },
+          configuration: {
+            Owner: 'iasql',
+            Repo: ghUrl,
+            Branch: 'main',
+            OAuthToken: `${process.env.GH_PAT}`,
+          },
+          outputArtifacts: [
+            {
+              name: 'Source',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Build',
+      actions: [
+        {
+          inputArtifacts: [
+            {
+              name: 'Source',
+            },
+          ],
+          name: 'Build',
+          actionTypeId: {
+            category: 'Build',
+            owner: 'AWS',
+            version: '1',
+            provider: 'AWS CodeBuild',
+          },
+          configuration: {
+            ProjectName: `${appName}`,
+            PrimarySource: 'Source'
+          },
+          outputArtifacts: [
+            {
+              name: "Image"
+            }
+          ]
+        },
+      ],
+    },
+  ]);
+
 
   const pjData = {
     project_name: appName,
