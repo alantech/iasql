5ba470721c27a2671509dc835554b67450ad64cd
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-19 10:58:24 +0200

    find the right `ModuleBase` instance name  (#1413)
    * find the right `ModuleBase` instance name
    
    * rename for clarity
    
    * fix lint

--- src/services/scheduler.ts
+++ src/services/scheduler.ts
@@ -2,13 +2,12 @@ import * as sentry from '@sentry/node';
 import express from 'express';
 import { run } from 'graphile-worker';
 import { default as cloneDeep } from 'lodash.clonedeep';
-import { camelCase } from 'typeorm/util/StringUtils';
 import { v4 as uuidv4 } from 'uuid';
 
 import config from '../config';
 import { throwError } from '../config/config';
 import { IasqlDatabase } from '../entity';
-import { Context, ModuleInterface, modules } from '../modules';
+import { Context, ModuleBase, ModuleInterface, modules } from '../modules';
 import { isString } from './common';
 import * as iasql from './iasql';
 import logger, { logErrSentry } from './logger';
@@ -175,14 +174,13 @@ export async function start(dbId: string, dbUser: string) {
           if (!Modules) {
             throwError(`Unsupported version ${versionString}. Please upgrade or replace this database.`);
           }
-          // `Modules` is an object where the keys are all the exported elements from the modules dir
-          // Classes are exported with PascalCase
-          // Instantiated objects are exported with camelCase. We are interested in this objects.
-          // `modulename` is arriving with snake_case since is how the module defines it based on the dirname
-          const moduleName = Object.keys(Modules ?? {}).find(k => k === camelCase(modulename)) ?? 'unknown';
-          if (!Modules[moduleName]) throwError(`Module ${modulename} not found`);
+          // Look for the Module's instance name with the RPC to be called
+          const [moduleInstanceName] = Object.entries(Modules ?? {})
+            .filter(([_, m]: [string, any]) => m instanceof ModuleBase)
+            .find(([_, m]: [string, any]) => m.name === modulename) ?? ['unknown', undefined];
+          if (!Modules[moduleInstanceName]) throwError(`Module ${modulename} not found`);
           const context = await getContext(conn, Modules);
-          const rpcRes: any[] | undefined = await (Modules[moduleName] as ModuleInterface)?.rpc?.[
+          const rpcRes: any[] | undefined = await (Modules[moduleInstanceName] as ModuleInterface)?.rpc?.[
             methodname
           ].call(dbId, dbUser, context, ...params);
           if (rpcRes) output = JSON.stringify(rpcRes);
