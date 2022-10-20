e71683fba5a148c16c82460fff588e0d21a0f0eb
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-20 18:29:10 +0200

    add test to avoid regression

---
+++ test/common/all-modules-have-tables.ts
@@ -0,0 +1,61 @@
+import * as iasql from '../../src/services/iasql';
+import {
+  runQuery,
+  finish,
+  execComposeUp,
+  execComposeDown,
+  runInstallAll,
+  runInstall,
+  runSync,
+} from '../helpers';
+
+const dbAlias = 'allmodulestest';
+
+const sync = runSync.bind(null, dbAlias);
+const query = runQuery.bind(null, dbAlias);
+const install = runInstall.bind(null, dbAlias);
+const installAll = runInstallAll.bind(null, dbAlias);
+
+jest.setTimeout(240000);
+beforeAll(async () => await execComposeUp());
+afterAll(async () => await execComposeDown());
+
+describe('Every module installed need to have at least a table', () => {
+  it('creates a new test db', done =>
+    void iasql.connect(dbAlias, 'not-needed', 'not-needed').then(...finish(done)));
+
+  it('installs the aws_account module', install(['aws_account']));
+
+  it(
+    'inserts aws credentials',
+    query(
+      `
+    INSERT INTO aws_credentials (access_key_id, secret_access_key)
+    VALUES ('${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+  `,
+      undefined,
+      false,
+    ),
+  );
+
+  it('syncs the regions', sync());
+
+  it(
+    'sets the default region',
+    query(`
+    UPDATE aws_regions SET is_default = TRUE WHERE region = '${process.env.AWS_REGION}';
+  `),
+  );
+
+  it('installs all modules', installAll());
+
+  it('compare iasql_tables with iasql_modules_installed', query(`
+    SELECT *
+    FROM iasql_modules_installed()
+    WHERE module_name NOT LIKE 'iasql_%' AND concat_ws ('@', module_name, module_version) NOT IN (
+      SELECT module FROM iasql_tables
+    )
+  `, (res: any[]) => expect(res?.length).toBe(0)));
+
+  it('deletes the test db', done => void iasql.disconnect(dbAlias, 'not-needed').then(...finish(done)));
+});
