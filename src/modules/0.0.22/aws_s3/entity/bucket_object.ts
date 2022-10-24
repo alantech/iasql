d00b9359ed9a1e1ee4e6d4312c9665f3df1d0c59
Yolanda <info@ysoft.biz>
2022-10-24 13:08:04 +0200

    add initial version

--- src/modules/0.0.22/aws_s3/entity/bucket_object.ts
+++ src/modules/0.0.22/aws_s3/entity/bucket_object.ts
@@ -1,16 +1,57 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn, Unique } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 import { cloudId } from '../../../../services/cloud-id';
+import { AwsRegions } from '../../aws_account/entity';
 import { Bucket } from './bucket';
 
 @Entity()
+@Unique('uq_bucketobject_id_region', ['id', 'region'])
+@Unique('uq_bucketobject_bucket_key_region', ['key', 'region'])
 export class BucketObject {
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @PrimaryColumn({
     nullable: false,
     type: 'varchar',
   })
   @cloudId
   key: string;
+
+  @cloudId
+  @ManyToOne(() => Bucket, bucket => Bucket.name, {
+    eager: true,
+    nullable: true,
+  })
+  @JoinColumn([
+    {
+      name: 'bucket_name',
+      referencedColumnName: 'name',
+    },
+    {
+      name: 'region',
+      referencedColumnName: 'region',
+    },
+  ])
+  bucket?: Bucket;
+
+  @Column({
+    type: 'character varying',
+    nullable: false,
+    default: () => 'default_aws_region()',
+  })
+  @ManyToOne(() => AwsRegions, { nullable: false })
+  @JoinColumn({ name: 'region' })
+  @cloudId
+  region: string;
 
   @Column({
     type: 'varchar',
@@ -29,17 +70,4 @@ export class BucketObject {
     nullable: true,
   })
   size?: number;
-
-  @cloudId
-  @ManyToOne(() => Bucket, bucket => Bucket.name, {
-    eager: true,
-    nullable: true,
-  })
-  @JoinColumn([
-    {
-      name: 'bucket_name',
-      referencedColumnName: 'name',
-    },
-  ])
-  bucket?: Bucket;
 }
--- src/modules/0.0.22/aws_s3/mappers/bucket_object.ts
+++ src/modules/0.0.22/aws_s3/mappers/bucket_object.ts
@@ -1,13 +1,6 @@
-import {
-  S3,
-  _Object,
-  GetBucketPolicyCommandInput,
-  PutBucketPolicyCommandInput,
-  paginateListObjectsV2,
-} from '@aws-sdk/client-s3';
+import { S3, _Object, paginateListObjectsV2 } from '@aws-sdk/client-s3';
 
 import { AwsS3Module } from '..';
-import { policiesAreSame } from '../../../../services/aws-diff';
 import { AWS, crudBuilder2, crudBuilderFormat, paginateBuilder } from '../../../../services/aws_macros';
 import { Context, Crud2, MapperBase } from '../../../interfaces';
 import { Bucket, BucketObject } from '../entity';
@@ -16,10 +9,21 @@ export class BucketObjectMapper extends MapperBase<BucketObject> {
   module: AwsS3Module;
   entity = BucketObject;
 
-  bucketObjectMapper(instance: _Object, bucket: Bucket) {
+  async bucketObjectMapper(instance: _Object, ctx: Context, bucket: String, region: string) {
     const bo: BucketObject = new BucketObject();
     if (!instance.Key) throw new Error('Received a bucket object without a key');
-    bo.bucket = bucket;
+
+    // check for bucket instance
+    if (!Object.values(ctx.memo?.cloud?.Bucket ?? {}).length) {
+      bo.bucket =
+        (await this.module.bucket.db.read(ctx, `${bucket}|${region}`)) ??
+        (await this.module.bucket.cloud.read(ctx, `${bucket}|${region}`));
+    } else {
+      bo.bucket =
+        (await this.module.bucket.db.read(ctx, `${bucket}|${region}`)) ??
+        ctx?.memo?.cloud?.CodebuildProject?.[`${bucket}|${region}`];
+    }
+    bo.region = region;
     bo.eTag = instance.ETag;
     bo.key = instance.Key;
     bo.lastModified = instance.LastModified;
@@ -33,186 +37,68 @@ export class BucketObjectMapper extends MapperBase<BucketObject> {
     return res;
   };
 
-  getBucketObjects = paginateBuilder<BucketObject>(
-    paginateListObjectsV2,
-    'Contents',
-    undefined,
-    undefined,
-    bucket => ({ Bucket: bucket }),
-  );
+  getBucketObjects = paginateBuilder<S3>(paginateListObjectsV2, 'Contents', undefined, undefined, (b, k) => ({
+    Bucket: b,
+    Prefix: k,
+  }));
 
-  getBucketLocation = crudBuilderFormat<S3, 'getBucketLocation', string | undefined>(
-    'getBucketLocation',
-    name => ({
-      Bucket: name,
-    }),
-    res => res?.LocationConstraint,
-  );
+  deleteBucketObject = crudBuilder2<S3, 'deleteObject'>('deleteObject', (b, k) => ({ Bucket: b, Key: k }));
 
-  headBucket = crudBuilderFormat<S3, 'headBucket', void>(
-    'headBucket',
-    name => ({
-      Bucket: name,
-    }),
-    res => res,
-  );
-
-  deleteBucket = crudBuilder2<S3, 'deleteBucket'>('deleteBucket', b => ({ Bucket: b }));
-
-  createBucket = crudBuilder2<S3, 'createBucket'>('createBucket', b => ({ Bucket: b }));
-
-  async getBucketPolicy(client: S3, input: GetBucketPolicyCommandInput) {
-    try {
-      const res = await client.getBucketPolicy(input);
-      return res;
-    } catch (_) {
-      // policy does not exist, return
-      return null;
-    }
-  }
-  updateBucketPolicy = crudBuilder2<S3, 'putBucketPolicy'>('putBucketPolicy', input => input);
-
-  async createBucketPolicy(client: S3, bucket: Bucket, ctx: Context) {
-    const input: PutBucketPolicyCommandInput = {
-      Bucket: bucket.name,
-      Policy: JSON.stringify(bucket.policyDocument ?? {}),
-    };
-    await this.updateBucketPolicy(client, input);
-
-    // requery the created policy from AWS
-    const inputGet: GetBucketPolicyCommandInput = {
-      Bucket: bucket.name,
-    };
-    const bucketPolicy = await this.getBucketPolicy(client, inputGet);
-    if (bucketPolicy && bucketPolicy.Policy) {
-      bucket.policyDocument = JSON.parse(bucketPolicy.Policy);
-    } else {
-      bucket.policyDocument = undefined;
-    }
-    await this.module.bucket.db.update(bucket, ctx);
-
-    return bucket.policyDocument;
-  }
-
-  cloud = new Crud2<Bucket>({
-    // TODO: There are lots of useful permission controls on create to be added to this model
-    create: async (es: Bucket[], ctx: Context) => {
-      const out = [];
-      const enabledRegions = (await ctx.getEnabledAwsRegions()) as string[];
-      let client;
-
-      for (const e of es) {
-        // if the bucket name exists in another region we cannot create it, throw error
-        for (const region of enabledRegions) {
-          client = (await ctx.getAwsClient(region)) as AWS;
-          try {
-            await this.headBucket(client.s3Client, e.name);
-            throw new Error('Cannot create the bucket, it already exists in another region');
-          } catch (_) {
-            // it is ok, we can create bucket
-          }
-        }
-
-        // we can create the bucket
-        client = (await ctx.getAwsClient(e.region)) as AWS;
-        await this.createBucket(client.s3Client, e.name);
-
-        out.push(e);
-      }
+  cloud = new Crud2<BucketObject>({
+    create: async (es: BucketObject[], ctx: Context) => {
+      // we cannot create buckets
+      const out: any = [];
       return out;
     },
     read: async (ctx: Context, id?: string) => {
       const client = (await ctx.getAwsClient(await ctx.getDefaultRegion())) as AWS;
       const enabledRegions = (await ctx.getEnabledAwsRegions()) as string[];
       const out: Bucket[] = [];
-      let rawBuckets: BucketAWS[] = [];
+      let rawObjects: BucketObject[] = [];
 
       if (!!id) {
-        const { name, region } = this.idFields(id);
+        const { bucketName, objectKey, region } = this.idFields(id);
+
         if (enabledRegions.includes(region)) {
-          const allBuckets = await this.getBuckets(client.s3Client);
-          const foundBucket = allBuckets.find((b: BucketAWS) => b.Name === name);
-          if (foundBucket) {
-            // read policy
-            const input: GetBucketPolicyCommandInput = {
-              Bucket: foundBucket.Name,
-            };
+          const client = (await ctx.getAwsClient(region)) as AWS;
 
-            const bucketPolicy = await this.getBucketPolicy(client.s3Client, input);
-            const b: Bucket = this.module.bucket.bucketMapper(foundBucket, region);
-
-            if (bucketPolicy && bucketPolicy.Policy) {
-              b.policyDocument = JSON.parse(bucketPolicy.Policy);
-            } else {
-              b.policyDocument = undefined;
-            }
-            return b;
+          const bucketObjects = await this.getBucketObjects(client.s3Client, bucketName, objectKey);
+          for (const o of bucketObjects) {
+            const finalObject = await this.bucketObjectMapper(o, ctx, bucketName, region);
+            return finalObject;
           }
         }
       } else {
-        // we need to retrieve all buckets from all regions
-        rawBuckets = (await this.getBuckets(client.s3Client)) ?? [];
-      }
-
-      if (rawBuckets && rawBuckets.length > 0) {
-        for (const rawBucket of rawBuckets) {
-          // for each bucket, retrieve the location
-          let location = await this.getBucketLocation(client.s3Client, rawBucket.Name);
-          if (!location) location = 'us-east-1';
-          if (enabledRegions.includes(location)) {
-            // read policy
-            const input: GetBucketPolicyCommandInput = {
-              Bucket: rawBucket.Name,
-            };
-
-            const bucketPolicy = await this.getBucketPolicy(client.s3Client, input);
-            const b: Bucket = this.module.bucket.bucketMapper(rawBucket, location);
-
-            if (bucketPolicy && bucketPolicy.Policy) {
-              b.policyDocument = JSON.parse(bucketPolicy.Policy);
-            } else {
-              b.policyDocument = undefined;
-            }
-            out.push(b);
+        // we need to retrieve all buckets from all regions, then read their objects
+        const rawBuckets = (await this.module.bucket.db.read(client.s3Client)) ?? [];
+        const out = [];
+        for (const bucket of rawBuckets) {
+          // retrieve objects
+          const objects = await this.getBucketObjects(client.s3Client, bucket.name);
+          for (const o of objects) {
+            const finalObject = await this.bucketObjectMapper(o, ctx, bucket.name, bucket.region);
+            if (finalObject) out.push(finalObject);
           }
         }
+        return out;
       }
-      return out;
     },
-    updateOrReplace: (a: Bucket, b: Bucket) => {
-      if (!policiesAreSame(a.policyDocument ?? [], b.policyDocument ?? [])) return 'update';
-      else return 'replace';
-    },
-    // TODO: With the model this simple it is actually impossible to really update this thing
-    // as the user can't change the create timestamp themselves, so just restore the record from
-    // the cloud cache and force it into the DB cache
-    update: async (es: Bucket[], ctx: Context) => {
+    update: async (es: BucketObject[], ctx: Context) => {
+      // we can just replace, no modification allowed
       const out = [];
-      const enabledRegions = (await ctx.getEnabledAwsRegions()) as string[];
 
       for (const e of es) {
-        if (enabledRegions.includes(e.region)) {
-          const client = (await ctx.getAwsClient(e.region)) as AWS;
-          const cloudRecord = ctx?.memo?.cloud?.Bucket?.[this.entityId(e)];
-          const isUpdate = Object.is(this.module.bucket.cloud.updateOrReplace(cloudRecord, e), 'update');
-          if (isUpdate) {
-            e.createdAt = cloudRecord.createdAt;
-            e.policyDocument = await this.module.bucket.createBucketPolicy(client.s3Client, e, ctx);
-            out.push(e);
-          } else {
-            // we cannot modify bucket name or region of the bucket, replace it
-            await this.module.bucket.db.update(cloudRecord, ctx);
-            ctx.memo.db.Bucket[this.entityId(cloudRecord)] = cloudRecord;
-            out.push(cloudRecord);
-          }
-        }
+        const cloudRecord = ctx?.memo?.cloud?.BucketObject?.[this.entityId(e)];
+        await this.module.bucketObject.db.update(cloudRecord, ctx);
+        ctx.memo.db.BucketObject[this.entityId(cloudRecord)] = cloudRecord;
+        out.push(cloudRecord);
       }
       return out;
     },
-    delete: async (es: Bucket[], ctx: Context) => {
+    delete: async (es: BucketObject[], ctx: Context) => {
       for (const e of es) {
         const client = (await ctx.getAwsClient(e.region)) as AWS;
-        await this.deleteBucket(client.s3Client, e.name);
+        await this.deleteBucketObject(client.s3Client, e.bucket, e.key);
       }
     },
   });
