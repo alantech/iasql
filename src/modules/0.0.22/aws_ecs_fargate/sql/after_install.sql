9024f062e35ced8646452ada901f0a667b90c3b1
Alejandro Guillen <aguillenv93@gmail.com>
2022-10-26 15:47:48 +0200

    block region updates

--- src/modules/0.0.22/aws_ecs_fargate/sql/after_install.sql
+++ src/modules/0.0.22/aws_ecs_fargate/sql/after_install.sql
@@ -27,3 +27,92 @@ UPDATE
   ON subnet FOR EACH ROW
 EXECUTE
   FUNCTION check_subnets_by_service ();
+
+-- Disable region changes
+CREATE
+OR REPLACE FUNCTION block_ecs_cluster_region_update () RETURNS TRIGGER AS $block_ecs_cluster_region_update$
+    BEGIN
+        RAISE EXCEPTION 'ECS Cluster region cannot be modified'
+        USING detail = 'An ECS cluster cannot be moved to another region', 
+        hint = 'If you want to change the cluster region, first remove it and recreate in the desired region.';
+        RETURN OLD;
+    END;
+$block_ecs_cluster_region_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  block_ecs_cluster_region_update BEFORE
+UPDATE
+  ON "cluster" FOR EACH ROW
+  WHEN (
+    OLD.region IS DISTINCT
+    FROM
+      NEW.region
+  )
+EXECUTE
+  FUNCTION block_ecs_cluster_region_update ();
+
+CREATE
+OR REPLACE FUNCTION block_ecs_service_region_update () RETURNS TRIGGER AS $block_ecs_service_region_update$
+    BEGIN
+        RAISE EXCEPTION 'ECS Service region cannot be modified'
+        USING detail = 'An ECS service cannot be moved to another region', 
+        hint = 'If you want to change the service region, first remove it and recreate in the desired region.';
+        RETURN OLD;
+    END;
+$block_ecs_service_region_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  block_ecs_service_region_update BEFORE
+UPDATE
+  ON service FOR EACH ROW
+  WHEN (
+    OLD.region IS DISTINCT
+    FROM
+      NEW.region
+  )
+EXECUTE
+  FUNCTION block_ecs_service_region_update ();
+
+CREATE
+OR REPLACE FUNCTION block_ecs_task_definition_region_update () RETURNS TRIGGER AS $block_ecs_task_definition_region_update$
+    BEGIN
+        RAISE EXCEPTION 'ECS Task Definition region cannot be modified'
+        USING detail = 'An ECS Task Definition cannot be moved to another region', 
+        hint = 'If you want to change the Task Definition region, first remove it and recreate in the desired region.';
+        RETURN OLD;
+    END;
+$block_ecs_task_definition_region_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  block_ecs_task_definition_region_update BEFORE
+UPDATE
+  ON task_definition FOR EACH ROW
+  WHEN (
+    OLD.region IS DISTINCT
+    FROM
+      NEW.region
+  )
+EXECUTE
+  FUNCTION block_ecs_task_definition_region_update ();
+
+CREATE
+OR REPLACE FUNCTION block_ecs_container_definition_region_update () RETURNS TRIGGER AS $block_ecs_container_definition_region_update$
+    BEGIN
+        RAISE EXCEPTION 'ECS Container Definition region cannot be modified'
+        USING detail = 'An ECS Container Definition cannot be moved to another region', 
+        hint = 'If you want to change the Container Definition region, first remove it and recreate in the desired region.';
+        RETURN OLD;
+    END;
+$block_ecs_container_definition_region_update$ LANGUAGE plpgsql;
+
+CREATE TRIGGER
+  block_ecs_container_definition_region_update BEFORE
+UPDATE
+  ON container_definition FOR EACH ROW
+  WHEN (
+    OLD.region IS DISTINCT
+    FROM
+      NEW.region
+  )
+EXECUTE
+  FUNCTION block_ecs_container_definition_region_update ();
--- src/modules/0.0.22/aws_ecs_fargate/sql/before_uninstall.sql
+++ src/modules/0.0.22/aws_ecs_fargate/sql/before_uninstall.sql
@@ -3,3 +3,27 @@ DROP TRIGGER
 
 DROP FUNCTION
   "check_subnets_by_service";
+
+DROP TRIGGER
+  IF EXISTS block_ecs_cluster_region_update ON "cluster";
+
+DROP FUNCTION
+  "block_ecs_cluster_region_update";
+
+DROP TRIGGER
+  IF EXISTS block_ecs_service_region_update ON service;
+
+DROP FUNCTION
+  "block_ecs_service_region_update";
+
+DROP TRIGGER
+  IF EXISTS block_ecs_task_definition_region_update ON task_definition;
+
+DROP FUNCTION
+  "block_ecs_task_definition_region_update";
+
+DROP TRIGGER
+  IF EXISTS block_ecs_container_definition_region_update ON container_definition;
+
+DROP FUNCTION
+  "block_ecs_container_definition_region_update";
