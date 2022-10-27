ab61e83854a301568f55cd440d618b0a053e3f7c
Yolanda Robla Mota <info@ysoft.biz>
2022-10-27 10:49:43 +0200

    fix: deprecate s3_clean_bucket (#1447)
    * fix: deprecate s3_clean_bucket
    
    With the addition of s3 objects support, there is no need
    for s3_clean_bucket anymore
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    
    * clean remaining method
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>

--- src/modules/0.0.22/aws_s3/index.ts
+++ src/modules/0.0.22/aws_s3/index.ts
@@ -1,18 +1,16 @@
 import { ModuleBase } from '../../interfaces';
 import { BucketMapper, BucketObjectMapper } from './mappers';
-import { S3CleanBucketRpc, S3UploadObjectRpc } from './rpcs';
+import { S3UploadObjectRpc } from './rpcs';
 
 export class AwsS3Module extends ModuleBase {
   bucket: BucketMapper;
   bucketObject: BucketObjectMapper;
-  s3CleanBucket: S3CleanBucketRpc;
   s3UploadObject: S3UploadObjectRpc;
 
   constructor() {
     super();
     this.bucket = new BucketMapper(this);
     this.bucketObject = new BucketObjectMapper(this);
-    this.s3CleanBucket = new S3CleanBucketRpc(this);
     this.s3UploadObject = new S3UploadObjectRpc(this);
     super.init();
   }
--- src/modules/0.0.22/aws_s3/rpcs/index.ts
+++ src/modules/0.0.22/aws_s3/rpcs/index.ts
@@ -1,2 +1,1 @@
-export * from './s3_clean_bucket';
 export * from './s3_upload_object';
