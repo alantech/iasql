3efc07f19b9587d9c8c8334dec9564eff58cb488
Yolanda Robla Mota <info@ysoft.biz>
2022-10-27 16:26:15 +0200

    fix: start new connection for statement in byStatement mode (#1450)
    * fix: start new connection for statement in byStatement mode
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>
    Closes: #1449
    
    * add independent connections in bystatement case
    
    Signed-off-by: Yolanda Robla <info@ysoft.biz>

--- src/services/iasql.ts
+++ src/services/iasql.ts
@@ -218,24 +218,40 @@ export async function runSql(dbAlias: string, uid: string, sql: string, byStatem
       }
       maxTries--;
     } while (!success && maxTries);
-    connTemp = new pg.Client({
-      database,
-      user,
-      password: pass,
-      host: dbMan.baseConnConfig.host,
-      ssl: dbMan.baseConnConfig.extra.ssl,
-    });
-    await connTemp.connect();
-    await connTemp.query(dbMan.setPostgresRoleQuery(database, versionString));
-    const stmts = parse(sql);
     const out = [];
-    for (const stmt of stmts) {
-      if (byStatement) {
+    if (byStatement) {
+      const stmts = parse(sql);
+      for (const stmt of stmts) {
+        connTemp = new pg.Client({
+          database,
+          user,
+          password: pass,
+          host: dbMan.baseConnConfig.host,
+          ssl: dbMan.baseConnConfig.extra.ssl,
+        });
+
+        await connTemp.connect();
+        await connTemp.query(dbMan.setPostgresRoleQuery(database, versionString));
+
         out.push({
           statement: deparse(stmt),
           queryRes: await connTemp.query(deparse(stmt)),
         });
-      } else {
+        await connTemp.end();
+      }
+    } else {
+      connTemp = new pg.Client({
+        database,
+        user,
+        password: pass,
+        host: dbMan.baseConnConfig.host,
+        ssl: dbMan.baseConnConfig.extra.ssl,
+      });
+
+      await connTemp.connect();
+      await connTemp.query(dbMan.setPostgresRoleQuery(database, versionString));
+      const stmts = parse(sql);
+      for (const stmt of stmts) {
         out.push(await connTemp.query(deparse(stmt)));
       }
     }
