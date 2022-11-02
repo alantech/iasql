49e479056980ed0261c87fb71a54f188183a6664
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2022-11-02 14:21:08 +0300

    Add more tests for ecr_build

--- test/modules/aws-ecr-build-integration.ts
+++ test/modules/aws-ecr-build-integration.ts
@@ -11,6 +11,7 @@ import {
   runUninstall,
   getKeyCertPair,
 } from '../helpers';
+import { AWS } from '../../src/services/aws_macros';
 
 const prefix = getPrefix();
 const dbAlias = 'ecrbuildertest';
@@ -22,7 +23,7 @@ const sync = runSync.bind(null, dbAlias);
 const query = runQuery.bind(null, dbAlias);
 const install = runInstall.bind(null, dbAlias);
 const uninstall = runUninstall.bind(null, dbAlias);
-const modules = ['aws_ecr'];
+const modules = ['aws_ecr', 'aws_iam'];
 
 
 jest.setTimeout(360000);
@@ -58,20 +59,66 @@ describe('AwsEcrBuild Integration Testing', () => {
 
   it('applies the creation of ecr repository', apply());
 
-  it('tries to call ecr_build without aws_iam installed', (done) => {
-    try {
-      query(`
-          SELECT ecr_build(
-                         'https://github.com/karthequian/docker-helloworld',
-                         (SELECT id FROM repository WHERE repository_name = '${repositoryName}')::varchar(255),
-                         '.',
-                         NULL,
-                         'master'
-                     );
-      `, (res: any) => expect(res).toThrowError());
-    } catch (e: any) {
-      expect(e.message).toContain('ecr_build RPC is only available if you have "aws_iam" module installed');
-      done();
-    }
+  it('builds hello world image and pushes to the new ecr repo', query(`
+      SELECT ecr_build(
+                     'https://github.com/karthequian/docker-helloworld',
+                     (SELECT id FROM repository WHERE repository_name = '${repositoryName}')::varchar(255),
+                     '.',
+                     NULL, -- no github access token needed
+                     'master'
+                 );
+  `));
+
+  it('checks if the image is created in the database', query(`
+      SELECT image_tag
+      FROM repository_image
+      WHERE private_repository_id = (SELECT id FROM repository WHERE repository_name = '${repositoryName}');
+  `, (res: any) => {
+    expect(res.length).toBe(1);
+    expect(res[0].image_tag === 'latest');
+  }));
+
+  it('syncs the cloud state', sync());
+
+  it('checks if the image is still there after sync', query(`
+      SELECT image_tag
+      FROM repository_image
+      WHERE private_repository_id = (SELECT id FROM repository WHERE repository_name = '${repositoryName}');
+  `, (res: any) => {
+    expect(res.length).toBe(1);
+    expect(res[0].image_tag === 'latest');
+  }));
+
+  it('checks if there is no iam role leftover', query(`
+      SELECT role_name
+      FROM iam_role
+      WHERE role_name LIKE '%-ecr-builder-codebuild-role';
+  `, (res: any) => expect(res.length).toBe(0)));
+
+  it('checks if the codebuild project is deleted', async () => {
+    const client = new AWS({
+      region: process.env.AWS_REGION!,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+    });
+
+    const projects = (await client.cbClient.listProjects({})).projects;
+    projects?.map(name => expect(name).not.toMatch(/-ecr-builder$/));
   });
+
+  it('deletes the image', query(`
+      DELETE
+      FROM repository_image
+      WHERE private_repository_id = (SELECT id FROM repository WHERE repository_name = '${repositoryName}');
+  `));
+
+  it('deletes the repository', query(`
+      DELETE
+      FROM repository
+      WHERE repository_name = '${repositoryName}'
+  `));
+
+  it('applies the deletion of resources', apply());
 });
