0dcb560c990a2c85f18a04f7380b30e17aead38d
Alejandro Guillen <aguillenv93@gmail.com>
2022-11-07 16:44:26 +0100

    restore rpc index

--- src/modules/0.0.22/iasql_functions/rpcs/index.ts
+++ src/modules/0.0.22/iasql_functions/rpcs/index.ts
@@ -5,3 +5,7 @@ export * from './iasql_modules_list';
 export * from './iasql_upgrade';
 export * from './iasql_preview';
 export * from './iasql_rollback';
+export * from './iasql_preview_sync';
+export * from './iasql_sync';
+export * from './iasql_preview_apply';
+export * from './iasql_apply';
