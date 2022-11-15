8b936875cccc589ce192ff0a11127aca2df4ad44
David Ellis <isv.damocles@gmail.com>
2022-11-15 14:34:46 -0600

    Make 'aws_s3' uninstallable

---
+++ src/modules/0.0.23/aws_s3/sql/before_uninstall.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER
+  block_s3_object_primary_key_update;
+
+DROP FUNCTION
+  block_s3_object_primary_key_update;
+
+DROP TRIGGER
+  block_s3_primary_key_update;
+
+DROP FUNCTION
+  block_s3_primary_key_update;
