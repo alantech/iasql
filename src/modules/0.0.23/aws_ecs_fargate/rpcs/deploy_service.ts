07d3777bf9fb9b59aa7cd6efff72d63567697c46
Yolanda <info@ysoft.biz>
2022-11-21 11:19:42 +0100

    add debugging

--- src/modules/0.0.23/aws_ecs_fargate/rpcs/deploy_service.ts
+++ src/modules/0.0.23/aws_ecs_fargate/rpcs/deploy_service.ts
@@ -24,7 +24,9 @@ export class DeployServiceRPC extends RpcBase {
     ctx: Context,
     arn: string,
   ): Promise<RpcResponseObject<typeof this.outputTable>[]> => {
+    console.log('in deploy service');
     if (!arn) {
+      console.log('no arn');
       return [
         {
           arn: '',
@@ -34,41 +36,60 @@ export class DeployServiceRPC extends RpcBase {
       ];
     }
     // given the service name, read the details
+    console.log('before service');
     const serviceObj: Service =
       (await this.module.service.db.read(ctx, this.module.service.generateId({ arn: arn ?? '' }))) ??
       (await this.module.service.cloud.read(ctx, this.module.service.generateId({ arn: arn ?? '' })));
+    console.log('after service');
 
     if (serviceObj) {
+      console.log('i start client');
       const client = (await ctx.getAwsClient(serviceObj.region)) as AWS;
+      console.log('before update servcie');
 
-      const service = await this.updateService(client.ecsClient, {
-        service: serviceObj.arn,
-        cluster: serviceObj.cluster?.clusterArn,
-      });
+      try {
+        const service = await this.updateService(client.ecsClient, {
+          service: serviceObj.arn,
+          cluster: serviceObj.cluster?.clusterArn,
+        });
 
-      if (service) {
-        // return ok
-        return [
-          {
-            arn,
-            status: 'OK',
-            message: 'Service updated successfully',
-          },
-        ];
-      } else {
+        if (service) {
+          console.log('i am ok');
+          // return ok
+          return [
+            {
+              arn,
+              status: 'OK',
+              message: 'Service updated successfully',
+            },
+          ];
+        } else {
+          console.log('error in update');
+          return [
+            {
+              arn: '',
+              status: 'ERROR',
+              message: 'Error updating service',
+            },
+          ];
+        }
+      } catch (e) {
+        console.log('Error in service');
+        console.log(e);
         return [
           {
             arn: '',
             status: 'ERROR',
-            message: 'Error updating service',
+            message: 'Exception',
           },
         ];
       }
     } else {
+      console.log('not found');
       return [
         {
           arn: '',
-          status: 'OK',
+          status: 'ERROR',
           message: 'Service not found',
         },
       ];
