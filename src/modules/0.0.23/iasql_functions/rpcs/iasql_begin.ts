302ae59f23722b219613a4d2fddf431982683846
Alejandro Guillen <aguillenv93@gmail.com>
2022-11-22 17:25:09 +0100

    handle begin if another commit is running or another transaction is open

--- src/modules/0.0.23/iasql_functions/rpcs/iasql_begin.ts
+++ src/modules/0.0.23/iasql_functions/rpcs/iasql_begin.ts
@@ -1,5 +1,6 @@
 import { IasqlFunctions } from '..';
 import { Context, RpcBase, RpcResponseObject } from '../../../interfaces';
+import { isCommitRunning } from '../iasql';
 
 export class IasqlBegin extends RpcBase {
   module: IasqlFunctions;
@@ -11,10 +12,18 @@ export class IasqlBegin extends RpcBase {
     _dbUser: string,
     ctx: Context,
   ): Promise<RpcResponseObject<typeof this.outputTable>[]> => {
+    const [isRunning, statusRes] = await Promise.all([
+      isCommitRunning(ctx.orm),
+      ctx.orm.query(`SELECT * FROM query_cron('status');`),
+    ]);
+    // if there's a commit/rollback running or the cron is inactive we do not continue
+    if (isRunning || (statusRes?.length && statusRes[0]['query_cron'] === 'f')) {
+      throw new Error('Another transaction is open. Close it before opening a new one.');
+    }
     await ctx.orm.query(`SELECT * FROM query_cron('disable');`);
     const message = 'Transaction started';
     // TODO: Find a way to set a timeout to init the cron again.
-    // We can use the cron state job but we sould also need to look for the last time begin was called maybe?
+    // We can use the cron state job but we should also need to look for the last time begin was called maybe?
     return [{ message }];
   };
 
