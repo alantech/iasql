ae65c022b6d25424a0d17d45420f5e0237c8e6d7
Yolanda Robla Mota <info@ysoft.biz>
2022-11-24 10:53:44 +0100

    feat: enable long queries on TheButton (#1582)
    * feat: enable long queries on TheButton
    
    When generating queries, we are passing them via GET, which
    rapidly is hitting max limit for parameters. We can instead
    generate a cookie with the md5 for the query and store the full
    select there. Next, the frontend should be able to identify
    the cookie and translate into proper query.
    
    Signed-off-by: Yolanda Robla <yolanda@iasql.com>
    
    * compress string to allow longer queries
    
    * add static dependencies
    
    Signed-off-by: Yolanda Robla <yolanda@iasql.com>

--- site/src/rehype/thebutton.js
+++ site/src/rehype/thebutton.js
@@ -1,36 +1,58 @@
 const visit = require('unist-util-visit');
+const { createHash } = require('crypto');
 
 function fixedEncodeURIComponent(str) {
   return encodeURIComponent(str).replace(/[!'()*]/g, escape);
 }
 
-const plugin = (options) => {
-  return async (ast) => {
+const plugin = options => {
+  return async ast => {
     visit(ast, 'code', (node, index, parent) => {
       const regex = /TheButton((\[(?<title>[^\]]+)\])?=("?(?<text>[^"]+)"?))?/;
       let m;
-      if ((m = regex.exec((node.meta || ''))) !== null) {
+      if ((m = regex.exec(node.meta || '')) !== null) {
         const buttonTitle = m.groups.title;
         const buttonText = m.groups.text ?? 'Run SQL';
-        let buttonUrl;
-        if (buttonTitle)
-          buttonUrl = `https://app.iasql.com/#/button/${fixedEncodeURIComponent(buttonTitle)}/${fixedEncodeURIComponent(node.value)}`;
-        else
-          buttonUrl = `https://app.iasql.com/#/button/${fixedEncodeURIComponent(node.value)}`;
 
-        parent.children.splice(index + 1, 0, {
-          type: 'paragraph',
-          children: [
-            {
-              type: 'jsx',
-              value: `<button className={"button button--primary button--lg margin-bottom--lg"} onClick={() => window.open('${buttonUrl}', '_blank')}>${buttonText}</button>`,
-            },
-          ],
-        });
+        const encodedQuery = fixedEncodeURIComponent(node.value);
+        if (encodedQuery.length >= 1024) {
+          // we go via cookies
+          const id = createHash('sha256').update(encodedQuery).digest('hex');
+          if (buttonTitle)
+            buttonUrl = `https://app.iasql.com/#/button/${fixedEncodeURIComponent(buttonTitle)}/${id}`;
+          else buttonUrl = `https://app.iasql.com/#/button/${id}`;
+
+          parent.children.splice(index + 1, 0, {
+            type: 'paragraph',
+            children: [
+              {
+                type: 'jsx',
+                value: `<button className={"button button--primary button--lg margin-bottom--lg"} onClick={() => { Cookies.set("${id}", LZString.compressToBase64("${encodedQuery}"), {expires:1, domain: window.location.hostname == 'localhost' ? 'localhost' : 'iasql.com'}); window.open('${buttonUrl}', '_blank');}}>${buttonText}</button>`,
+              },
+            ],
+          });
+        } else {
+          // we go via url
+          let buttonUrl;
+          if (buttonTitle)
+            buttonUrl = `https://app.iasql.com/#/button/${fixedEncodeURIComponent(
+              buttonTitle,
+            )}/${fixedEncodeURIComponent(node.value)}`;
+          else buttonUrl = `https://app.iasql.com/#/button/${fixedEncodeURIComponent(node.value)}`;
+
+          parent.children.splice(index + 1, 0, {
+            type: 'paragraph',
+            children: [
+              {
+                type: 'jsx',
+                value: `<button className={"button button--primary button--lg margin-bottom--lg"} onClick={() => window.open('${buttonUrl}', '_blank')}>${buttonText}</button>`,
+              },
+            ],
+          });
+        }
       }
     });
   };
 };
 
 module.exports = plugin;
-
