5017ff3ab89983d14382ef065027597e0b6337ce
Mohammad Teimori Pabandi <mtp1376@gmail.com>
2022-12-12 17:40:20 +0300

    Try to fix CI fails in lambda delete - 2

--- src/modules/aws_lambda/mappers/lambda_function.ts
+++ src/modules/aws_lambda/mappers/lambda_function.ts
@@ -394,13 +394,12 @@ export class LambdaFunctionMapper extends MapperBase<LambdaFunction> {
     delete: async (es: LambdaFunction[], ctx: Context) => {
       for (const e of es) {
         const client = (await ctx.getAwsClient(e.region)) as AWS;
-        const cloudRecord = ctx?.memo?.cloud?.LambdaFunction?.[this.entityId(e)];
 
         // Update function configuration, decoupling the VPC to allow
         // network interfaces to be released and removed automatically
         const input: UpdateFunctionConfigurationCommandInput = {
           FunctionName: e.name,
-          Role: e.role?.arn ?? cloudRecord.role?.arn,
+          Role: e.role?.arn,
           Handler: e.handler,
           Description: e.description,
           MemorySize: e.memorySize,
