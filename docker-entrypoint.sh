d7e869ccfc3fbbd5f4c5c6e176b004b73e842afb
David Ellis <isv.damocles@gmail.com>
2022-12-13 14:51:59 -0600

    This is BASH. We use GLOBALS and we LIKE IT

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,7 +15,7 @@ fi
 # initialize empty PGDATA (/var/lib/postgresql/14/main) directory with new database via 'initdb'
 # "initdb" is particular about the current user existing in "/etc/passwd", so we use "nss_wrapper" to fake that if necessary
 # see https://github.com/docker-library/postgres/pull/253, https://github.com/docker-library/postgres/issues/359, https://cwrap.org/nss_wrapper.html
-local uid; uid="$(id -u)"
+uid="$(id -u)"
 if ! getent passwd "$uid" &> /dev/null; then
   # see if we can find a suitable "libnss_wrapper.so" (https://salsa.debian.org/sssd-team/nss-wrapper/-/commit/b9925a653a54e24d09d9b498a2d913729f7abb15)
   local wrapper
