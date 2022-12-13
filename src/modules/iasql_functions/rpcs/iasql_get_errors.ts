d598a9b272daa8615d302071e712749bb4edccb5
Alejandro Guillen <aguillenv93@gmail.com>
2022-12-13 18:33:32 -0500

    add doc comments

--- src/modules/iasql_functions/rpcs/iasql_get_errors.ts
+++ src/modules/iasql_functions/rpcs/iasql_get_errors.ts
@@ -2,8 +2,29 @@ import { IasqlFunctions } from '..';
 import { TypeormWrapper } from '../../../services/typeorm';
 import { Context, RpcBase, RpcResponseObject } from '../../interfaces';
 
+/**
+ * Method to list the error messages produced by `iasql_commit` or `iasql_rollback`
+ *
+ * Returns following columns:
+ * - ts: Error message timestamp
+ * - message: Error message
+ *
+ * @example
+ * ```sql
+ * SELECT * FROM iasql_get_errors();
+ * ```
+ *
+ * @see https://iasql.com/docs/function/
+ *
+ */
 export class IasqlGetErrors extends RpcBase {
+  /**
+   * @internal
+   */
   module: IasqlFunctions;
+  /**
+   * @internal
+   */
   outputTable = {
     ts: 'timestamp with time zone',
     message: 'varchar',
