255f178714ae5fdb718f38148575dfe2ad97335f
David Ellis <isv.damocles@gmail.com>
2022-12-14 12:27:25 -0600

    Just restore the alteration of the postgres role for now to get the test suite going. May come back to this later.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -46,6 +46,10 @@ fi
 
 service postgresql start
 
+su - postgres -c "psql -c \"ALTER ROLE postgres WITH password '$(node ./dist/scripts/from-config.js db.password)'\""
+
+service postgresql restart
+
 su - postgres -c "echo \"SELECT 'CREATE DATABASE iasql_metadata' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'iasql_metadata')\gexec\" | psql"
 su - postgres -c "psql iasql_metadata -c \"CREATE EXTENSION IF NOT EXISTS pg_cron;\""
 su - postgres -c "psql iasql_metadata -c \"GRANT EXECUTE ON FUNCTION cron.schedule_in_database(text,text,text,text,text,boolean) TO postgres;\""
