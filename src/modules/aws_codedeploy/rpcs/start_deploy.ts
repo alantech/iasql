00db0d621381b19b2f65a296639db81dbf9d22a3
Yolanda <info@ysoft.biz>
2022-12-14 19:34:58 +0100

    add rpc for codedeploy

---
+++ src/modules/aws_codedeploy/rpcs/start_deploy.ts
@@ -0,0 +1,245 @@
+import { CodeDeploy, RevisionLocation, waitUntilDeploymentSuccessful } from "@aws-sdk/client-codedeploy";
+import { WaiterOptions } from "@aws-sdk/util-waiter";
+import { AwsCodedeployModule } from "..";
+import { AWS, crudBuilderFormat } from "../../../services/aws_macros";
+import { Context, RpcBase, RpcResponseObject } from "../../interfaces";
+
+/**
+ * Method for deploying a CodeDeploy application revision through a deployment group.
+ * 
+ * Accepts the following parameters:
+ * 
+ * - application: name of the application to deploy
+ * 
+ * - region: region where to trigger the deployment
+ * 
+ * - deployment group (optional): name of the deployment group to use
+ * 
+ * - revision (optional): complex type specifying the type and location of the revision to deploy
+ * 
+ * Returns following columns:
+ *
+ * - id: the ID of the triggered deployment
+ *
+ * - status: OK if the build was started successfully
+ *
+ * - message: Error message in case of failure
+ *
+ * @example
+ * ```sql TheButton[Deploy CodeDeploy application]="Deploy CodeDeploy application"
+ *   SELECT * FROM start_deploy('application_name', 'deployment_group_name', 'revision');
+ * ```
+ *
+ * @see https://github.com/iasql/iasql-engine/blob/main/test/modules/aws-codebuild-integration.ts#L313
+ * @see https://docs.aws.amazon.com/cli/latest/reference/deploy/create-deployment.html
+ *
+ */
+export class StartDeployRPC extends RpcBase {
+  /** @internal */
+  module: AwsCodedeployModule;
+
+  /** @internal */
+  outputTable = {
+    id: 'varchar',
+    status: 'varchar',
+    message: 'varchar',
+  } as const;
+
+  /** @internal */
+  startDeploy = crudBuilderFormat<CodeDeploy, 'createDeployment', string | undefined>(
+    'createDeployment',
+    input => input,
+    res => res?.deploymentId,
+  );
+
+  /** @internal */
+  call = async (
+    _dbId: string,
+    _dbUser: string,
+    ctx: Context,
+    applicationName: string,
+    region: string,
+    deploymentGroupName?: string | undefined,
+    revision?: RevisionLocation | undefined,
+
+  ): Promise<RpcResponseObject<typeof this.outputTable>[]> => {
+    if (!applicationName) {
+      return [
+        {
+          id: '',
+          status: 'ERROR',
+          message: 'Please provide the name of the CodeDeploy application to deploy',
+        },
+      ];
+    }
+
+    // validate if application name exists
+    const appObj =
+      (await this.module.application.db.read(
+        ctx,
+        this.module.application.generateId({ name: applicationName, region }),
+      )) ??
+      (await this.module.application.cloud.read(
+        ctx,
+        this.module.application.generateId({ name: applicationName, region }),
+      ));
+
+    if (!appObj) {
+      return [
+        {
+          id: '',
+          status: 'CodeDeploy application not found',
+          message: '',
+        },
+      ];
+    }
+
+    // validate if deployment group name exists
+    if (deploymentGroupName) {
+      const dgObj =
+        (await this.module.deploymentGroup.db.read(
+          ctx,
+          this.module.deploymentGroup.generateId({ deploymentGroupName, applicationName, region }),
+        )) ??
+        (await this.module.deploymentGroup.cloud.read(
+          ctx,
+          this.module.deploymentGroup.generateId({ deploymentGroupName, applicationName, region }),
+        ));
+
+      if (!appObj) {
+        return [
+          {
+            id: '',
+            status: 'CodeDeploy deployment group not found',
+            message: '',
+          },
+        ];
+      }
+    }
+
+    const client = (await ctx.getAwsClient(region)) as AWS;
+    const deploymentId = await this.createDeployment(client.cbClient, {
+      applicationName: applicationName,
+      deploymentGroupName: deploymentGroupName,
+      revision: revision,
+      region: region,
+    });
+
+    if (!deploymentId) {
+      return [
+        {
+          id: '',
+          status: 'KO',
+          message: 'Error creating deployment',
+        },
+      ];
+    }
+
+    // wait until deployment is succeeded
+    const result = await waitUntilDeploymentSuccessful(
+      {
+        client: client.cdClient,
+        // all in seconds
+        maxWaitTime: 1200,
+        minDelay: 1,
+        maxDelay: 4,
+      } as WaiterOptions<CodeDeploy>,
+      { deploymentId },
+    );
+
+    // get latest status of the deploy
+    const currentDeploy = await this.module.deployment.cloud.read(
+      ctx,
+      this.module.deployment.generateId({ deploymentId, region }),
+    );
+
+    if (!currentDeploy) {
+      return [
+        {
+          id: deploymentId,
+          status: 'KO',
+          message: 'Error getting status of deployment',
+        },
+      ];
+    }
+
+    const dbDeploy = await this.module.deployment.deploymentMapper(currentDeploy, ctx, projectObj.region);
+if (!dbBuild) {
+  return [
+    {
+      name,
+      status: 'KO',
+      message: 'Error starting build',
+    },
+  ];
+}
+
+// create the builds in the database
+await this.module.buildList.db.create(dbBuild, ctx);
+
+return [
+  {
+    name,
+    status: 'OK',
+    message: '',
+  },
+];    
+
+    if (result.state === 'SUCCESS') {
+      e.status = DeploymentStatusEnum.SUCCEEDED;
+      await this.db.update(e, ctx);
+      out.push(e);
+    }
+  }
+
+
+    // wait for builds to complete
+    await this.module.buildList.waitForBuildsToComplete(client.cbClient, [cloudBuild.id]);
+
+    // get latest status of the build
+    const awsId = cloudBuild.id;
+
+    const currentBuild = await this.module.buildList.cloud.read(
+      ctx,
+      this.module.buildList.generateId({ awsId, region }),
+    );
+
+    if (!currentBuild) {
+      return [
+        {
+          name,
+          status: 'KO',
+          message: 'Error getting status of build',
+        },
+      ];
+    }
+
+    const dbBuild = await this.module.buildList.buildListMapper(cloudBuild, ctx, projectObj.region);
+    if (!dbBuild) {
+      return [
+        {
+          name,
+          status: 'KO',
+          message: 'Error starting build',
+        },
+      ];
+    }
+
+    // create the builds in the database
+    await this.module.buildList.db.create(dbBuild, ctx);
+
+    return [
+      {
+        name,
+        status: 'OK',
+        message: '',
+      },
+    ];
+  };
+
+  constructor(module: AwsCodebuildModule) {
+    super();
+    this.module = module;
+    super.init();
+  }
+}
