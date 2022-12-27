6f72cedd8fa0ba75eda0f4a66df8588121acaac4
Alejandro Guillen <aguillenv93@gmail.com>
2022-12-27 12:22:26 +0100

    update test names

--- test/common/rollback.ts
+++ test/common/rollback.ts
@@ -1,14 +1,28 @@
 import * as iasql from '../../src/services/iasql';
-import { runQuery, finish, execComposeUp, execComposeDown, runInstall, runBegin } from '../helpers';
+import {
+  runQuery,
+  finish,
+  execComposeUp,
+  execComposeDown,
+  runInstall,
+  runBegin,
+  defaultRegion,
+  runCommit,
+  getPrefix,
+} from '../helpers';
 
-const dbAlias = 'rollbacktest';
+const prefix = getPrefix();
+const dbAlias = `${prefix}rollbacktest`;
+const sgName = `${prefix}-rb-sg`;
+const amznAmiId = 'resolve:ssm:/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2';
 
 const begin = runBegin.bind(null, dbAlias);
+const commit = runCommit.bind(null, dbAlias);
 const query = runQuery.bind(null, dbAlias);
 const install = runInstall.bind(null, dbAlias);
 const uid = '12345';
 const email = 'test@example.com';
-const logGroupName = 'teslgcommit';
+const region = defaultRegion();
 
 jest.setTimeout(360000);
 beforeAll(async () => await execComposeUp());
@@ -37,24 +51,24 @@ describe('basic rollback functionality', () => {
     'inserts aws credentials',
     query(
       `
-    INSERT INTO aws_credentials (access_key_id, secret_access_key)
-    VALUES ('${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
-  `,
+        INSERT INTO aws_credentials (access_key_id, secret_access_key)
+        VALUES ('${process.env.AWS_ACCESS_KEY_ID}', '${process.env.AWS_SECRET_ACCESS_KEY}')
+      `,
       undefined,
       false,
       () => ({ username, password }),
     ),
   );
 
-  it('installs the aws_cloudwatch module', install(['aws_cloudwatch']));
-
   it('starts a transaction', begin());
 
+  it('syncs the regions', commit());
+
   it(
-    'insert a log group',
+    'sets the default region',
     query(
       `
-    insert into log_group (log_group_name) values ('${logGroupName}');
+    UPDATE aws_regions SET is_default = TRUE WHERE region = '${region}';
   `,
       undefined,
       true,
@@ -62,41 +76,98 @@ describe('basic rollback functionality', () => {
     ),
   );
 
+  it('installs the module', install(['aws_ec2']));
+
+  it('starts a transaction', begin());
+
   it(
-    'calls iasql_preview should expect a creation',
+    'insert a log group',
     query(
       `
-    select * from iasql_preview();
-  `,
+        BEGIN;
+          INSERT INTO security_group (description, group_name, region)
+          VALUES ('${sgName} security group', '${sgName}', '${region}');
+
+          INSERT INTO instance (ami, instance_type, tags, subnet_id)
+            SELECT '${amznAmiId}', 'fake-instance-type', '{"name":"${dbAlias}"}', id
+            FROM subnet
+            WHERE availability_zone = (SELECT name FROM availability_zone WHERE region = '${region}' ORDER BY 1 DESC LIMIT 1)
+            LIMIT 1;
+
+          INSERT INTO instance_security_groups (instance_id, security_group_id) SELECT
+            (SELECT id FROM instance WHERE tags ->> 'name' = '${dbAlias}'),
+            (SELECT id FROM security_group WHERE group_name='${sgName}' AND region = '${region}');
+        COMMIT;
+      `,
+      undefined,
+      true,
+      () => ({ username, password }),
+    ),
+  );
+
+  it(
+    'checks the security group',
+    query(
+      `
+        select * from security_group where group_name = '${sgName}';
+      `,
       (res: any) => {
-        expect(res[0]['action']).toBe('create');
+        expect(res.length).toBe(1);
       },
     ),
   );
 
   it(
-    'calls iasql_rollback should delete',
+    'checks the instance',
     query(
       `
-    select * from iasql_rollback();
-  `,
+        select * from instance where tags ->> 'name' = '${dbAlias}';
+      `,
       (res: any) => {
-        expect(res[0]['action']).toBe('delete');
+        expect(res.length).toBe(1);
       },
     ),
   );
 
+  it('commit should fail and rollback', done =>
+    void query(`
+      SELECT * FROM iasql_commit();
+    `)((e?: any) => {
+      try {
+        expect(e?.message).toContain('Instance cloud create error');
+      } catch (err) {
+        done(err);
+        return {};
+      }
+      done();
+      return {};
+    }));
+
   it(
-    'checks the log group',
+    'checks the security group',
     query(
       `
-    select * from log_group where log_group_name = '${logGroupName}';
-  `,
+        select * from security_group where group_name = '${sgName}';
+      `,
       (res: any) => {
         expect(res.length).toBe(0);
       },
     ),
   );
+
+  it(
+    'checks the instance',
+    query(
+      `
+        select * from instance where tags ->> 'name' = '${dbAlias}';
+      `,
+      (res: any) => {
+        expect(res.length).toBe(0);
+      },
+    ),
+  );
+
+  it('confirm you can start a transaction', begin());
 
   it('deletes the test db', done => void iasql.disconnect(dbAlias, uid).then(...finish(done)));
 });
