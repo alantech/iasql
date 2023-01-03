1239e240ed4bf1f08870b388880dcbcf4192f6fc
Alejandro Guillen <aguillenv93@gmail.com>
2023-01-03 12:07:10 +0100

    Fail fast (#1751)
    * update jest config
    
    * force ci
    
    * restore ci
    
    * trigger ci on jest config updates
    
    * reference gh issue comment

---
+++ jest-environment-fail-fast.js
@@ -0,0 +1,21 @@
+// Based on https://github.com/facebook/jest/issues/6527#issuecomment-760092817
+
+const ParentEnvironment = require('jest-environment-node');
+
+class JestEnvironmentFailFast extends ParentEnvironment {
+  failedTest = false;
+
+  async handleTestEvent(event, state) {
+    if (event.name === 'hook_failure' || event.name === 'test_fn_failure') {
+      this.failedTest = true;
+    } else if (this.failedTest && event.name === 'test_start') {
+      event.test.mode = 'skip';
+    }
+
+    if (super.handleTestEvent) {
+      await super.handleTestEvent(event, state);
+    }
+  }
+}
+
+module.exports = JestEnvironmentFailFast;
