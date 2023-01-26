8965c77536c56556b804e3521eba38eaae5a62ae
Alejandro Guillen <aguillenv93@gmail.com>
2023-01-26 13:35:01 +0100

    Add comments

--- src/modules/iasql_platform/sql/after_install.sql
+++ src/modules/iasql_platform/sql/after_install.sql
@@ -1,3 +1,4 @@
+-- ALTER DEFAULT PRIVILEGES allows you to set the privileges that will be applied to objects created in the future. (https://www.postgresql.org/docs/current/sql-alterdefaultprivileges.html)
 ALTER DEFAULT PRIVILEGES
 FOR USER CURRENT_USER
 IN SCHEMA public
@@ -13,4 +14,5 @@ FOR USER CURRENT_USER
 IN SCHEMA public
 GRANT USAGE, SELECT ON SEQUENCES TO PUBLIC;
 
+-- Just GRANT SELECT privilege for iasql_platform tables
 GRANT SELECT ON iasql_module, iasql_tables, iasql_audit_log TO PUBLIC;
