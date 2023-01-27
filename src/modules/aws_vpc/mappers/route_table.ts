b82ef17e0b48e0f322ae40344e51b31b746f0b98
Alejandro Guillen <aguillenv93@gmail.com>
2023-01-27 15:32:34 +0100

    update route table mapper

--- src/modules/aws_vpc/mappers/route_table.ts
+++ src/modules/aws_vpc/mappers/route_table.ts
@@ -1,17 +1,10 @@
-import _ from 'lodash';
-
 import { EC2, paginateDescribeRouteTables } from '@aws-sdk/client-ec2';
 import { CreateRouteTableCommandInput } from '@aws-sdk/client-ec2/dist-types/commands/CreateRouteTableCommand';
-import {
-  Route as AwsRoute,
-  RouteTable as AwsRouteTable,
-} from '@aws-sdk/client-ec2/dist-types/models/models_1';
+import { RouteTable as AwsRouteTable } from '@aws-sdk/client-ec2/dist-types/models/models_1';
 
 import { AWS, crudBuilderFormat, paginateBuilder } from '../../../services/aws_macros';
-import { getCloudId } from '../../../services/cloud-id';
-import { findDiff } from '../../../services/diff';
 import { Context, Crud2, MapperBase } from '../../interfaces';
-import { Route, RouteTable } from '../entity';
+import { RouteTable } from '../entity';
 import { AwsVpcModule } from '../index';
 import { convertTagsForAws, convertTagsFromAws, eqTags } from './tags';
 
@@ -19,38 +12,8 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
   module: AwsVpcModule;
   entity = RouteTable;
   equals = (a: RouteTable, b: RouteTable) => {
-    return (
-      a.vpc.vpcId === b.vpc.vpcId &&
-      this.eqListItems<Route>(this.eqRoute, a.routes, b.routes) &&
-      eqTags(a.tags, b.tags)
-    );
+    return a.vpc.vpcId === b.vpc.vpcId && eqTags(a.tags, b.tags);
   };
-
-  eqListItems<T>(eq: (a: T, b: T) => boolean, a?: T[], b?: T[]) {
-    // warning: this function assumes neither of a nor b have repeated elements
-    if (a?.length !== b?.length) return false;
-    const subset1 = !!a?.every(a1 => !!b?.find(a2 => eq(a1, a2)));
-    const subset2 = !!b?.every(a1 => !!a?.find(a2 => eq(a1, a2)));
-    return subset1 && subset2;
-  }
-
-  findRoutesDiff(dbEntities: Route[], cloudEntities: Route[]) {
-    const routeId = (route: Route) => {
-      const cloudColumns = getCloudId(Route) as string[];
-      return cloudColumns.map(col => (route as any)[col]).join('|');
-    };
-    const { entitiesInDbOnly, entitiesInAwsOnly } = findDiff(
-      dbEntities,
-      cloudEntities,
-      routeId,
-      () => true, // dummy function because all fields are cloud id
-    );
-    return { entitiesInDbOnly, entitiesInAwsOnly };
-  }
-
-  eqRoute(a: Route, b: Route) {
-    return _.isEqual(_.omit(a, ['id', 'routeTable']), _.omit(b, ['id', 'routeTable']));
-  }
 
   getRouteTables = paginateBuilder<EC2>(paginateDescribeRouteTables, 'RouteTables');
   getRouteTable = crudBuilderFormat<EC2, 'describeRouteTables', AwsRouteTable | undefined>(
@@ -58,25 +21,6 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
     routeTableId => ({ RouteTableIds: [routeTableId] }),
     res => res?.RouteTables?.pop(),
   );
-
-  routeMapper(route: AwsRoute) {
-    const out = new Route();
-    out.DestinationCidrBlock = route.DestinationCidrBlock;
-    out.DestinationIpv6CidrBlock = route.DestinationIpv6CidrBlock;
-    out.DestinationPrefixListId = route.DestinationPrefixListId;
-    out.EgressOnlyInternetGatewayId = route.EgressOnlyInternetGatewayId;
-    out.GatewayId = route.GatewayId;
-    out.InstanceId = route.InstanceId;
-    out.InstanceOwnerId = route.InstanceOwnerId;
-    out.NatGatewayId = route.NatGatewayId;
-    out.TransitGatewayId = route.TransitGatewayId;
-    out.LocalGatewayId = route.LocalGatewayId;
-    out.CarrierGatewayId = route.CarrierGatewayId;
-    out.NetworkInterfaceId = route.NetworkInterfaceId;
-    out.VpcPeeringConnectionId = route.VpcPeeringConnectionId;
-    out.CoreNetworkArn = route.CoreNetworkArn;
-    return out;
-  }
 
   async routeTableMapper(routeTable: AwsRouteTable, region: string, ctx: Context) {
     const out = new RouteTable();
@@ -92,16 +36,10 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
         this.module.vpc.generateId({ vpcId: routeTable.VpcId ?? '', region }),
       ));
     if (!out.vpc) return undefined;
-
-    out.routes = [];
-    if (routeTable.Routes)
-      for (const rawRoute of routeTable.Routes) {
-        const route = await this.routeMapper(rawRoute);
-        out.routes.push(route);
-      }
-
     out.tags = convertTagsFromAws(routeTable.Tags);
     out.region = region;
+    out.routes = routeTable.Routes ?? [];
+
     return out;
   }
 
@@ -135,12 +73,6 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
         const routeTable = await this.routeTableMapper(rawRouteTable, e.region, ctx);
         if (!routeTable) continue;
 
-        // create routes
-        e.routes?.map(async r => {
-          if (r.GatewayId === 'local') return; // created by AWS, can't be created by the user
-          await RouteTableMapper.createRoute(client.ec2client, routeTable, r);
-        });
-
         routeTable.id = e.id;
         await this.module.routeTable.db.update(routeTable, ctx);
         out.push(routeTable);
@@ -170,40 +102,27 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
         return out;
       }
     },
+    updateOrReplace: (prev: RouteTable, next: RouteTable) => {
+      if (prev.vpc.vpcId !== next.vpc.vpcId) return 'replace';
+      return 'update';
+    },
     update: async (es: RouteTable[], ctx: Context) => {
       const out = [];
       for (const e of es) {
         const cloudRecord: RouteTable = ctx?.memo?.cloud?.RouteTable?.[this.entityId(e)];
-
-        if (cloudRecord.vpc.vpcId !== e.vpc.vpcId) {
+        const isUpdate = this.module.routeTable.cloud.updateOrReplace(cloudRecord, e) === 'update';
+        if (!isUpdate) {
           // delete and create a new one
           await this.module.routeTable.cloud.delete(cloudRecord, ctx);
           out.push((await this.module.routeTable.cloud.create(e, ctx)) as RouteTable);
           continue;
         }
-
-        const client = (await ctx.getAwsClient(e.region)) as AWS;
-        if (!this.eqListItems<Route>(this.eqRoute, e.routes, cloudRecord.routes)) {
-          // delete and create routes
-          const { entitiesInDbOnly, entitiesInAwsOnly } = this.findRoutesDiff(e.routes, cloudRecord.routes);
-          for (const r of entitiesInAwsOnly) {
-            if (r.GatewayId === 'local') {
-              // default route, can't be deleted by the user
-              await ctx.orm.save(Route, r);
-              ctx.memo.db.RouteTable[this.entityId(e)] = await ctx.orm.find(RouteTable, {
-                where: { id: e.id },
-              });
-              continue;
-            }
-            await RouteTableMapper.deleteRoute(client.ec2client, e, r);
-          }
-          for (const r of entitiesInDbOnly) {
-            await RouteTableMapper.createRoute(client.ec2client, e, r);
-          }
-        }
-
-        return out;
+        // Restore record
+        cloudRecord.id = e.id;
+        await this.module.routeTable.db.update(cloudRecord, ctx);
+        out.push(cloudRecord);
       }
+      return out;
     },
     delete: async (es: RouteTable[], ctx: Context) => {
       await Promise.all(
@@ -215,37 +134,6 @@ export class RouteTableMapper extends MapperBase<RouteTable> {
       );
     },
   });
-
-  private static async deleteRoute(client: EC2, routeTable: RouteTable, r: Route) {
-    return await client.deleteRoute({
-      DestinationCidrBlock: r.DestinationCidrBlock,
-      DestinationIpv6CidrBlock: r.DestinationIpv6CidrBlock,
-      DestinationPrefixListId: r.DestinationPrefixListId,
-      RouteTableId: routeTable.routeTableId,
-    });
-  }
-
-  private static async createRoute(client: EC2, routeTable: RouteTable, r: Route) {
-    return (
-      await client.createRoute({
-        RouteTableId: routeTable.routeTableId,
-        DestinationCidrBlock: r.DestinationCidrBlock,
-        DestinationIpv6CidrBlock: r.DestinationIpv6CidrBlock,
-        DestinationPrefixListId: r.DestinationPrefixListId,
-        // VpcEndpointId: r.VpcEndpointId, // exists in the CreateRouteRequest but not in Route :-?
-        EgressOnlyInternetGatewayId: r.EgressOnlyInternetGatewayId,
-        GatewayId: r.GatewayId,
-        InstanceId: r.InstanceId,
-        NatGatewayId: r.NatGatewayId,
-        TransitGatewayId: r.TransitGatewayId,
-        LocalGatewayId: r.LocalGatewayId,
-        CarrierGatewayId: r.CarrierGatewayId,
-        NetworkInterfaceId: r.NetworkInterfaceId,
-        VpcPeeringConnectionId: r.VpcPeeringConnectionId,
-        CoreNetworkArn: r.CoreNetworkArn,
-      })
-    ).Return;
-  }
 
   constructor(module: AwsVpcModule) {
     super();
