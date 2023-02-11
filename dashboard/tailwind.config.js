0c452510a1cadf4188f3b4f80d18a0d91fdabc73
David Ellis <isv.damocles@gmail.com>
2023-02-11 15:32:03 -0600

    I think this gets it working

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /*purge: [],*/
+  darkMode: 'class', // or 'media' or 'class'
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
