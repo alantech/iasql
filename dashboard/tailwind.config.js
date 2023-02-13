2ab8c744695b37a4bd8624ea9e0b7c495d708a14
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-13 11:37:00 +0100

    fux styles

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,14 +1,32 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  /*purge: [],*/
   darkMode: 'class', // or 'media' or 'class'
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3D95CE',
+        'secondary': '#95CE3D',
+        'tertiary': '#CD3C94',
+        'warn': '#D4AA00',
+        'background': 'rgb(33, 36, 38)'
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat'],
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      height: {
+        '50vh': '50vh',
+        'sidebar': 'calc(50vh - 2rem)'
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
