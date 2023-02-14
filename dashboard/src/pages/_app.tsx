fe97ef90af65ab7d0be7612a4a7a5658d3adc600
David Ellis <isv.damocles@gmail.com>
2023-02-14 12:36:49 -0600

    Fix an issue with the telemetry var in the front-end and only init posthog there

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -11,7 +11,6 @@ import * as Sentry from '@/services/sentry';
 import '@/styles/globals.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-Posthog.init();
 Sentry.init();
 
 // If you want to start measuring performance in your app, pass a function
@@ -21,6 +20,7 @@ reportWebVitals();
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
+    Posthog.init();
     const isDarkMode = localStorage.theme === 'dark';
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
