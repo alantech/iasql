a2237ec2e7e60f12aca1a725d21fd01c1a9a9144
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-16 17:12:16 +0100

    add spacing between sidebar and editor

--- dashboard/src/components/QuerySidebar/QuerySidebar.tsx
+++ dashboard/src/components/QuerySidebar/QuerySidebar.tsx
@@ -8,7 +8,7 @@ import Schema from './Schema';
 export default function QuerySidebar() {
   const { functions, allModules, installedModules } = useAppContext();
   return (
-    <div className='h-50vh w-1/5 font-normal text-xs' id='query-sidebar'>
+    <div className='h-50vh w-1/5 font-normal text-xs mr-2' id='query-sidebar'>
       <Tab tabs={[{ title: 'Schema' }, { title: 'Modules' }]} defaultIndex={1}>
         <VBox alignment={align.start} customStyles='h-sidebar bg-gray-200 dark:bg-gray-800 w-full'>
           <ReactTab.Panel className='w-full p-2 font-mono bg-gray-200 dark:bg-gray-800'>
