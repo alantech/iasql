2de29403eb8da6e6df641b840508ac797719b0c7
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-16 16:44:58 +0100

    apply @dfellis suggestion

--- dashboard/src/components/QueryResult.tsx
+++ dashboard/src/components/QueryResult.tsx
@@ -4,11 +4,12 @@ import { Label, Spinner, Table, VBox } from './common';
 
 export default function QueryResult() {
   const { editorSelectedTab, editorTabs, isRunningSql } = useAppContext();
+  const queryRes = editorTabs[editorSelectedTab]?.queryRes;
 
   // TODO: show all statements with its respective query and not just the last one
   return isRunningSql ? (
     <Spinner />
-  ) : editorTabs[editorSelectedTab].queryRes === undefined ? (
+  ) : queryRes === undefined ? (
     <EmptyState>
       <p>No query results</p>
       <p className='font-normal'>
@@ -17,31 +18,27 @@ export default function QueryResult() {
     </EmptyState>
   ) : (
     <VBox id='query-builder-result' customStyles='overflow-x-auto'>
-      {!!editorTabs[editorSelectedTab].queryRes &&
-        typeof editorTabs[editorSelectedTab].queryRes === 'string' && (
-          <Label>{editorTabs[editorSelectedTab].queryRes}</Label>
-        )}
-      {!!editorTabs[editorSelectedTab].queryRes &&
-        editorTabs[editorSelectedTab].queryRes instanceof Array &&
-        !editorTabs[editorSelectedTab].queryRes[0]?.result && (
-          <VBox>{<Table data={editorTabs[editorSelectedTab].queryRes} />}</VBox>
-        )}
-      {!!editorTabs[editorSelectedTab].queryRes &&
-        editorTabs[editorSelectedTab].queryRes instanceof Array &&
-        editorTabs[editorSelectedTab].queryRes[0]?.result instanceof Array &&
-        editorTabs[editorSelectedTab].queryRes.filter((r: any) => (r.result ?? []).length > 0).length > 0 && (
+      {!!queryRes && typeof queryRes === 'string' && <Label>{queryRes}</Label>}
+      {!!queryRes && queryRes instanceof Array && !queryRes[0]?.result && (
+        <VBox>{<Table data={queryRes} />}</VBox>
+      )}
+      {!!queryRes &&
+        queryRes instanceof Array &&
+        queryRes[0]?.result instanceof Array &&
+        queryRes.filter((r: any) => (r.result ?? []).length > 0).length > 0 && (
           <Table
-            data={editorTabs[editorSelectedTab].queryRes
+            data={queryRes
               .filter((r: any) => (r.result ?? []).length > 0)
               .map((r: any) => r.result ?? [])
               .at(-1)}
           />
         )}
-      {!!editorTabs[editorSelectedTab].queryRes &&
-        editorTabs[editorSelectedTab].queryRes instanceof Array &&
-        (editorTabs[editorSelectedTab].queryRes[0]?.result ?? []) instanceof Array &&
-        editorTabs[editorSelectedTab].queryRes.filter((r: any) => (r.result ?? []).length > 0).length ===
-          0 && <Label>Empty response</Label>}
+      {!!queryRes &&
+        queryRes instanceof Array &&
+        (queryRes[0]?.result ?? []) instanceof Array &&
+        queryRes.filter((r: any) => (r.result ?? []).length > 0).length === 0 && (
+          <Label>Empty response</Label>
+        )}
     </VBox>
   );
 }
