21980a26a3006f50816881b35f3303a25d5ad021
depombo <lfdepombo@gmail.com>
2023-02-17 14:16:16 -0500

    use kernel file

--- dashboard/src/hooks/useAuth.ts
+++ dashboard/src/hooks/useAuth.ts
@@ -5,12 +5,18 @@ import { useAuth0 } from '@auth0/auth0-react';
 import config from '../config';
 import * as Posthog from '../services/posthog';
 import * as Sentry from '../services/sentry';
+import { throwError } from '@/config/config';
 
 export function useAuth() {
   const [token, setToken] = useState(null) as unknown as [string | null, (arg0: string) => void];
   const { getAccessTokenSilently, loginWithRedirect, isAuthenticated, isLoading, user } = useAuth0();
   useEffect(() => {
-    if (!config.auth) return setToken('noauth');
+    if (!config.auth) {
+      const uid = process.env.NEXT_PUBLIC_UID ?? throwError('No uid found');
+      Sentry.identify(uid);
+      Posthog.identify(uid);
+      return setToken('noauth');
+    }
     if (!isAuthenticated && !isLoading) {
       return void loginWithRedirect({ redirectUri: window.location.href } as any);
     }
