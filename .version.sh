8f94f6e3d091c9daa2a75018d41e50da03f868b9
David Ellis <isv.damocles@gmail.com>
2023-02-20 13:50:35 -0500

    Keep the three package.json version numbers in sync (#2028)
    * Keep the three package.json version numbers in sync
    
    * Apply suggestions from code review

--- .version.sh
+++ .version.sh
@@ -17,6 +17,8 @@ CONFIGVERSION="$(cat src/config/index.ts | sed "s/^  version:.*/version: '${VERS
 
 # Update the package metadata with the specified version
 PACKAGEJSON="$(jq ".version = \"${VERSION}\"" package.json)" && echo "${PACKAGEJSON}" > package.json
+PACKAGEJSON="$(jq ".version = \"${VERSION}\"" dashboard/package.json)" && echo "${PACKAGEJSON}" > dashboard/package.json
+PACKAGEJSON="$(jq ".version = \"${VERSION}\"" site/package.json)" && echo "${PACKAGEJSON}" > site/package.json
 
 # Make sure the lockfiles are updated, too
 yarn
@@ -24,8 +26,18 @@ yarn
 # Make sure it's all formatted the way we want it
 yarn format
 
+# Same for the dashboard
+cd dashboard
+yarn
+cd ..
+
+# Same for the site
+cd site
+yarn
+cd ..
+
 # Commit and tag the update
-git add package.json yarn.lock src/config/index.ts
+git add package.json yarn.lock src/config/index.ts dashboard/package.json dashboard/yarn.lock site/package.json site/yarn.lock
 git commit -m "v${VERSION}"
 git push origin main
 gh release create v${VERSION} -t v${VERSION} -n v${VERSION} --target main
