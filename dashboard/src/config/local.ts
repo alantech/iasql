453bd6884e0e0b43b5333288dfd9193b1209e768
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-23 13:01:30 +0100

    update local config

--- dashboard/src/config/local.ts
+++ dashboard/src/config/local.ts
@@ -8,14 +8,14 @@ const local: ConfigInterface = {
     pgForceSsl: false,
     backendUrl: '/api/run',
   },
-};
-local.posthog = {
-  key: 'phc_xvAQWfpHug7G0SuU5P9wwAbvP9ZawgAfIEZ9FUsiarS',
-};
-local.sentry = {
-  dsn: 'https://8ba9a3820f7f4179b5dc12754da9c943@o1090662.ingest.sentry.io/6544238',
-  integrations: [new posthog.SentryIntegration(posthog, 'iasql', 6544238)],
-  environment: 'local',
+  posthog: {
+    key: 'phc_xvAQWfpHug7G0SuU5P9wwAbvP9ZawgAfIEZ9FUsiarS',
+  },
+  sentry: {
+    dsn: 'https://8ba9a3820f7f4179b5dc12754da9c943@o1090662.ingest.sentry.io/6544238',
+    integrations: [new posthog.SentryIntegration(posthog, 'iasql', 6544238)],
+    environment: 'local',
+  },
 };
 
 export default local;
