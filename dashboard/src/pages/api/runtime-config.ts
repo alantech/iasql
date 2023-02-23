f4cb19a0b0c9b668df354e8e4b92ba228f2e7aeb
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-23 13:02:29 +0100

    update config endpoint

--- dashboard/src/pages/api/runtime-config.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiResponse } from 'next';
-
-const handler = ({} = {}, res: NextApiResponse) => {
-  res.status(200).json({ uid: process.env.IASQL_UID ?? 'nouid', telemetry: process.env.IASQL_TELEMETRY });
-};
-
-export default handler;
