ae8dbda0e20ea9de30529b1e6aec78692a88ab06
Alejandro Guillen <aguillenv93@gmail.com>
2023-02-23 13:28:25 +0100

    remove window dependency for config provider

--- dashboard/src/components/providers/ConfigProvider.tsx
+++ dashboard/src/components/providers/ConfigProvider.tsx
@@ -20,31 +20,29 @@ const useAppConfigContext = () => {
 const AppConfigProvider = ({ children }: { children: any }) => {
   const [appConfig, setAppConfig] = useState({} as AppConfig);
   useEffect(() => {
-    if (!!global.window) {
-      const getConfig = async () => {
-        const response = await fetch(`${window.location.origin}/api/config`);
-        if (response.status === 200) {
-          const configJson = await response.json();
-          if (!Object.keys(appConfig).length) {
-            const initialAppConfig: AppConfig = {} as AppConfig;
-            if (!configJson.iasqlEnv) throwError('No IASQL_ENV provided');
-            initialAppConfig.iasqlEnv = configJson.iasqlEnv;
-            const envConfig = config[configJson?.iasqlEnv as ConfigEnvironments];
-            if (!envConfig) throwError('Invalid IASQL_ENV provided');
-            initialAppConfig.config = envConfig;
-            if (!initialAppConfig.config?.auth && !configJson.uid) throwError('No UID provided');
-            initialAppConfig.uid = configJson.uid;
-            initialAppConfig.telemetry = configJson.telemetry;
-            setAppConfig(initialAppConfig);
-          }
+    const getConfig = async () => {
+      const response = await fetch(`/api/config`);
+      if (response.status === 200) {
+        const configJson = await response.json();
+        if (!Object.keys(appConfig).length) {
+          const initialAppConfig: AppConfig = {} as AppConfig;
+          if (!configJson.iasqlEnv) throwError('No IASQL_ENV provided');
+          initialAppConfig.iasqlEnv = configJson.iasqlEnv;
+          const envConfig = config[configJson?.iasqlEnv as ConfigEnvironments];
+          if (!envConfig) throwError('Invalid IASQL_ENV provided');
+          initialAppConfig.config = envConfig;
+          if (!initialAppConfig.config?.auth && !configJson.uid) throwError('No UID provided');
+          initialAppConfig.uid = configJson.uid;
+          initialAppConfig.telemetry = configJson.telemetry;
+          setAppConfig(initialAppConfig);
         }
-      };
-      getConfig().catch((e: any) => {
-        setAppConfig({
-          configError: e.message ?? 'An error occurred getting initialization values.',
-        } as AppConfig);
-      });
-    }
+      }
+    };
+    getConfig().catch((e: any) => {
+      setAppConfig({
+        configError: e.message ?? 'An error occurred getting initialization values.',
+      } as AppConfig);
+    });
   }, [appConfig]);
   return <AppConfigContext.Provider value={appConfig}>{children}</AppConfigContext.Provider>;
 };
