f5f399cc1fc172c520873266a6c1e09e259c088b
Yolanda <info@ysoft.biz>
2023-03-14 13:31:58 +0100

    fix multi-region rds test

--- test/modules/aws-rds-multi-region.ts
+++ test/modules/aws-rds-multi-region.ts
@@ -84,8 +84,8 @@ describe('RDS Multi-Region Testing', () => {
     query(
       `
     BEGIN;
-      INSERT INTO rds (db_instance_identifier, allocated_storage, db_instance_class, master_username, master_user_password, availability_zone, engine, backup_retention_period)
-        VALUES ('${prefix}test', 20, 'db.t3.micro', 'test', 'testpass', (SELECT name FROM availability_zone WHERE region = '${region}' LIMIT 1), 'postgres:13.4', 0);
+      INSERT INTO rds (db_instance_identifier, allocated_storage, db_instance_class, master_username, master_user_password, availability_zone, engine, engine_version, backup_retention_period)
+        VALUES ('${prefix}test', 20, 'db.t3.micro', 'test', 'testpass', (SELECT name FROM availability_zone WHERE region = '${region}' LIMIT 1), 'postgres', '13.4', 0);
       INSERT INTO rds_security_groups (rds_id, security_group_id) SELECT
         (SELECT id FROM rds WHERE db_instance_identifier='${prefix}test'),
         (SELECT id FROM security_group WHERE group_name='default' AND region = '${region}');
