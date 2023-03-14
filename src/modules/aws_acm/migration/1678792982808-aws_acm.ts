92d04220e307f97cbdeeeff455a3ab0bca8f3d58
Alejandro Guillen <aguillenv93@gmail.com>
2023-03-14 13:45:33 +0100

    add tags to certificate schema

--- src/modules/aws_acm/migration/1678792982808-aws_acm.ts
+++ src/modules/aws_acm/migration/1678792982808-aws_acm.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class awsAcm1666218426906 implements MigrationInterface {
-  name = 'awsAcm1666218426906';
+export class awsAcm1678792982808 implements MigrationInterface {
+  name = 'awsAcm1678792982808';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
@@ -14,7 +14,7 @@ export class awsAcm1666218426906 implements MigrationInterface {
       `CREATE TYPE "public"."certificate_renewal_eligibility_enum" AS ENUM('ELIGIBLE', 'INELIGIBLE')`,
     );
     await queryRunner.query(
-      `CREATE TABLE "certificate" ("id" SERIAL NOT NULL, "arn" character varying, "certificate_id" character varying, "domain_name" character varying NOT NULL, "certificate_type" "public"."certificate_certificate_type_enum", "status" "public"."certificate_status_enum", "renewal_eligibility" "public"."certificate_renewal_eligibility_enum", "in_use" boolean NOT NULL DEFAULT false, "region" character varying NOT NULL DEFAULT default_aws_region(), CONSTRAINT "UQ_f863cd2ad563468a1aa9341384f" UNIQUE ("arn"), CONSTRAINT "PK_8daddfc65f59e341c2bbc9c9e43" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "certificate" ("id" SERIAL NOT NULL, "arn" character varying, "certificate_id" character varying, "domain_name" character varying NOT NULL, "certificate_type" "public"."certificate_certificate_type_enum", "status" "public"."certificate_status_enum", "renewal_eligibility" "public"."certificate_renewal_eligibility_enum", "in_use" boolean NOT NULL DEFAULT false, "region" character varying NOT NULL DEFAULT default_aws_region(), "tags" json, CONSTRAINT "UQ_f863cd2ad563468a1aa9341384f" UNIQUE ("arn"), CONSTRAINT "PK_8daddfc65f59e341c2bbc9c9e43" PRIMARY KEY ("id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "certificate" ADD CONSTRAINT "FK_4b117343e5525ae6b754b679890" FOREIGN KEY ("region") REFERENCES "aws_regions"("region") ON DELETE NO ACTION ON UPDATE NO ACTION`,
