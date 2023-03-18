772b154060150d63871c1a3ebb2062d8def03e70
David Ellis <isv.damocles@gmail.com>
2023-03-18 16:15:52 -0500

    Add file writing support

--- src/modules/ssh_accounts/index.ts
+++ src/modules/ssh_accounts/index.ts
@@ -153,6 +153,52 @@ class SshReadFileText extends RpcBase {
   }
 }
 
+class SshWriteFileText extends RpcBase {
+  /**
+   * @internal
+   */
+  module: SshAccounts;
+  /**
+   * @internal
+   */
+  outputTable = {
+    status: 'varchar',
+  } as const;
+  /**
+   * @internal
+   */
+  inputTable: RpcInput = {
+    serverName: 'varchar',
+    path: 'varchar',
+    data: 'text',
+  };
+
+  call = async (
+    _dbId: string,
+    _dbUser: string,
+    ctx: Context,
+    serverName: string,
+    path: string,
+    data: string,
+  ): Promise<RpcResponseObject<typeof this.outputTable>[]> => {
+    const sshClient = await ctx.getSshClient(serverName);
+    const filestream = await sshClient.sftp().createWriteStream(path, { encoding: 'utf8', autoClose: false });
+    await new Promise((resolve, reject) => {
+      filestream.end(data, 'utf8', (err?: Error) => {
+        if (err) reject(err);
+        resolve(true);
+      });
+    });
+    return [{ status: 'written' }]; // If we reach this path, the promise did not fail
+  };
+
+  constructor(module: SshAccounts) {
+    super();
+    this.module = module;
+    super.init();
+  }
+}
+
 class SshAccounts extends ModuleBase {
   context: PartialContext = {
     // This function is `async function () {` instead of `async () => {` because that enables the
@@ -189,12 +235,14 @@ class SshAccounts extends ModuleBase {
   sshCredentials: CredentialsMapper;
   sshLs: SshLs;
   sshReadFileText: SshReadFileText;
+  sshWriteFileText: SshWriteFileText;
 
   constructor() {
     super();
     this.sshCredentials = new CredentialsMapper(this);
     this.sshLs = new SshLs(this);
     this.sshReadFileText = new SshReadFileText(this);
+    this.sshWriteFileText = new SshWriteFileText(this);
     super.init();
   }
 }
